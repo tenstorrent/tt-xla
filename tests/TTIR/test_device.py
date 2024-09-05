33c432e716160cc61c385a3cc7ad04496e747a7b
Aleks Knezevic <aknezevic@tenstorrent.com>
2024-09-05 20:54:21 +0000

    Slight improvement to verification infra

--- tests/TTIR/test_device.py
+++ tests/TTIR/test_device.py
@@ -10,7 +10,34 @@ def test_num_devices():
 
 
 def test_to_device():
-  cpu_array = cpu_random_input((2, 2), 42)
+  cpu_array = cpu_random_input((32, 32))
   device = jax.devices()[0]
   tt_array = jax.device_put(cpu_array, device)
   assert tt_array.device.device_kind == "wormhole"
+
+
+def test_input_on_device():
+  def module_add(a, b):
+    return a + b
+  
+  tt_device = jax.devices()[0]
+  cpu_param = cpu_random_input((32, 32))
+  tt_param = jax.device_put(cpu_param, tt_device)
+
+  graph = jax.jit(module_add)
+  cpu_activation_0 = cpu_random_input((32, 32))
+  cpu_activation_1 = cpu_random_input((32, 32))
+  tt_activation_0 = jax.device_put(cpu_activation_0, tt_device)
+  tt_activation_1 = jax.device_put(cpu_activation_1, tt_device)
+  
+  res0 = graph(tt_activation_0, tt_param)
+  res1 = graph(tt_activation_1, tt_param)
+
+  res0_cpu = graph(cpu_activation_0, cpu_param)
+  res1_cpu = graph(cpu_activation_1, cpu_param)
+
+  res0 = jax.device_put(res0, res0_cpu.device)
+  res1 = jax.device_put(res1, res1_cpu.device)
+
+  assert jnp.allclose(res0, res0_cpu, atol=1e-2)
+  assert jnp.allclose(res1, res1_cpu, atol=1e-2)
--- tests/infrastructure.py
+++ tests/infrastructure.py
@@ -1,29 +1,41 @@
 import jax
 import jax.numpy as jnp
 
-def cpu_random_input(shape, key):
+def cpu_random_input(shape, key=42):
   def random_input(shape, key):
       return jax.random.uniform(jax.random.PRNGKey(key), shape=shape)
   
   cpu_random_input = jax.jit(random_input, static_argnums=[0,1], backend='cpu')
   return cpu_random_input(shape, key)
 
+def compare_tensor_to_golden(tensor, golden, required_pcc=0.99, required_atol=1e-2, assert_on_error=True):
+  ret = True
+  if tensor.device != golden.device:
+    tensor = jax.device_put(tensor, golden.device)
+  
+  ret = ret and tensor.shape == golden.shape
+  if assert_on_error:
+    assert ret, "Shapes do not match"
+  
+  if not tensor.flatten().size == 1: #pcc invalid for scalar values
+    pcc = jnp.min(jnp.corrcoef(tensor.flatten(), golden.flatten()))
+    ret = ret and pcc >= required_pcc
+    if assert_on_error:
+      assert ret, f"PCC is {pcc} which is less than {required_pcc}"
+  
+  atol = jnp.max(jnp.abs(tensor - golden))
+  ret = ret and atol <= required_atol
+  if assert_on_error:
+    assert ret, f"ATOL is {atol} which is greater than {required_atol}"
+  
+  return ret
+
 def verify_module(module, input_shapes, key=42, required_pcc=0.99, required_atol=1e-2):
   tt_device = jax.devices()[0]
   cpu_inputs = [cpu_random_input(shape, key) for shape in input_shapes]
   tt_inputs = [jax.device_put(cpu_input, tt_device) for cpu_input in cpu_inputs]
-  tt_graph = jax.jit(module, backend='tt')
-  res = tt_graph(*tt_inputs)
-  cpu_graph = jax.jit(module, backend='cpu')
-  res_cpu = cpu_graph(*cpu_inputs)
-  res = jax.device_put(res, res_cpu.device)
-
-  assert res.shape == res_cpu.shape, "Shapes do not match"
+  graph = jax.jit(module)
+  res = graph(*tt_inputs)
+  res_cpu = graph(*cpu_inputs)
   
-  if not res.flatten().size == 1: #pcc invalid for scalar values
-    pcc = jnp.min(jnp.corrcoef(res.flatten(), res_cpu.flatten()))
-    assert pcc >= required_pcc, f"PCC is {pcc} which is less than {required_pcc}"
-  
-  atol = jnp.max(jnp.abs(res - res_cpu))
-  assert atol <= required_atol, f"ATOL is {atol} which is greater than {required_atol}"
-  
+  compare_tensor_to_golden(res, res_cpu, required_pcc, required_atol)
