88ab45aca27d4e6fdf9f058e0406a46e9a621a81
Usman Aziz <uaziz@tenstorrent.com>
2024-09-20 13:42:42 +0000

    Add seperate test for convert op.

---
+++ tests/TTIR/test_convert.py
@@ -0,0 +1,16 @@
+import pytest
+import jax
+import jax.numpy as jnp
+
+def test_convert():
+  pytest.skip("Not working")  
+  def module_add(a, b):
+    return a + b
+
+  a = jnp.float32(5.)
+  b = jnp.array(6.)
+  tt_graph = jax.jit(module_add)
+  res = tt_graph(a, b)
+  cpu_graph = jax.jit(module_add, backend='cpu')
+  res_cpu = cpu_graph(a, b)
+  assert jnp.allclose(res, res_cpu)
