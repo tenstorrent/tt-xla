70a163fa2ae9dc3cfc1fe059ba34d08bb796a251
Usman Aziz <uaziz@tenstorrent.com>
2024-09-20 13:39:31 +0000

    Enable scalar add test by default.

--- tests/TTIR/test_scalar_add.py
+++ tests/TTIR/test_scalar_add.py
@@ -3,12 +3,11 @@ import jax
 import jax.numpy as jnp
 
 def test_scalar_add():
-  pytest.skip("Not working")
   def module_add(a, b):
     return a + b
 
   a = jnp.float32(5.)
-  b = jnp.array(6.)
+  b = jnp.float32(6.)
   tt_graph = jax.jit(module_add)
   res = tt_graph(a, b)
   cpu_graph = jax.jit(module_add, backend='cpu')
