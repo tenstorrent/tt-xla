c596e80307797c8ce1ce870b57ef42e77b90f366
Kristijan Mitrovic <kmitrovic@tenstorrent.com>
2025-01-13 17:56:58 +0000

    Rewrote tests for scalars and dtypes. Wrote tests for ranks.
    Fixed https://github.com/tenstorrent/tt-xla/issues/135.

--- tests/jax/test_data_types.py
+++ tests/jax/test_data_types.py
@@ -1,48 +1,65 @@
 # SPDX-FileCopyrightText: (c) 2024 Tenstorrent AI ULC
 #
 # SPDX-License-Identifier: Apache-2.0
-
-from typing import Union
+"""
+This file contains sanity tests which create arrays of various dtypes, in order not to
+parametrize each test additionally with dtypes.
+"""
 
 import jax
 import jax.numpy as jnp
 import pytest
-from infra import run_op_test, supported_dtypes
+from infra import run_op_test
 from jax._src.typing import DTypeLike
 
-# Convenience alias.
-scalar = Union[int, float]
+# Allow 64bit precision in jax which is disabled by default.
+jax.config.update("jax_enable_x64", True)
 
 
-@pytest.mark.parametrize("dtype", supported_dtypes)
-@pytest.mark.skip(
-    "Passes locally but fails on CI due to AssertionError: Unexpected XLA layout override"
+@pytest.mark.parametrize(
+    "dtype",
+    [
+        pytest.param(
+            jnp.uint8,
+            marks=pytest.mark.xfail(reason="Unsupported data type"),
+        ),
+        jnp.uint16,
+        jnp.uint32,
+        jnp.uint64,
+        pytest.param(
+            jnp.int8,
+            marks=pytest.mark.xfail(reason="Unsupported data type"),
+        ),
+        jnp.int16,
+        jnp.int32,
+        jnp.int64,
+        pytest.param(
+            jnp.float16,
+            marks=pytest.mark.xfail(reason="Unsupported data type"),
+        ),
+        jnp.float32,
+        pytest.param(
+            jnp.float64,
+            marks=pytest.mark.skip(
+                reason=(
+                    "Executable expected parameter 0 of size 8 but got buffer "
+                    "with incompatible size 4. See issue "
+                    "https://github.com/tenstorrent/tt-xla/issues/170"
+                )
+            ),
+        ),
+        jnp.bfloat16,
+    ],
 )
-def test_scalar_dtype(dtype: DTypeLike):
-    """
-    This test just returns a scalar of a certain dtype. It will fail if dtype is
-    unsupported.
-    """
+def test_dtypes(dtype: DTypeLike):
+    def scalar() -> jax.Array:
+        """
+        This test just returns a scalar of a certain dtype. It will fail if dtype is
+        unsupported.
 
-    def add(x: scalar) -> scalar:
-        return x
+        Scalars are actually 0-dim arrays. They can be created the same way arrays are,
+        using `jax.array(<some-value>, dtype)` or using `dtype(<some-value>)`.
+        """
+        return jnp.array(1, dtype)  # same as dtype(1)
 
-    in0 = dtype(1)  # Dummy scalar used as input.
-    run_op_test(add, [in0])
-
-
-@pytest.mark.parametrize("dtype", supported_dtypes)
-@pytest.mark.skip(
-    "Passes locally but fails on CI due to AssertionError: Unexpected XLA layout override"
-)
-def test_array_dtype(dtype: DTypeLike):
-    """
-    This test just returns an array of a certain dtype. It will fail if dtype is
-    unsupported.
-    """
-
-    def array(x: jax.Array) -> jax.Array:
-        return x
-
-    in0 = jnp.ones((32, 32), dtype)  # Dummy array used as input.
-    run_op_test(array, [in0])
+    run_op_test(scalar, [])
