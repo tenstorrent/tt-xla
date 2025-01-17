5b7ccd8c91b241979b8fcc8f4fe2459109182ca9
umalesTT <umales@tenstorrent.com>
2025-01-17 15:32:08 +0000

    Add tests for dot_general op for cases not equivalent to matmul op.

--- tests/jax/ops/test_dot_general.py
+++ tests/jax/ops/test_dot_general.py
@@ -11,14 +11,14 @@ from infra import run_op_test_with_random_inputs
 @pytest.mark.parametrize(
     ["x_shape", "y_shape"],
     [
-        [(32, 32), (32, 32)],
-        [(64, 64), (64, 64)],
-        [(32, 64), (64, 32)],
-        [(64, 32), (32, 64)],
+        [(1, 32), (1, 32)],
+        [(1, 32, 64), (1, 32, 32)],
+        [(2, 32, 64), (2, 32, 64)],
+        [(2, 16, 32, 64), (2, 16, 64, 32)],
     ],
 )
 def test_dot_general(x_shape: tuple, y_shape: tuple):
     def dot_general(x: jax.Array, y: jax.Array) -> jax.Array:
-        return jnp.dot(x, y)
+        return jax.lax.dot_general(x, y, dimension_numbers=((1, 1), (0, 0)))
 
     run_op_test_with_random_inputs(dot_general, [x_shape, y_shape])
