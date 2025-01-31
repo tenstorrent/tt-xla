3bd8ae83fa8fcd42a3a7fd20539097c15de820cf
Stefan Gligorijevic <189116645+sgligorijevicTT@users.noreply.github.com>
2025-01-31 15:50:04 +0100

    Add more variants of GPT2 (#216)

---
+++ tests/jax/models/gpt2/medium/test_gpt2_medium.py
@@ -0,0 +1,43 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+
+
+import pytest
+from infra import ModelTester, RunMode
+
+from ..tester import GPT2Tester
+
+MODEL_PATH = "openai-community/gpt2-medium"
+
+
+# ----- Fixtures -----
+
+
+@pytest.fixture
+def inference_tester() -> GPT2Tester:
+    return GPT2Tester(MODEL_PATH)
+
+
+@pytest.fixture
+def training_tester() -> GPT2Tester:
+    return GPT2Tester(ModelTester, run_mode=RunMode.TRAINING)
+
+
+# ----- Tests -----
+
+
+@pytest.mark.xfail(
+    reason="Cannot get the device from a tensor with host storage (https://github.com/tenstorrent/tt-xla/issues/171)"
+)
+def test_gpt2_medium_inference(
+    inference_tester: GPT2Tester,
+):
+    inference_tester.test()
+
+
+@pytest.mark.skip(reason="Support for training not implemented")
+def test_gpt2_medium_training(
+    training_tester: GPT2Tester,
+):
+    training_tester.test()
