5feff2ee1f2a8850b47260d26906e0a0fe0f3c8c
ajakovljevicTT <ajakovljevic@tenstorrent.com>
2025-02-05 14:38:17 +0000

    CLeaning up loaded executable

--- src/common/pjrt_implementation/loaded_executable_instance.cc
+++ src/common/pjrt_implementation/loaded_executable_instance.cc
@@ -84,46 +84,40 @@ LoadedExecutableInstance::Execute(PJRT_LoadedExecutable_Execute_Args *args) {
   DLOG_F(LOG_DEBUG, "LoadedExecutableInstance::Execute");
 
   auto [system_desc, chip_ids] = tt::runtime::getCurrentSystemDesc();
+  size_t num_devices = args->num_devices;
 
-  assert(args->num_devices == 2);
-  int dev_index = 0;
   tt::runtime::Binary binary(image_->get_binary());
 
   std::vector<tt::runtime::Tensor> rt_inputs;
-  rt_inputs.reserve(2);
 
   std::unordered_set<int> device_ids;
 
-  for (int k=0;k<2;k++)
-  {
-    for (size_t i = 0; i < args->num_args; ++i) {
-      BufferInstance *buffer =
-          BufferInstance::Unwrap(args->argument_lists[k][i]);
-      rt_inputs.emplace_back(buffer->tensor());
-      device_ids.insert(
-          chip_ids[buffer->device().device_description()->device_id()]);
-      std::cerr << "aaaa=" << buffer->device().device_description()->debug_string() << std::endl;
-      DLOG_F(INFO, "Runtime input id: %d", buffer->unique_id());
-    }
+  for (size_t i = 0; i < num_devices; i++) {
+    BufferInstance *buffer =
+      BufferInstance::Unwrap(args->argument_lists[i][0]);
+    device_ids.insert(
+      chip_ids[buffer->device().device_description()->device_id()]);
   }
 
-  assert(device_ids.size() == 2);
+  for (size_t i = 0; i < args->num_args; ++i) {
+    BufferInstance *buffer =
+        BufferInstance::Unwrap(args->argument_lists[0][i]);
+    rt_inputs.push_back(buffer->tensor());
+    DLOG_F(INFO, "Runtime input id: %d", buffer->unique_id());
+  }
+
+  assert(device_ids.size() == num_devices);
 
   std::vector<int> device_ids_vector(device_ids.begin(), device_ids.end());
 
   tt::runtime::Device device = tt::runtime::openDevice(device_ids_vector);
   std::vector<tt::runtime::Tensor> input_tensors;
   int size_inputs = rt_inputs.size();
-  std::vector<std::vector<tt::runtime::Tensor>> rt_outputs;
-  for (int k = 0; k<2;k++)
-  {
-    rt_outputs.push_back(
-        tt::runtime::submit(device, binary, 0, {rt_inputs.begin() + k*size_inputs/2, rt_inputs.begin() + (k+1)*size_inputs/2}));
-  }
+  std::vector<tt::runtime::Tensor> rt_outputs = tt::runtime::submit(device, binary, 0, rt_inputs);
   std::vector<tt::runtime::TensorDesc> output_specs =
       binary.getProgramOutputs(0);
 
-  for (int k=0;k<2;k++)
+  for (int k=0;k<num_devices;k++)
   {
     for (size_t i = 0; i < output_specs.size(); ++i) {
       bool is_scalar = client_.isOutputScalar(i);
@@ -131,7 +125,7 @@ LoadedExecutableInstance::Execute(PJRT_LoadedExecutable_Execute_Args *args) {
       std::vector<std::uint32_t> output_shape =
           is_scalar ? std::vector<std::uint32_t>() : output_specs[i].shape;
       auto result_buffer = std::make_unique<BufferInstance>(
-          *this->addressable_devices_[k], rt_outputs[k][i], output_shape,
+          *this->addressable_devices_[k], rt_outputs[i], output_shape,
           output_specs[i].stride);
       result_buffer->setType(tt::pjrt::utils::convertElementTypeToBufferType(
           output_specs[i].dataType));
@@ -139,12 +133,10 @@ LoadedExecutableInstance::Execute(PJRT_LoadedExecutable_Execute_Args *args) {
       args->output_lists[k][i] = *(result_buffer.release());
     }
   }
-  std::cerr << "ended2" << std::endl;
 
   if (args->device_complete_events) {
     for (int i=0;i<2;i++) args->device_complete_events[i] = *(new EventInstance());
   }
-  std::cerr << "ended" << std::endl;
 
   tt::runtime::closeDevice(device);
 
