c43883e9ed52751391469e25cf1df08b18ca1efd
Stefan Gligorijevic <189116645+sgligorijevicTT@users.noreply.github.com>
2025-02-06 11:33:51 +0100

    Add BLOOM tests (#236)

---
+++ tests/jax/models/bloom/bloom_560m/test_560m.py
@@ -0,0 +1,50 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from typing import Callable
+
+import pytest
+from infra import ModelTester, RunMode
+from utils import compile_fail, record_model_test_properties
+
+from ..tester import BloomTester
+
+MODEL_PATH = "bigscience/bloom-560m"
+MODEL_NAME = "bloom-560m"
+
+
+# ----- Fixtures -----
+
+
+@pytest.fixture
+def inference_tester() -> BloomTester:
+    return BloomTester(MODEL_PATH)
+
+
+@pytest.fixture
+def training_tester() -> BloomTester:
+    return BloomTester(ModelTester, run_mode=RunMode.TRAINING)
+
+
+# ----- Tests -----
+
+
+@pytest.mark.skip(reason=compile_fail("Unsupported data type"))  # segfault
+def test_bloom_560m_inference(
+    inference_tester: BloomTester,
+    record_tt_xla_property: Callable,
+):
+    record_model_test_properties(record_tt_xla_property, MODEL_NAME)
+
+    inference_tester.test()
+
+
+@pytest.mark.skip(reason="Support for training not implemented")
+def test_bloom_560m_training(
+    training_tester: BloomTester,
+    record_tt_xla_property: Callable,
+):
+    record_model_test_properties(record_tt_xla_property, MODEL_NAME)
+
+    training_tester.test()
