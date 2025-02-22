d6612b093cb883c4f2c4c377075a74aba49d2531
Marko Rakita <mrakita@tenstorrent.com>
2025-02-22 18:37:39 +0000

    Install ttmlir and llvm requirements during ttmlir build

---
+++ venv/install_ttmlir_requirements.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: (c) 2024 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Installs python requirements for third party tt-mlir project.
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+source ${TTPJRT_SOURCE_DIR}/venv/bin/activate
+
+TT_MLIR_ENV_DIR=${TTPJRT_SOURCE_DIR}/third_party/tt-mlir/src/tt-mlir/env
+
+# Install llvm requirements
+LLVM_VERSION=$(grep -oP 'set\(LLVM_PROJECT_VERSION "\K[^"]+' ${TT_MLIR_ENV_DIR}/CMakeLists.txt)
+REQUIREMENTS_CASHE_DIR=${TTPJRT_SOURCE_DIR}/venv/bin/requirements_cashe
+LLVM_REQUIREMENTS_NAME=llvm-requirements-${LLVM_VERSION}.txt
+LLVM_REQUIREMENTS_PATH=${REQUIREMENTS_CASHE_DIR}/${LLVM_REQUIREMENTS_NAME}
+if [ ! -e $LLVM_REQUIREMENTS_PATH ]; then
+  wget -P $REQUIREMENTS_CASHE_DIR -O $LLVM_REQUIREMENTS_NAME "https://github.com/llvm/llvm-project/raw/$LLVM_VERSION/mlir/python/requirements.txt" --quiet
+fi
+pip install -r $LLVM_REQUIREMENTS_PATH
+
+# Install tt-mlir requirements
+pip install -r ${TT_MLIR_ENV_DIR}/build-requirements.txt
