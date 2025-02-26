8fa564f05b0e16add9694589397acd61c3709243
sgligorijevicTT <189116645+sgligorijevicTT@users.noreply.github.com>
2025-02-26 11:08:13 +0000

    Add tests for CLIP

---
+++ tests/jax/models/clip/base_patch16/test_clip_base_patch16.py
@@ -0,0 +1,58 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+
+
+from typing import Callable
+
+import pytest
+from infra import RunMode
+from utils import compile_fail, record_model_test_properties
+
+from ..tester import FlaxCLIPTester
+
+MODEL_PATH = "openai/clip-vit-base-patch16"
+MODEL_NAME = "clip-base-patch16"
+
+
+# ----- Fixtures -----
+
+
+@pytest.fixture
+def inference_tester() -> FlaxCLIPTester:
+    return FlaxCLIPTester(MODEL_PATH)
+
+
+@pytest.fixture
+def training_tester() -> FlaxCLIPTester:
+    return FlaxCLIPTester(MODEL_PATH, RunMode.TRAINING)
+
+
+# ----- Tests -----
+
+
+@pytest.mark.push
+@pytest.mark.nightly
+@pytest.mark.skip(
+    reason=compile_fail(
+        'Assertion `llvm::isUIntN(BitWidth, val) && "Value is not an N-bit unsigned value"\' failed.'
+    )
+)
+def test_clip_base_patch16_inference(
+    inference_tester: FlaxCLIPTester,
+    record_tt_xla_property: Callable,
+):
+    record_model_test_properties(record_tt_xla_property, MODEL_NAME)
+
+    inference_tester.test()
+
+
+@pytest.mark.push
+@pytest.mark.skip(reason="Support for training not implemented")
+def test_clip_base_patch16_training(
+    training_tester: FlaxCLIPTester,
+    record_tt_xla_property: Callable,
+):
+    record_model_test_properties(record_tt_xla_property, MODEL_NAME)
+
+    training_tester.test()
