27a4ae0773171cdc6db07b64dd62baec75425618
ajakovljevicTT <ajakovljevic@tenstorrent.com>
2025-03-07 14:01:19 +0000

    Fixed tests

--- tests/jax/multichip/manual/unary_eltwise.py
+++ tests/jax/multichip/manual/unary_eltwise.py
@@ -12,7 +12,6 @@ from utils import compile_fail
 @pytest.mark.parametrize(
     ("x_shape", "mesh_shape", "axis_names"), [((256, 256), (1, 2), ("x", "y"))]
 )
-@pytest.mark.skip(reason=compile_fail("Multichip still in development"))
 def test_unary_eltwise(x_shape: tuple, mesh_shape: tuple, axis_names: tuple):
     def fwd(a_block):
         b_block = jnp.negative(a_block)
