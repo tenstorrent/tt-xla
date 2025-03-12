103547bb087e646453daa11eea2ba53b2bb9920e
ctr-pmuruganTT <ctr-pmurugan@ext.tenstorrent.com>
2025-03-12 18:13:27 +0530

    Added tests for ceil, floor & clamp ops

--- tests/jax/ops/test_ceil.py
+++ tests/jax/ops/test_ceil.py
@@ -1,5 +1,23 @@
 # SPDX-FileCopyrightText: (c) 2024 Tenstorrent AI ULC
 #
 # SPDX-License-Identifier: Apache-2.0
+import jax
+import pytest
 
-# TODO add tests for `stablehlo.ceil`.
+from infra import run_op_test_with_random_inputs
+from tests.utils import Category
+
+
+@pytest.mark.push
+@pytest.mark.nightly
+@pytest.mark.record_test_properties(
+    category=Category.OP_TEST,
+    jax_op_name="jax.lax.ceil",
+    shlo_op_name="stablehlo.ceil",
+)
+@pytest.mark.parametrize("x_shape", [(32, 32), (64, 64)], ids=lambda val: f"{val}")
+def test_ceil(x_shape: tuple):
+    def ceil(x: jax.Array) -> jax.Array:
+        return jax.lax.ceil(x)
+
+    run_op_test_with_random_inputs(ceil, [x_shape], minval=-5.0, maxval=5.0)
--- tests/jax/ops/test_clamp.py
+++ tests/jax/ops/test_clamp.py
@@ -2,4 +2,30 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-# TODO add tests for `stablehlo.clamp`.
+import jax
+import pytest
+
+from infra import run_op_test_with_random_inputs
+from tests.utils import Category
+
+
+@pytest.mark.push
+@pytest.mark.nightly
+@pytest.mark.record_test_properties(
+    category=Category.OP_TEST,
+    jax_op_name="jax.lax.clamp",
+    shlo_op_name="stablehlo.clamp",
+)
+@pytest.mark.parametrize(
+    ["x_shape", "min_shape", "max_shape"],
+    [
+        [(32, 32), (32, 32), (32, 32)],
+        [(64, 64), (64, 64), (64, 64)],
+    ],
+    ids=lambda val: f"{val}",
+)
+def test_clamp(x_shape: tuple, min_shape: tuple, max_shape: tuple):
+    def clamp(x: jax.Array, min: jax.Array, max: jax.Array) -> jax.Array:
+        return jax.lax.clamp(min, x, max)
+
+    run_op_test_with_random_inputs(clamp, [x_shape, min_shape, max_shape])
