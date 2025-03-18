f6dab0bf53098e05d6d6cb76717c17f49724c9d9
ctr-pmuruganTT <ctr-pmurugan@ext.tenstorrent.com>
2025-03-18 13:05:47 +0000

    Add tests for LongT5

---
+++ tests/jax/models/longt5/large_tglobal/test_longt5_large_tglobal.py
@@ -0,0 +1,65 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+import pytest
+from infra import Framework, RunMode
+
+from tests.utils import (
+    BringupStatus,
+    Category,
+    ModelGroup,
+    ModelSource,
+    ModelTask,
+    build_model_name,
+    failed_ttmlir_compilation,
+)
+
+from ..tester import LongT5Tester
+
+MODEL_PATH = "google/long-t5-tglobal-large"
+MODEL_NAME = build_model_name(
+    Framework.JAX,
+    "longt5",
+    "large_tglobal",
+    ModelTask.NLP_TEXT_CLS,
+    ModelSource.HUGGING_FACE,
+)
+
+# ----- Fixtures -----
+
+
+@pytest.fixture
+def inference_tester() -> LongT5Tester:
+    return LongT5Tester(MODEL_PATH)
+
+
+@pytest.fixture
+def training_tester() -> LongT5Tester:
+    return LongT5Tester(MODEL_PATH, run_mode=RunMode.TRAINING)
+
+
+# ----- Tests -----
+
+
+@pytest.mark.model_test
+@pytest.mark.record_test_properties(
+    category=Category.MODEL_TEST,
+    model_name=MODEL_NAME,
+    model_group=ModelGroup.GENERALITY,
+    run_mode=RunMode.INFERENCE,
+    bringup_status=BringupStatus.FAILED_TTMLIR_COMPILATION,
+)
+def test_longt5_large_tglobal_inference(inference_tester: LongT5Tester):
+    inference_tester.test()
+
+
+@pytest.mark.nightly
+@pytest.mark.record_test_properties(
+    category=Category.MODEL_TEST,
+    model_name=MODEL_NAME,
+    model_group=ModelGroup.GENERALITY,
+    run_mode=RunMode.TRAINING,
+)
+@pytest.mark.skip(reason="Support for training not implemented")
+def test_longt5_large_tglobal_training(training_tester: LongT5Tester):
+    training_tester.test()
---
+++ tests/jax/models/longt5/tester.py
@@ -0,0 +1,54 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+from typing import Dict, Sequence
+
+import jax
+import jax.numpy as jnp
+from infra import ComparisonConfig, ModelTester, RunMode
+from transformers import (
+    AutoTokenizer,
+    FlaxLongT5ForConditionalGeneration,
+    FlaxPreTrainedModel,
+)
+
+
+class LongT5Tester(ModelTester):
+    """Tester for Long T5 model variants."""
+
+    def __init__(
+        self,
+        model_name: str,
+        comparison_config: ComparisonConfig = ComparisonConfig(),
+        run_mode: RunMode = RunMode.INFERENCE,
+    ) -> None:
+        self._model_name = model_name
+        super().__init__(comparison_config, run_mode)
+
+    # @override
+    def _get_model(self) -> FlaxPreTrainedModel:
+        return FlaxLongT5ForConditionalGeneration.from_pretrained(self._model_name)
+
+    # @override
+    def _get_input_activations(self) -> Sequence[jax.Array]:
+        tokenizer = AutoTokenizer.from_pretrained(self._model_name)
+        inputs = tokenizer("Hello, my dog is cute", return_tensors="np")
+        return inputs["input_ids"]
+
+    def _get_decode_input_ids(self) -> Sequence[jax.Array]:
+        model = self._get_model()
+        decoder_start_token_id = model.config.decoder_start_token_id
+        decoder_input_ids = (
+            jnp.ones((inputs.input_ids.shape[0], 1), dtype="i4")
+            * decoder_start_token_id
+        )
+        return decoder_input_ids
+
+    # @override
+    def _get_forward_method_kwargs(self) -> Dict[str, jax.Array]:
+        assert hasattr(self._model, "params")
+        return {
+            "params": self._model.params,
+            "input_ids": self._get_input_activations(),
+            "decoder_input_ids": self._get_decode_input_ids(),
+        }
