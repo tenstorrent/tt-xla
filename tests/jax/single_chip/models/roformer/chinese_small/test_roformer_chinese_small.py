7548a15b32447b927bab75c13003fc63d2b2774f
ctr-pmuruganTT <ctr-pmurugan@ext.tenstorrent.com>
2025-03-20 12:49:17 +0000

    Add tests for RoFormer model

---
+++ tests/jax/single_chip/models/roformer/chinese_small/test_roformer_chinese_small.py
@@ -0,0 +1,72 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import pytest
+from infra import Framework, RunMode
+
+from tests.utils import (
+    BringupStatus,
+    Category,
+    ModelGroup,
+    ModelSource,
+    ModelTask,
+    build_model_name,
+    failed_ttmlir_compilation,
+)
+
+from ..tester import RoFormerTester
+
+MODEL_PATH = "junnyu/roformer_chinese_small"
+MODEL_NAME = build_model_name(
+    Framework.JAX,
+    "roformer",
+    "chinese_small",
+    ModelTask.NLP_MASKED_LM,
+    ModelSource.HUGGING_FACE,
+)
+
+# ----- Fixtures -----
+
+
+@pytest.fixture
+def inference_tester() -> RoFormerTester:
+    return RoFormerTester(MODEL_PATH)
+
+
+@pytest.fixture
+def training_tester() -> RoFormerTester:
+    return RoFormerTester(MODEL_PATH, run_mode=RunMode.TRAINING)
+
+
+# ----- Tests -----
+
+
+@pytest.mark.model_test
+@pytest.mark.record_test_properties(
+    category=Category.MODEL_TEST,
+    model_name=MODEL_NAME,
+    model_group=ModelGroup.GENERALITY,
+    run_mode=RunMode.INFERENCE,
+    bringup_status=BringupStatus.FAILED_TTMLIR_COMPILATION,
+)
+@pytest.mark.skip(
+    reason=failed_ttmlir_compilation(
+        "failed to legalize operation 'ttir.gather' that was explicitly marked illegal "
+        "https://github.com/tenstorrent/tt-xla/issues/318"
+    )
+)
+def test_roformer_chinese_small_inference(inference_tester: RoFormerTester):
+    inference_tester.test()
+
+
+@pytest.mark.nightly
+@pytest.mark.record_test_properties(
+    category=Category.MODEL_TEST,
+    model_name=MODEL_NAME,
+    model_group=ModelGroup.GENERALITY,
+    run_mode=RunMode.TRAINING,
+)
+@pytest.mark.skip(reason="Support for training not implemented")
+def test_roformer_chinese_small_training(training_tester: RoFormerTester):
+    training_tester.test()
