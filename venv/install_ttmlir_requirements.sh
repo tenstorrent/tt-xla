bc24d5054d6b762536887299afaf3bf7beaea25f
Marko Rakita <mrakita@tenstorrent.com>
2025-03-22 12:37:32 +0100

    Tidying up md files (#384)
    ### Ticket
    /
    
    ### Problem description
    - `LICENSE` file was out of date
    - `CONTRIBUTING.md` had a few grammar mistakes fixed in tt-mlir project,
    aligning here
    - Env script `venv/install_ttmlir_requirements.sh` had a typo
    
    ### What's changed
    - Updated `LICENSE` file with notices for all dependencies and missing
    Appendix from Apache 2.0 License
    - Fixed grammar mistakes in `CONTRIBUTING.md`
    - Fixed typo in env script `venv/install_ttmlir_requirements.sh`
    
    ### Checklist
    - [x] New/Existing tests provide coverage for changes

--- venv/install_ttmlir_requirements.sh
+++ venv/install_ttmlir_requirements.sh
@@ -14,7 +14,7 @@ TT_MLIR_ENV_DIR=${TTPJRT_SOURCE_DIR}/third_party/tt-mlir/src/tt-mlir/env
 
 # Install llvm requirements
 LLVM_VERSION=$(grep -oP 'set\(LLVM_PROJECT_VERSION "\K[^"]+' ${TT_MLIR_ENV_DIR}/CMakeLists.txt)
-REQUIREMENTS_CACHE_DIR=${TTPJRT_SOURCE_DIR}/venv/bin/requirements_cashe
+REQUIREMENTS_CACHE_DIR=${TTPJRT_SOURCE_DIR}/venv/bin/requirements_cache
 LLVM_REQUIREMENTS_PATH=${REQUIREMENTS_CACHE_DIR}/llvm-requirements-${LLVM_VERSION}.txt
 if [ ! -e $LLVM_REQUIREMENTS_PATH ]; then
   mkdir -p $REQUIREMENTS_CACHE_DIR
