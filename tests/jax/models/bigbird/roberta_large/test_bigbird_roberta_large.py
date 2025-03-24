092b82f155dd5f27a0e0cd3a3486b734337a389a
ctr-pmuruganTT <ctr-pmurugan@ext.tenstorrent.com>
2025-03-24 07:34:13 +0000

    Add tests for Bigbird model

---
+++ tests/jax/models/bigbird/roberta_large/test_bigbird_roberta_large.py
@@ -0,0 +1,70 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import pytest
+from infra import Framework, RunMode
+
+from tests.utils import (
+    BringupStatus,
+    Category,
+    ModelGroup,
+    ModelSource,
+    ModelTask,
+    build_model_name,
+    incorrect_result,
+)
+
+from ..tester import BigBirdTester
+
+MODEL_PATH = "google/bigbird-roberta-large"
+MODEL_NAME = build_model_name(
+    Framework.JAX,
+    "bigbird",
+    "roberta_large",
+    ModelTask.NLP_CAUSAL_LM,
+    ModelSource.HUGGING_FACE,
+)
+
+# ----- Fixtures -----
+
+
+@pytest.fixture
+def inference_tester() -> BigBirdTester:
+    return BigBirdTester(MODEL_PATH)
+
+
+def training_tester() -> BigBirdTester:
+    return BigBirdTester(MODEL_PATH, run_mode=RunMode.TRAINING)
+
+
+# ----- Tests -----
+
+
+@pytest.mark.push
+@pytest.mark.record_test_properties(
+    category=Category.MODEL_TEST,
+    model_name=MODEL_NAME,
+    model_group=ModelGroup.GENERALITY,
+    run_mode=RunMode.INFERENCE,
+    bringup_status=BringupStatus.INCORRECT_RESULT,
+)
+@pytest.mark.xfail(
+    reason=incorrect_result(
+        "Atol comparison failed. Calculated: atol=131020.7265625. Required: atol=0.16."
+    )
+)
+def test_bigbird_roberta_large_inference(inference_tester: BigBirdTester):
+    inference_tester.test()
+
+
+@pytest.mark.nightly
+@pytest.mark.record_test_properties(
+    category=Category.MODEL_TEST,
+    model_name=MODEL_NAME,
+    model_group=ModelGroup.GENERALITY,
+    run_mode=RunMode.TRAINING,
+)
+@pytest.mark.skip(reason="Support for training not implemented")
+def test_bigbird_roberta_large_training(inference_tester: BigBirdTester):
+    training_tester.test()
