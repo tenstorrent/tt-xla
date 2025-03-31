f7ad425a05e2e6e2a48a1503c3c58544b1d61912
Prem Kumar M <ctr-pmurugan@ext.tenstorrent.com>
2025-03-31 15:09:11 +0530

    Add tests for Pegasus model (#431)
    ### Ticket
    closes #429 
    
    ### Problem description
    Add tests for 2 variants of Pegasus Model
    
    ### What's changed
    *  a tester class is added to test all 2 variants

---
+++ tests/jax/single_chip/models/pegasus/large/test_pegasus_large.py
@@ -0,0 +1,72 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import pytest
+from infra import Framework, RunMode
+
+from tests.utils import (
+    BringupStatus,
+    Category,
+    ModelGroup,
+    ModelSource,
+    ModelTask,
+    build_model_name,
+    failed_ttmlir_compilation,
+)
+
+from ..tester import PegasusTester
+
+MODEL_PATH = "google/pegasus-large"
+MODEL_NAME = build_model_name(
+    Framework.JAX,
+    "pegasus",
+    "large",
+    ModelTask.NLP_SUMMARIZATION,
+    ModelSource.HUGGING_FACE,
+)
+
+# ----- Fixtures -----
+
+
+@pytest.fixture
+def inference_tester() -> PegasusTester:
+    return PegasusTester(MODEL_PATH)
+
+
+@pytest.fixture
+def training_tester() -> PegasusTester:
+    return PegasusTester(MODEL_PATH, run_mode=RunMode.TRAINING)
+
+
+# ----- Tests -----
+
+
+@pytest.mark.model_test
+@pytest.mark.record_test_properties(
+    category=Category.MODEL_TEST,
+    model_name=MODEL_NAME,
+    model_group=ModelGroup.GENERALITY,
+    run_mode=RunMode.INFERENCE,
+    bringup_status=BringupStatus.FAILED_TTMLIR_COMPILATION,
+)
+@pytest.mark.xfail(
+    reason=failed_ttmlir_compilation(
+        "'ttir.scatter' op Dimension size to slice into must be 1 "
+        "https://github.com/tenstorrent/tt-xla/issues/386 "
+    )
+)
+def test_pegasus_large_inference(inference_tester: PegasusTester):
+    inference_tester.test()
+
+
+@pytest.mark.nightly
+@pytest.mark.record_test_properties(
+    category=Category.MODEL_TEST,
+    model_name=MODEL_NAME,
+    model_group=ModelGroup.GENERALITY,
+    run_mode=RunMode.TRAINING,
+)
+@pytest.mark.skip(reason="Support for training not implemented")
+def test_pegasus_large_training(training_tester: PegasusTester):
+    training_tester.test()
