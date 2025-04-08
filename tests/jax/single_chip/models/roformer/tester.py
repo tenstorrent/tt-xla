91d543c38d90223df73e2b7e50858da9ee89085c
Prem Kumar M <ctr-pmurugan@ext.tenstorrent.com>
2025-04-08 18:27:53 +0530

    Add tests for RoFormer model (#474)
    Closes https://github.com/tenstorrent/tt-xla/issues/380
    
    Add tests for Roformer model
    
    A tester class is added to test 3 variants of Roformer models

---
+++ tests/jax/single_chip/models/roformer/tester.py
@@ -0,0 +1,49 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from typing import Dict, Sequence
+
+import jax
+from infra import ComparisonConfig, ModelTester, RunMode
+from transformers import (
+    RoFormerTokenizer,
+    FlaxRoFormerForMaskedLM,
+    FlaxPreTrainedModel,
+)
+from jaxtyping import PyTree
+
+
+class RoFormerTester(ModelTester):
+    """Tester for RoFormer Models for language Modeling task."""
+
+    def __init__(
+        self,
+        model_name: str,
+        comparison_config: ComparisonConfig = ComparisonConfig(),
+        run_mode: RunMode = RunMode.INFERENCE,
+    ) -> None:
+        self._model_name = model_name
+        super().__init__(comparison_config, run_mode)
+
+    # @override
+    def _get_model(self) -> FlaxPreTrainedModel:
+        return FlaxRoFormerForMaskedLM.from_pretrained(self._model_name)
+
+    # @override
+    def _get_input_activations(self) -> Dict[str, jax.Array]:
+        tokenizer = RoFormerTokenizer.from_pretrained(self._model_name)
+        inputs = tokenizer("The capital of France is [MASK].", return_tensors="jax")
+        return inputs
+
+    # @override
+    def _get_forward_method_kwargs(self) -> Dict[str, PyTree]:
+        assert hasattr(self._model, "params")
+        return {
+            "params": self._model.params,
+            **self._get_input_activations(),
+        }
+
+    # @override
+    def _get_static_argnames(self):
+        return ["train"]
