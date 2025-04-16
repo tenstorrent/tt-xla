8dd9489ce5a0dc6022090e3165bceefec95d599d
Prem Kumar M <ctr-pmurugan@ext.tenstorrent.com>
2025-04-16 14:02:55 +0530

    skip pegasus xsum test for OOM issue (#512)
    ### Problem description
    OOM issue occured for `test_pegasus_xsum.py`
    
    ### What's changed
    updated the test with skip marker

--- tests/jax/single_chip/models/pegasus/xsum/test_pegasus_xsum.py
+++ tests/jax/single_chip/models/pegasus/xsum/test_pegasus_xsum.py
@@ -12,7 +12,7 @@ from tests.utils import (
     ModelSource,
     ModelTask,
     build_model_name,
-    failed_ttmlir_compilation,
+    failed_fe_compilation,
 )
 
 from ..tester import PegasusTester
@@ -48,12 +48,11 @@ def training_tester() -> PegasusTester:
     model_name=MODEL_NAME,
     model_group=ModelGroup.GENERALITY,
     run_mode=RunMode.INFERENCE,
-    bringup_status=BringupStatus.FAILED_TTMLIR_COMPILATION,
+    bringup_status=BringupStatus.FAILED_FE_COMPILATION,
 )
-@pytest.mark.xfail(
-    reason=failed_ttmlir_compilation(
-        "'ttir.scatter' op Dimension size to slice into must be 1 "
-        "https://github.com/tenstorrent/tt-xla/issues/386 "
+@pytest.mark.skip(
+    reason=failed_fe_compilation(
+        "OOMs in CI (https://github.com/tenstorrent/tt-xla/issues/186)"
     )
 )
 def test_pegasus_xsum_inference(inference_tester: PegasusTester):
