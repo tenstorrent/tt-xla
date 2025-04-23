df3991a9e9006e205dc06a1aba3d43bc30427e67
Prem Kumar M <ctr-pmurugan@ext.tenstorrent.com>
2025-04-23 17:39:25 +0530

    Add tests for Electra model (#530)
    ### Ticket
    closes #529 
    
    Tests are added for Electra model variants
    
     A tester class is added to test below variants
      * small-discriminator
      * large-discriminator 
      * base-discriminator
      * electra-base-generator

---
+++ tests/jax/single_chip/models/electra/tester.py
@@ -0,0 +1,51 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from typing import Dict
+
+import jax
+from infra import ComparisonConfig, ModelTester, RunMode
+from transformers import (
+    AutoTokenizer,
+    FlaxPreTrainedModel,
+    FlaxElectraForCausalLM,
+)
+from jaxtyping import PyTree
+
+
+class ElectraTester(ModelTester):
+    """Tester for Electra models for Causal LM task"""
+
+    def __init__(
+        self,
+        model_path: str,
+        comparison_config: ComparisonConfig = ComparisonConfig(),
+        run_mode: RunMode = RunMode.INFERENCE,
+    ) -> None:
+        self._model_path = model_path
+        super().__init__(comparison_config, run_mode)
+
+    # @override
+    def _get_model(self) -> FlaxPreTrainedModel:
+        return FlaxElectraForCausalLM.from_pretrained(self._model_path)
+
+    # @override
+    def _get_input_activations(self) -> Dict[str, jax.Array]:
+        tokenizer = AutoTokenizer.from_pretrained(self._model_path)
+        return tokenizer(
+            "Hello, my dog is cute",
+            return_tensors="jax",
+        )
+
+    # @override
+    def _get_forward_method_kwargs(self) -> Dict[str, PyTree]:
+        assert hasattr(self._model, "params")
+        return {
+            "params": self._model.params,
+            **self._get_input_activations(),
+        }
+
+    # @override
+    def _get_static_argnames(self):
+        return ["train"]
