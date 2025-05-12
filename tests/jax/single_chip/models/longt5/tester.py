581aa8787111ed60dc4e1ec5435b00e4cd7727fc
ctr-pmuruganTT <ctr-pmurugan@ext.tenstorrent.com>
2025-05-12 12:20:14 +0000

    Add tests for LongT5 model

---
+++ tests/jax/single_chip/models/longt5/tester.py
@@ -0,0 +1,57 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from typing import Dict
+
+import jax
+import jax.numpy as jnp
+from infra import ComparisonConfig, ModelTester, RunMode
+from transformers import (
+    AutoTokenizer,
+    FlaxLongT5ForConditionalGeneration,
+    FlaxPreTrainedModel,
+)
+from jaxtyping import PyTree
+
+
+class LongT5Tester(ModelTester):
+    """Tester for Long T5 model variants."""
+
+    def __init__(
+        self,
+        model_path: str,
+        comparison_config: ComparisonConfig = ComparisonConfig(),
+        run_mode: RunMode = RunMode.INFERENCE,
+    ) -> None:
+        self._model_path = model_path
+        super().__init__(comparison_config, run_mode)
+
+    # @override
+    def _get_model(self) -> FlaxPreTrainedModel:
+        return FlaxLongT5ForConditionalGeneration.from_pretrained(self._model_path)
+
+    # @override
+    def _get_input_activations(self) -> Dict[str, jax.Array]:
+        tokenizer = AutoTokenizer.from_pretrained(self._model_path)
+        inputs = tokenizer(
+            "My friends are cool but they eat too many carbs.", return_tensors="jax"
+        )
+        return inputs
+
+    # @override
+    def _get_forward_method_kwargs(self) -> Dict[str, PyTree]:
+        assert hasattr(self._model, "params")
+        tokenizer = AutoTokenizer.from_pretrained(self._model_path)
+        decoder_input_ids = tokenizer(
+            text_target="I eat less carbs.", return_tensors="jax"
+        ).input_ids
+        return {
+            "params": self._model.params,
+            "decoder_input_ids": decoder_input_ids,
+            **self._get_input_activations(),
+        }
+
+    # @override
+    def _get_static_argnames(self):
+        return ["train"]
