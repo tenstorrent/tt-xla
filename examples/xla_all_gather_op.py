256cba10ea76e696c79c6861c63d0e9c636827b7
Het Shah <hshah@tenstorrent.com>
2025-05-22 16:18:35 +0000

    cleanup all-gather example a bit

--- examples/xla_all_gather_op.py
+++ examples/xla_all_gather_op.py
@@ -31,7 +31,6 @@ xr.use_spmd()
 
 # Device mesh, this and partition spec as well as the input tensor shape define the individual shard shape.
 num_devices = xr.global_runtime_device_count()
-print("XR world size: ", xr.world_size())
 
 mesh_shape = (2, 4)
 device_ids = np.array(range(num_devices))
@@ -46,17 +45,17 @@ golden = t @ w
 t = t.to(xm.xla_device())
 w = w.to(xm.xla_device())
 
-# xs.mark_sharding(t, mesh, ("x", None))
 xs.mark_sharding(t, mesh, ("x", None))
 xs.mark_sharding(w, mesh, (None, None))
 
 # These cause the number of devices error
 # t = t + t
 # t = 1 * t
+# xm.mark_step()
 
 # Adding a new tensor, doesn't cause the error. But the all gather shape is still wrong.
-z = t + t
-xm.mark_step()
+# z = t + t
+# xm.mark_step()
 
 from torch_xla.distributed.spmd.debugging import visualize_tensor_sharding
 
@@ -65,23 +64,24 @@ visualize_tensor_sharding(t, use_color=False)
 print("Sharding of w:")
 visualize_tensor_sharding(w, use_color=False)
 
+y_local = t @ w
 
-print("Before all-gather, t shape: ", t.shape)
+# print("Before all-gather, t shape: ", t.shape)
 # spmd all-gather doesnt have an easily accessible API, so we use the internal API (for now)
-t = torch_xla._XLAC._xla_all_gather(t, 0, 2, [[0, 1, 2, 3], [4, 5, 6, 7]], True)
-# xm.mark_step()
-print("After all-gather, t shape: ", t.shape)
-print("After all-gather, w device: ", w.shape)
+y = torch_xla._XLAC._xla_all_gather(
+    y_local, 0, 2, [[0, 4], [1, 5], [2, 6], [3, 7]], True
+)
 
-y = t @ w
-xm.mark_step()
+
+# print("After all-gather, t shape: ", t.shape)
+# print("After all-gather, w device: ", w.shape)
 
 y = y.to("cpu")
 print(f"Y Shape: {y.shape}")
 print(f"Golden Shape: {golden.shape}")
 print(f"Y: {y}")
 print(f"Golden: {golden}")
-print(f"Is close: {torch.allclose(y, golden, atol=0.5)}")
+# print(f"Is close: {torch.allclose(y, golden, atol=0.5)}")
 t = t.to("cpu")
 print(f"t Shape: {t.shape}")
 # unique_rows, counts = torch.unique(t, dim=0, return_counts=True)
