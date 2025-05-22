204b0f88fab7cc105a23a279c4a57d1828234ac6
Het Shah <hshah@tenstorrent.com>
2025-05-22 21:07:55 +0000

    Add 2 separate versions of the all_gather example

--- examples/xla_all_gather_op_v1.py
+++ examples/xla_all_gather_op_v1.py
@@ -48,15 +48,6 @@ w = w.to(xm.xla_device())
 xs.mark_sharding(t, mesh, ("x", None))
 xs.mark_sharding(w, mesh, (None, None))
 
-# These cause the number of devices error
-# t = t + t
-# t = 1 * t
-# xm.mark_step()
-
-# Adding a new tensor, doesn't cause the error. But the all gather shape is still wrong.
-# z = t + t
-# xm.mark_step()
-
 from torch_xla.distributed.spmd.debugging import visualize_tensor_sharding
 
 print("Sharding of t:")
@@ -64,35 +55,17 @@ visualize_tensor_sharding(t, use_color=False)
 print("Sharding of w:")
 visualize_tensor_sharding(w, use_color=False)
 
+# In this version of the all gather, we perform the matmul first and then
+# all-gather the matmul result.
 y_local = t @ w
 
-# print("Before all-gather, t shape: ", t.shape)
-# spmd all-gather doesnt have an easily accessible API, so we use the internal API (for now)
+# Internal all-gather API
 y = torch_xla._XLAC._xla_all_gather(
     y_local, 0, 2, [[0, 4], [1, 5], [2, 6], [3, 7]], True
 )
-
-
-# print("After all-gather, t shape: ", t.shape)
-# print("After all-gather, w device: ", w.shape)
 
 y = y.to("cpu")
 print(f"Y Shape: {y.shape}")
 print(f"Golden Shape: {golden.shape}")
 print(f"Y: {y}")
 print(f"Golden: {golden}")
-# print(f"Is close: {torch.allclose(y, golden, atol=0.5)}")
-t = t.to("cpu")
-print(f"t Shape: {t.shape}")
-# unique_rows, counts = torch.unique(t, dim=0, return_counts=True)
-# duplicate_mask = counts > 1
-# duplicates = unique_rows[duplicate_mask]
-# zero_row_mask = (t == 0).all(dim=0)
-# zero_rows = t[zero_row_mask]
-# print(f"Zero rows shape: {zero_rows.shape}")
-# print(f"Zero rows:\n{zero_rows}")
-# print(f"Duplicate rows shape: {duplicates.shape}")
-# print(f"Duplicate rows from t:\n{duplicates}")
-# print(f"Unique rows shape from t:\n{unique_rows.shape}")
-# print(f"Unique rows from t:\n{unique_rows}")
-# assert torch.allclose(y, golden, atol=0.5)
