a90022b7f29f079377444ed7eb36360cb6ff59de
Stefan Gligorijevic <189116645+sgligorijevicTT@users.noreply.github.com>
2025-05-26 12:21:26 +0000

    Add a graph test for gelu

---
+++ tests/jax/single_chip/graphs/test_gelu.py
@@ -0,0 +1,29 @@
+# SPDX-FileCopyrightText: (c) 2024 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import jax
+import pytest
+from infra import run_graph_test_with_random_inputs
+from tests.utils import incorrect_result
+from tests.utils import Category
+
+import flax.linen as nn
+
+
+@pytest.mark.push
+@pytest.mark.nightly
+@pytest.mark.record_test_properties(category=Category.GRAPH_TEST)
+@pytest.mark.parametrize(
+    "x_shape",
+    [
+        (32, 32),
+        (64, 64),
+    ],
+)
+@pytest.mark.xfail(reason=incorrect_result("Bad accuracy"))
+def test_gelu(x_shape):
+    def gelu(x: jax.Array) -> jax.Array:
+        return nn.gelu(x, approximate=False)
+
+    run_graph_test_with_random_inputs(gelu, [x_shape])
