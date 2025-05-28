1fdbd24b0c9ab74222a9dbf1598a1c37d2b57b79
Marko Rakita <mrakita@tenstorrent.com>
2025-05-28 12:12:54 +0000

    Add MNIST MLP tensor parallel model implementation and tests

--- tests/jax/multi_chip/n300/models/tensor_parallel/mnist_mlp/test_mnist_mlp.py
+++ tests/jax/multi_chip/n300/models/tensor_parallel/mnist_mlp/test_mnist_mlp.py
@@ -4,6 +4,7 @@
 
 import pytest
 from infra import Framework, RunMode
+from infra.multichip_utils import enable_shardy
 
 from tests.utils import (
     BringupStatus,
@@ -14,12 +15,12 @@ from tests.utils import (
     build_model_name,
 )
 
-from .tester import MNISTMLPTester
+from .tester import MnistMLPMultichipTester
 
 MODEL_NAME = build_model_name(
     Framework.JAX,
     "mnist",
-    "mlp",
+    "mlp_multichip_n300",
     ModelTask.CV_IMAGE_CLS,
     ModelSource.CUSTOM,
 )
@@ -29,16 +30,32 @@ MODEL_NAME = build_model_name(
 
 
 @pytest.fixture
-def inference_tester(request) -> MNISTMLPTester:
-    return MNISTMLPTester(request.param)
+def inference_tester(request) -> MnistMLPMultichipTester:
+    return MnistMLPMultichipTester(request.param, run_mode=RunMode.INFERENCE)
 
 
 @pytest.fixture
-def training_tester(request) -> MNISTMLPTester:
-    return MNISTMLPTester(request.param, run_mode=RunMode.TRAINING)
+def training_tester(request) -> MnistMLPMultichipTester:
+    return MnistMLPMultichipTester(request.param, run_mode=RunMode.TRAINING)
 
 
 # ----- Tests -----
+
+
+@pytest.mark.push
+@pytest.mark.model_test
+@pytest.mark.record_test_properties(
+    category=Category.MODEL_TEST,
+    model_name=MODEL_NAME,
+    model_group=ModelGroup.GENERALITY,
+    run_mode=RunMode.INFERENCE,
+    bringup_status=BringupStatus.FAILED_RUNTIME,
+)
+@pytest.mark.parametrize(
+    "inference_tester", [(1024, 512, 256)], indirect=True, ids=lambda val: f"{val}"
+)
+def test_mnist_mlp_multichip_n300_inference(inference_tester: MnistMLPMultichipTester):
+    inference_tester.test()
 
 
 @pytest.mark.push
@@ -50,35 +67,13 @@ def training_tester(request) -> MNISTMLPTester:
     run_mode=RunMode.INFERENCE,
 )
 @pytest.mark.parametrize(
-    "inference_tester",
-    [
-        (128,),
-        (128, 128),
-        (192, 128),
-        (512, 512),
-        (128, 128, 128),
-    ],
-    indirect=True,
-    ids=lambda val: f"{val}",
+    "inference_tester", [(1024, 512, 256)], indirect=True, ids=lambda val: f"{val}"
 )
-def test_mnist_mlp_inference_nightly(inference_tester: MNISTMLPTester):
-    inference_tester.test()
-
-
-@pytest.mark.push
-@pytest.mark.model_test
-@pytest.mark.record_test_properties(
-    category=Category.MODEL_TEST,
-    model_name=MODEL_NAME,
-    model_group=ModelGroup.GENERALITY,
-    run_mode=RunMode.INFERENCE,
-    bringup_status=BringupStatus.PASSED,
-)
-@pytest.mark.parametrize(
-    "inference_tester", [(256, 128, 64)], indirect=True, ids=lambda val: f"{val}"
-)
-def test_mnist_mlp_inference(inference_tester: MNISTMLPTester):
-    inference_tester.test()
+def test_mnist_mlp_multichip_n300_inference_shardy(
+    inference_tester: MnistMLPMultichipTester,
+):
+    with enable_shardy(True):
+        inference_tester.test()
 
 
 @pytest.mark.push
@@ -90,5 +85,5 @@ def test_mnist_mlp_inference(inference_tester: MNISTMLPTester):
     run_mode=RunMode.TRAINING,
 )
 @pytest.mark.skip(reason="Support for training not implemented")
-def test_mnist_mlp_training(training_tester: MNISTMLPTester):
+def test_mnist_mlp_multichip_n300_training(training_tester: MnistMLPMultichipTester):
     training_tester.test()
