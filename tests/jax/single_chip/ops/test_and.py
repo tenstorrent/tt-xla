f418626c26e87a78d0a617884a0e72bac7ea1f3c
Kristijan Mitrovic <kmitrovic@tenstorrent.com>
2025-06-03 08:29:46 +0200

    Added missing tests for atan2, reverse, bitwise and/or/not/xor ops (#499)
    All of them were successfully added some time ago in tt-mlir, some were
    blocked by issues, now that they all work I am adding torch tests.
    
    Fixes https://github.com/tenstorrent/tt-xla/issues/61

--- tests/jax/single_chip/ops/test_and.py
+++ tests/jax/single_chip/ops/test_and.py
@@ -30,6 +30,31 @@ def test_logical_and(shape: tuple):
     def logical_and(a: jax.Array, b: jax.Array) -> jax.Array:
         return jnp.logical_and(a, b)
 
+    # TODO change int32 to bool https://github.com/tenstorrent/tt-xla/issues/339
     lhs = random_tensor(shape, jnp.int32, minval=0, maxval=2, random_seed=3)
     rhs = random_tensor(shape, jnp.int32, minval=0, maxval=2, random_seed=6)
     run_op_test(logical_and, [lhs, rhs])
+
+
+@pytest.mark.push
+@pytest.mark.nightly
+@pytest.mark.record_test_properties(
+    category=Category.OP_TEST,
+    jax_op_name="jax.numpy.bitwise_and",
+    shlo_op_name="stablehlo.and{BITWISE}",
+)
+@pytest.mark.parametrize(
+    ["shape"],
+    [
+        [(32, 32)],
+        [(64, 64)],
+    ],
+    ids=lambda val: f"{val}",
+)
+def test_bitwise_and(shape: tuple):
+    def bitwise_and(a: jax.Array, b: jax.Array) -> jax.Array:
+        return jnp.bitwise_and(a, b)
+
+    lhs = random_tensor(shape, jnp.int32, minval=0, maxval=10, random_seed=3)
+    rhs = random_tensor(shape, jnp.int32, minval=0, maxval=10, random_seed=6)
+    run_op_test(bitwise_and, [lhs, rhs])
