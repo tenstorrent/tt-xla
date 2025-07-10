36bf52a995ac8e831856601314f71a234c4d38c6
Stefan Gligorijevic <189116645+sgligorijevicTT@users.noreply.github.com>
2025-07-10 11:08:36 +0000

    Fix Roformer tests

--- tests/jax/single_chip/models/roformer/chinese_base/test_roformer_chinese_base.py
+++ tests/jax/single_chip/models/roformer/chinese_base/test_roformer_chinese_base.py
@@ -11,7 +11,6 @@ from utils import (
     ModelSource,
     ModelTask,
     build_model_name,
-    incorrect_result,
 )
 
 from ..tester import RoFormerTester
@@ -47,13 +46,7 @@ def training_tester() -> RoFormerTester:
     model_name=MODEL_NAME,
     model_group=ModelGroup.GENERALITY,
     run_mode=RunMode.INFERENCE,
-    bringup_status=BringupStatus.INCORRECT_RESULT,
-)
-@pytest.mark.xfail(
-    reason=incorrect_result(
-        "Atol comparison failed. Calculated: atol=131029.59375. Required: atol=0.16 "
-        "https://github.com/tenstorrent/tt-xla/issues/379"
-    )
+    bringup_status=BringupStatus.PASSED,
 )
 def test_roformer_chinese_base_inference(inference_tester: RoFormerTester):
     inference_tester.test()
