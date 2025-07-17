049fe312b19034248ee6fd7d26b2eb07b2253a6b
ajakovljevicTT <ajakovljevic@tenstorrent.com>
2025-07-17 07:04:12 +0000

    Add all reduce tests

---
+++ tests/jax/multi_chip/llmbox/8_devices/ops/data_parallel/batch_sharded/test_all_reduce.py
@@ -0,0 +1,240 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import jax
+import pytest
+from infra import (
+    ShardingMode,
+    make_partition_spec,
+    run_jax_multichip_graph_test_with_random_inputs,
+)
+from utils import failed_fe_compilation
+
+
+@pytest.mark.nightly
+@pytest.mark.push
+@pytest.mark.parametrize(
+    "use_shardy",
+    [
+        True,
+        False,
+    ],
+)
+@pytest.mark.parametrize(
+    ("x_shape", "mesh_shape", "axis_names"),
+    [
+        ((8192, 784), (1, 8), ("batch", "model")),
+        ((8192, 784), (2, 4), ("batch", "model")),
+    ],
+)
+@pytest.mark.parametrize(
+    "sharding_mode",
+    [
+        ShardingMode.INPUTS_AND_MODULE,
+        pytest.param(
+            ShardingMode.MODULE,
+            marks=pytest.mark.xfail(
+                reason=failed_fe_compilation(
+                    "Cannot get sharding information through the protobuf "
+                    "(https://github.com/tenstorrent/tt-xla/issues/277)"
+                )
+            ),
+        ),
+    ],
+)
+def test_psum(
+    use_shardy: bool,
+    x_shape: tuple,
+    mesh_shape: tuple,
+    axis_names: tuple,
+    sharding_mode: ShardingMode,
+):
+    def fwd(batch):
+        act = jax.lax.psum(batch, axis_names[1])
+        return act
+
+    in_specs = (make_partition_spec(axis_names),)
+    out_specs = make_partition_spec(axis_names)
+
+    run_jax_multichip_graph_test_with_random_inputs(
+        fwd,
+        [x_shape],
+        mesh_shape,
+        axis_names,
+        in_specs,
+        out_specs,
+        use_shardy,
+        sharding_mode,
+    )
+
+
+@pytest.mark.nightly
+@pytest.mark.push
+@pytest.mark.parametrize(
+    "use_shardy",
+    [
+        True,
+        False,
+    ],
+)
+@pytest.mark.parametrize(
+    ("x_shape", "mesh_shape", "axis_names"),
+    [
+        ((8192, 784), (1, 8), ("batch", "model")),
+        ((8192, 784), (2, 4), ("batch", "model")),
+    ],
+)
+@pytest.mark.parametrize(
+    "sharding_mode",
+    [
+        ShardingMode.INPUTS_AND_MODULE,
+        pytest.param(
+            ShardingMode.MODULE,
+            marks=pytest.mark.xfail(
+                reason=failed_fe_compilation(
+                    "Cannot get sharding information through the protobuf "
+                    "(https://github.com/tenstorrent/tt-xla/issues/277)"
+                )
+            ),
+        ),
+    ],
+)
+def test_all_reduce_sum(
+    use_shardy: bool,
+    x_shape: tuple,
+    mesh_shape: tuple,
+    axis_names: tuple,
+    sharding_mode: ShardingMode,
+):
+    def fwd(batch):
+        act = jax.lax.all_reduce(batch, "sum", axis_names[1])
+        return act
+
+    in_specs = (make_partition_spec(axis_names),)
+    out_specs = make_partition_spec(axis_names)
+
+    run_jax_multichip_graph_test_with_random_inputs(
+        fwd,
+        [x_shape],
+        mesh_shape,
+        axis_names,
+        in_specs,
+        out_specs,
+        use_shardy,
+        sharding_mode,
+    )
+
+
+@pytest.mark.nightly
+@pytest.mark.push
+@pytest.mark.parametrize(
+    "use_shardy",
+    [
+        True,
+        False,
+    ],
+)
+@pytest.mark.parametrize(
+    ("x_shape", "mesh_shape", "axis_names"),
+    [
+        ((8192, 784), (1, 8), ("batch", "model")),
+        ((8192, 784), (2, 4), ("batch", "model")),
+    ],
+)
+@pytest.mark.parametrize(
+    "sharding_mode",
+    [
+        ShardingMode.INPUTS_AND_MODULE,
+        pytest.param(
+            ShardingMode.MODULE,
+            marks=pytest.mark.xfail(
+                reason=failed_fe_compilation(
+                    "Cannot get sharding information through the protobuf "
+                    "(https://github.com/tenstorrent/tt-xla/issues/277)"
+                )
+            ),
+        ),
+    ],
+)
+def test_all_reduce_max(
+    use_shardy: bool,
+    x_shape: tuple,
+    mesh_shape: tuple,
+    axis_names: tuple,
+    sharding_mode: ShardingMode,
+):
+    def fwd(batch):
+        act = jax.lax.all_reduce(batch, "max", axis_names[1])
+        return act
+
+    in_specs = (make_partition_spec(axis_names),)
+    out_specs = make_partition_spec(axis_names)
+
+    run_jax_multichip_graph_test_with_random_inputs(
+        fwd,
+        [x_shape],
+        mesh_shape,
+        axis_names,
+        in_specs,
+        out_specs,
+        use_shardy,
+        sharding_mode,
+    )
+
+
+@pytest.mark.nightly
+@pytest.mark.push
+@pytest.mark.parametrize(
+    "use_shardy",
+    [
+        True,
+        False,
+    ],
+)
+@pytest.mark.parametrize(
+    ("x_shape", "mesh_shape", "axis_names"),
+    [
+        ((8192, 784), (1, 8), ("batch", "model")),
+        ((8192, 784), (2, 4), ("batch", "model")),
+    ],
+)
+@pytest.mark.parametrize(
+    "sharding_mode",
+    [
+        ShardingMode.INPUTS_AND_MODULE,
+        pytest.param(
+            ShardingMode.MODULE,
+            marks=pytest.mark.xfail(
+                reason=failed_fe_compilation(
+                    "Cannot get sharding information through the protobuf "
+                    "(https://github.com/tenstorrent/tt-xla/issues/277)"
+                )
+            ),
+        ),
+    ],
+)
+def test_all_reduce_min(
+    use_shardy: bool,
+    x_shape: tuple,
+    mesh_shape: tuple,
+    axis_names: tuple,
+    sharding_mode: ShardingMode,
+):
+    def fwd(batch):
+        act = jax.lax.all_reduce(batch, "min", axis_names[1])
+        return act
+
+    in_specs = (make_partition_spec(axis_names),)
+    out_specs = make_partition_spec(axis_names)
+
+    run_jax_multichip_graph_test_with_random_inputs(
+        fwd,
+        [x_shape],
+        mesh_shape,
+        axis_names,
+        in_specs,
+        out_specs,
+        use_shardy,
+        sharding_mode,
+    )
