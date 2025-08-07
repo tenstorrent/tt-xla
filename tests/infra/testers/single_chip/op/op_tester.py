66dda051e743ac14ec20b2cfb4925039bd566456
Vladimir Zeljkovic <vzeljkovic@tenstorrent.com>
2025-08-07 13:13:38 +0000

    Refactor Multichip-Testers (Model and Ops) and Singlechip-Testers (Ops)
    Refactor Multichip-Testers (Model and Ops) and Singlechip-Testers (Ops)

--- tests/infra/testers/single_chip/op/op_tester.py
+++ tests/infra/testers/single_chip/op/op_tester.py
@@ -18,8 +18,6 @@ from ...base_tester import BaseTester
 class OpTester(BaseTester):
     """Specific single chip tester for ops."""
 
-    # -------------------- Public methods --------------------
-
     def test(self, workload: Workload) -> None:
         """
         Runs test by running `workload` on TT device and CPU and comparing the results.
@@ -30,33 +28,6 @@ class OpTester(BaseTester):
         cpu_res = self._run_on_cpu(compiled_workload)
 
         self._compare(tt_res, cpu_res)
-
-    def test_with_random_inputs(
-        self,
-        f: Callable,
-        input_shapes: Sequence[tuple],
-        minval: float = 0.0,
-        maxval: float = 1.0,
-    ) -> None:
-        """
-        Tests `f` by running it with random inputs in range [`minval`, `maxval`) on
-        TT device and CPU and comparing the results.
-        """
-        inputs = [
-            random_tensor(
-                shape,
-                minval=minval,
-                maxval=maxval,
-                framework=self._framework,
-            )
-            for shape in input_shapes
-        ]
-        workload = WorkloadFactory.create_workload(
-            self._framework, executable=f, args=inputs
-        )
-        self.test(workload)
-
-    # -------------------- Private methods --------------------
 
     # @override
     def _compile(self, workload: Workload) -> Workload:
@@ -83,6 +54,31 @@ class OpTester(BaseTester):
         else:
             assert isinstance(workload, TorchWorkload)
             return compile_torch_workload(workload)
+
+    def test_with_random_inputs(
+        self,
+        f: Callable,
+        input_shapes: Sequence[tuple],
+        minval: float = 0.0,
+        maxval: float = 1.0,
+    ) -> None:
+        """
+        Tests `f` by running it with random inputs in range [`minval`, `maxval`) on
+        TT device and CPU and comparing the results.
+        """
+        inputs = [
+            random_tensor(
+                shape,
+                minval=minval,
+                maxval=maxval,
+                framework=self._framework,
+            )
+            for shape in input_shapes
+        ]
+        workload = WorkloadFactory.create_workload(
+            self._framework, executable=f, args=inputs
+        )
+        self.test(workload)
 
 
 def run_op_test(
