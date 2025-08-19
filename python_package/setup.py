c18ad2af78335f2212eca5cf4372f97ed23de34b
Vladimir Zeljkovic <vzeljkovic@tenstorrent.com>
2025-08-19 15:41:31 +0000

    Update

--- python_package/setup.py
+++ python_package/setup.py
@@ -359,7 +359,7 @@ setup(
         f"jax_plugins.pjrt_plugin_tt": "jax_plugins/pjrt_plugin_tt",
         "ttxla_tools": os.path.join("..", "ttxla_tools"),
     },
-    python_requires=">=3.10, <3.11",
+    python_requires=">=3.11, <3.12",
     url="https://github.com/tenstorrent/tt-xla",
     version=config.version,
     # Needs to reference embedded shared libraries (i.e. .so file), so not zip safe.
