7147d57d702df649a3ec5c25d14ed843713f54d0
Prem Kumar M <ctr-pmurugan@ext.tenstorrent.com>
2025-08-20 18:22:48 +0530

    update torch models to use ModelVariant (#1053)
    ### Problem description
    Some of torch tests have failed because of recent changes in `tt-forge-models` to use ModelVariant
    
    ### What's changed
    Updated the below tests to use ModelVariant from tt-forge-models
    * alexnet
    * mobilenet_v1
    * mobilenet_v2

--- tests/torch/single_chip/models/mobilenetv2/tester.py
+++ tests/torch/single_chip/models/mobilenetv2/tester.py
@@ -4,7 +4,7 @@
 
 from typing import Any, Dict, Sequence
 from infra import ComparisonConfig, Model, RunMode, TorchModelTester
-from third_party.tt_forge_models.mobilenetv2.pytorch import ModelLoader
+from third_party.tt_forge_models.mobilenetv2.pytorch import ModelLoader, ModelVariant
 
 
 class MobileNetV2Tester(TorchModelTester):
@@ -12,7 +12,7 @@ class MobileNetV2Tester(TorchModelTester):
 
     def __init__(
         self,
-        variant_name: str,
+        variant_name: ModelVariant,
         comparison_config: ComparisonConfig = ComparisonConfig(),
         run_mode: RunMode = RunMode.INFERENCE,
     ) -> None:
