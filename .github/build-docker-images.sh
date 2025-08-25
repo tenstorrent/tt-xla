1cc463c4adbf79c2141963072f2988d3ab13e3b0
Vladimir Vukoman <vvukoman@tenstorrent.com>
2025-08-25 07:35:52 +0200

    Add a Docker IRD image (#1100)
    ### Ticket
    /
    
    ### Problem description
    TT-XLA doesn't have an IRD Docker image variant.
    
    ### What's changed
    Added building of the IRD and base IRD Docker images with the other
    Docker image builds.
    The `build-image` job now builds the IRD and base IRD Docker images
    along the other, already present, variants.
    
    ### Checklist
    - [ ] New/Existing tests provide coverage for changes

--- .github/build-docker-images.sh
+++ .github/build-docker-images.sh
@@ -8,6 +8,8 @@ set -e
 REPO=tenstorrent/tt-xla
 BASE_IMAGE_NAME=ghcr.io/$REPO/tt-xla-base-ubuntu-22-04
 CI_IMAGE_NAME=ghcr.io/$REPO/tt-xla-ci-ubuntu-22-04
+BASE_IRD_IMAGE_NAME=ghcr.io/$REPO/tt-xla-base-ird-ubuntu-22-04
+IRD_IMAGE_NAME=ghcr.io/$REPO/tt-xla-ird-ubuntu-22-04
 
 # Compute the hash of the Dockerfile
 DOCKER_TAG=$(./.github/get-docker-tag.sh)
@@ -20,7 +22,7 @@ build_and_push() {
     local image_name=$1
     local dockerfile=$2
     local on_main=$3
-
+    local from_image=$4
 
     if docker manifest inspect $image_name:$DOCKER_TAG > /dev/null; then
         echo "Image $image_name:$DOCKER_TAG already exists"
@@ -32,6 +34,7 @@ build_and_push() {
         docker build \
             --progress=plain \
             --build-arg FROM_TAG=$DOCKER_TAG \
+            ${from_image:+--build-arg FROM_IMAGE=$from_image} \
             -t $image_name:$DOCKER_TAG \
             -t $image_name:latest \
             -f $dockerfile .
@@ -48,6 +51,8 @@ build_and_push() {
 
 build_and_push $BASE_IMAGE_NAME .github/Dockerfile.base $ON_MAIN
 build_and_push $CI_IMAGE_NAME .github/Dockerfile.ci $ON_MAIN
+build_and_push $BASE_IRD_IMAGE_NAME .github/Dockerfile.ird $ON_MAIN base
+build_and_push $IRD_IMAGE_NAME .github/Dockerfile.ird $ON_MAIN ci
 
 echo "All images built and pushed successfully"
 echo "CI_IMAGE_NAME:"
