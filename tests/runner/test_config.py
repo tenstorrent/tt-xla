687d047939836e01fe3b4c8a104b23e594170a88
Kyle Mabee <kmabee@tenstorrent.com>
2025-08-26 04:47:58 +0000

    Update testlist examples

--- tests/runner/test_config.py
+++ tests/runner/test_config.py
@@ -7,17 +7,17 @@ from tests.utils import BringupStatus
 
 
 test_config = {
-    # Passing Test
+    # [x] Passing Test
     "mnist/pytorch-full-inference": {
         "status": ModelStatus.EXPECTED_PASSING,
         "marker": "tests_to_run",
     },
-    # PCC Failing test
+    # [x] PCC Failing test
     "openpose/v2/pytorch-full-inference": {
         "assert_pcc": True,
         "status": ModelStatus.EXPECTED_PASSING,
     },
-    # XFAIL pcc failing test
+    # [x]XFAIL pcc failing test
     "mobilenetv2/pytorch-mobilenet_v2-full-inference": {
         "required_pcc": 1.00,
         "status": ModelStatus.KNOWN_FAILURE_XFAIL,
@@ -43,6 +43,13 @@ test_config = {
         "bringup_status": BringupStatus.UNKNOWN,
         "marker": "tests_to_run",
     },
+    # Skip test
+    "segformer/pytorch-mit_b0-full-inference": {
+        "status": ModelStatus.NOT_SUPPORTED_SKIP,
+        "reason": "Doesn't fit on device",
+        "bringup_status": BringupStatus.FAILED_FE_COMPILATION,
+        "marker": "tests_to_run",
+    },
     # Another passing test
     "wide_resnet/pytorch-wide_resnet50_2-full-inference": {
         "required_pcc": 0.98,
@@ -50,10 +57,12 @@ test_config = {
         "marker": "tests_to_run",
     }
     # tests/runner/test_models.py::test_all_models[mnist/pytorch-full-inference]
-    # tests/runner/test_models.py::test_all_models[mobilenetv1/pytorch-mobilenet_v1-full-inference]
+    # tests/runner/test_models.py::test_all_models[openpose/v2/pytorch-full-inference]
     # tests/runner/test_models.py::test_all_models[mobilenetv2/pytorch-mobilenet_v2-full-inference]
     # tests/runner/test_models.py::test_all_models[monodepth2/pytorch-mono+stereo_640x192-full-inference]
     # tests/runner/test_models.py::test_all_models[vgg/pytorch-vgg11-full-inference]
+    # tests/runner/test_models.py::test_all_models[deepseek/pytorch-full-inference]
+    # tests/runner/test_models.py::test_all_models[segformer/pytorch-mit_b0-full-inference]
     # tests/runner/test_models.py::test_all_models[wide_resnet/pytorch-wide_resnet50_2-full-inference]
     # # Unknown tests
     # tests/runner/test_models.py::test_all_models[bi_lstm_crf/pytorch-full-inference] # Fails
