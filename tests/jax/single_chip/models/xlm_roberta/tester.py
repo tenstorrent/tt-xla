b8bee8b26a4ea183130c643a43750ef4feca0325
ctr-pmuruganTT <ctr-pmurugan@ext.tenstorrent.com>
2025-08-29 12:53:52 +0000

    Update JAX tests of Llama and XLM RoBERTa

--- tests/jax/single_chip/models/xlm_roberta/tester.py
+++ tests/jax/single_chip/models/xlm_roberta/tester.py
@@ -5,9 +5,11 @@
 from typing import Dict
 
 import jax
-from infra import ComparisonConfig, JaxModelTester, RunMode
-from jaxtyping import PyTree
-from transformers import AutoTokenizer, FlaxPreTrainedModel, FlaxXLMRobertaForCausalLM
+from infra import ComparisonConfig, JaxModelTester, RunMode, Model
+from third_party.tt_forge_models.xlm_roberta.causal_lm.jax import (
+    ModelLoader,
+    ModelVariant,
+)
 
 
 class XLMRobertaTester(JaxModelTester):
@@ -15,19 +17,17 @@ class XLMRobertaTester(JaxModelTester):
 
     def __init__(
         self,
-        model_path: str,
+        variant_name: ModelVariant,
         comparison_config: ComparisonConfig = ComparisonConfig(),
         run_mode: RunMode = RunMode.INFERENCE,
     ) -> None:
-        self._model_path = model_path
+        self._model_loader = ModelLoader(variant_name)
         super().__init__(comparison_config, run_mode)
 
     # @override
-    def _get_model(self) -> FlaxPreTrainedModel:
-        return FlaxXLMRobertaForCausalLM.from_pretrained(self._model_path)
+    def _get_model(self) -> Model:
+        return self._model_loader.load_model()
 
     # @override
     def _get_input_activations(self) -> Dict[str, jax.Array]:
-        tokenizer = AutoTokenizer.from_pretrained(self._model_path)
-        inputs = tokenizer("Hello, my dog is cute", return_tensors="jax")
-        return inputs
+        return self._model_loader.load_inputs()
