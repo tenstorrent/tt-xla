e830acdbf35fd8b9cf58126f28013f5f38487cc8
ctr-lelanchelian <ctr-lelanchelian@example.com>
2025-09-03 10:20:25 +0000

    add jax test for alexnet and vision text dual encoder model

--- tests/jax/single_chip/models/visiontextdualencoder/test_visiontextdualencoder.py
+++ tests/jax/single_chip/models/visiontextdualencoder/test_visiontextdualencoder.py
@@ -3,8 +3,8 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import pytest
-from infra import Framework, RunMode
-from utils import (
+from tests.infra import Framework, RunMode
+from tests.utils import (
     BringupStatus,
     Category,
     ModelGroup,
@@ -15,13 +15,16 @@ from utils import (
 )
 
 from .tester import VisionTextDualEncoderTester
+from third_party.tt_forge_models.vision_text_dual_encoder.mm_image_ttt.jax import (
+    ModelVariant,
+)
 
-IMAGE_MODEL_PATH = "google/vit-base-patch16-224"
-TEXT_MODEL_PATH = "google-bert/bert-base-uncased"
+VARIANT = ModelVariant.BASE
+
 MODEL_NAME = build_model_name(
     Framework.JAX,
     "vision_text_dual_encoder",
-    "vit_base_patch16_224_bert_base",
+    "base",
     ModelTask.MM_IMAGE_TTT,
     ModelSource.HUGGING_FACE,
 )
@@ -31,14 +34,12 @@ MODEL_NAME = build_model_name(
 
 @pytest.fixture
 def inference_tester() -> VisionTextDualEncoderTester:
-    return VisionTextDualEncoderTester(IMAGE_MODEL_PATH, TEXT_MODEL_PATH)
+    return VisionTextDualEncoderTester(VARIANT)
 
 
 @pytest.fixture
 def training_tester() -> VisionTextDualEncoderTester:
-    return VisionTextDualEncoderTester(
-        IMAGE_MODEL_PATH, TEXT_MODEL_PATH, RunMode.TRAINING
-    )
+    return VisionTextDualEncoderTester(VARIANT, run_mode=RunMode.TRAINING)
 
 
 # ----- Tests -----
@@ -54,7 +55,7 @@ def training_tester() -> VisionTextDualEncoderTester:
 )
 @pytest.mark.xfail(
     reason=incorrect_result(
-        "Atol comparison failed. Calculated: atol=6632.052734375. Required: atol=0.16. "
+        "Atol comparison failed. Calculated: atol=nan. Required: atol=0.99. "
         "https://github.com/tenstorrent/tt-xla/issues/379"
     )
 )
