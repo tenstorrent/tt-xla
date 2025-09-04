da3279d691076b997d44323fd2844035897d5309
Nikola Drakulic <ndrakulic@tenstorrent.com>
2025-09-04 12:31:43 +0000

    Jax Training infra

--- tests/jax/single_chip/models/gpt2/base/test_gpt2_base.py
+++ tests/jax/single_chip/models/gpt2/base/test_gpt2_base.py
@@ -7,6 +7,7 @@ from infra import Framework, RunMode
 from utils import (
     BringupStatus,
     Category,
+    ExecutionPass,
     ModelGroup,
     ModelSource,
     ModelTask,
@@ -60,7 +61,9 @@ def test_gpt2_base_inference(inference_tester: GPT2Tester):
     model_name=MODEL_NAME,
     model_group=ModelGroup.GENERALITY,
     run_mode=RunMode.TRAINING,
+    bringup_status=BringupStatus.FAILED_TTMLIR_COMPILATION,
+    execution_pass=ExecutionPass.BACKWARD,
 )
-@pytest.mark.skip(reason="Support for training not implemented")
+@pytest.mark.xfail(reason="error: failed to legalize operation 'ttir.scatter'")
 def test_gpt2_base_training(training_tester: GPT2Tester):
     training_tester.test()
