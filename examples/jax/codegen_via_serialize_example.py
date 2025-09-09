de8a4789952055d4f7f0ed972cc37dd6a476ff2d
Stefan Gligorijevic <189116645+sgligorijevicTT@users.noreply.github.com>
2025-09-09 08:58:03 +0000

    Examples of codegen via serialization

---
+++ examples/jax/codegen_via_serialize_example.py
@@ -0,0 +1,50 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import jax
+import jax.numpy as jnp
+from tt_jax import serialize_compiled_artifacts_to_disk
+import tt_alchemist
+import flax.nnx as nnx
+
+
+class Model(nnx.Module):
+    def __init__(self, *, rngs: nnx.Rngs):
+        self.A = nnx.Linear(32, 128, rngs=rngs)
+        self.B = nnx.Linear(128, 64, rngs=rngs)
+
+    def __call__(self, x):
+        x = self.A(x)
+        x = nnx.silu(x)
+        x = self.B(x)
+        x = nnx.tanh(x)
+        return jnp.sum(x**2)
+
+
+with jax.default_device(jax.devices("cpu")[0]):
+    model = Model(rngs=nnx.Rngs(0))
+    key = jax.random.key(1)
+    x = jax.random.normal(key, (32, 32))
+    graphdef, state = nnx.split(model)
+
+
+def forward(graphdef, state, x):
+    model = nnx.merge(graphdef, state)
+    return model(x)
+
+
+serialize_compiled_artifacts_to_disk(
+    forward, graphdef, state, x, output_prefix="model/model"
+)
+tt_alchemist.generate_cpp(
+    input_file="model/model_ttir.mlir",
+    output_dir="model/cpp",
+    local=False,
+)
+
+tt_alchemist.generate_python(
+    input_file="model/model_ttir.mlir",
+    output_dir="model/py",
+    local=False,
+)
