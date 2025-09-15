42ed0e5044aefc216c3c4f16a1e52bb777537690
Lewis Panos <lpanos@tenstorrent.com>
2025-09-15 10:22:09 -0400

    Add custom torch ops for fill/update cache and remove `rectify_buffer_inplace_copy` pass. (#1361)
    ### Ticket
    https://github.com/tenstorrent/tt-xla/issues/1257
    
    ### Problem description
    - We require these custom ops so we can signal to tt-mlir directly to
    use `ttir.fill_cache` and `ttir.update_cache`.
    - we must in-place copy the new kv cache values inside a custom vLLM
    attention backend. Removing these copy ops results in mismanagement of
    the KV cache and thus results in incorrect outputs.
    - This PR is contingent on the following PR in tt-mlir being merged:
    https://github.com/tenstorrent/tt-mlir/pull/4895
    
    ### What's changed
    - Added custom torch ops: `tt.fill_cache` and `tt.update_cache`
    - These ops can only be run on tensors which are on on `xla` device.
    These operations will call a `stablehlo.custom_call` to `tt.fill_cache`
    or `tt.update_cache`. These will be lowered directly to
    `ttir.fill_cache` and `ttir.update_cache`
    - Also added a CPU implementation of the op so that we can test it with
    Goldens.
    - Remove `rectify_buffer_inplace_copy`.
    - Replacing the input to an in-place copy_ op with a
    `tt.mark_argument_attributes` result causes XLA to handle the copying
    into an input tensor (kv_cache) incorrectly. Add contingency in
    `insert_argument_type_markers` to not replace the destination tensor
    with the `tt.mark_argument_attributes` result.
    - Added unit tests for the custom ops.
    
    
    ### Checklist
    - [X] New/Existing tests provide coverage for changes

---
+++ tests/torch/single_chip/ops/test_custom_ops.py
@@ -0,0 +1,47 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import torch
+import torch_xla.core.xla_model as xm
+
+import pytest
+from infra.utilities.types import Framework
+import tt_torch.custom_ops
+import tt_torch
+from tests.infra.testers.single_chip.op.op_tester import OpTester, run_op_test
+
+# TODO: Record superset properties for these tests.
+
+
+@pytest.mark.parametrize("num_heads", [12, 16])
+@pytest.mark.parametrize("max_seq_len", [64, 128])
+@pytest.mark.parametrize("head_size", [64, 128])
+@pytest.mark.parametrize("seq_len_to_fill", [32, 64])
+def test_fill_cache(num_heads, max_seq_len, head_size, seq_len_to_fill):
+
+    cache = torch.zeros(1, num_heads, max_seq_len, head_size, dtype=torch.bfloat16)
+    fill_value = torch.randn(
+        1, num_heads, seq_len_to_fill, head_size, dtype=torch.bfloat16
+    )
+
+    run_op_test(
+        torch.ops.tt.fill_cache, [cache, fill_value, 0], framework=Framework.TORCH
+    )
+
+
+@pytest.mark.parametrize("num_heads", [12, 16])
+@pytest.mark.parametrize("max_seq_len", [64, 128])
+@pytest.mark.parametrize("head_size", [64, 128])
+def test_update_cache(num_heads, max_seq_len, head_size):
+
+    cache = torch.zeros(1, num_heads, max_seq_len, head_size, dtype=torch.bfloat16)
+    fill_value = torch.randn(1, num_heads, 1, head_size, dtype=torch.bfloat16)
+
+    cache_position = torch.tensor([10], dtype=torch.int32)
+
+    run_op_test(
+        torch.ops.tt.update_cache,
+        [cache, fill_value, cache_position, 0],
+        framework=Framework.TORCH,
+    )
