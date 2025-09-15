c9bef20f232b2a19c05a53d7ae48fb4f7f630a74
Nikola Drakulic <ndrakulic@tenstorrent.com>
2025-09-15 09:24:32 +0000

    Add TODO for training kwargs in jax models

--- tests/infra/testers/single_chip/model/jax_model_tester.py
+++ tests/infra/testers/single_chip/model/jax_model_tester.py
@@ -140,6 +140,7 @@ class JaxModelTester(ModelTester):
                 **self._input_activations,
             }
 
+        # TODO: add support for model-specific kwargs for different models. https://github.com/tenstorrent/tt-xla/issues/1388
         return {}
 
     def _get_static_argnames(self) -> Optional[Sequence[str]]:
