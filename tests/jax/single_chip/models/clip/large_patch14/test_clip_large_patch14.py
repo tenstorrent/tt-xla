501e074499f03c7b9b2d1fbf3c7544257e77ca32
ctr-lelanchelian <ctr-lelanchelian@example.com>
2025-09-15 12:24:59 +0000

    refactor all the jax model to use modelinfo

--- tests/jax/single_chip/models/clip/large_patch14/test_clip_large_patch14.py
+++ tests/jax/single_chip/models/clip/large_patch14/test_clip_large_patch14.py
@@ -3,29 +3,23 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import pytest
-from infra import Framework, RunMode
+from infra import RunMode
 from utils import (
     BringupStatus,
     Category,
-    ModelGroup,
-    ModelSource,
-    ModelTask,
-    build_model_name,
     incorrect_result,
 )
-from third_party.tt_forge_models.clip.image_classification.jax import ModelVariant
+from third_party.tt_forge_models.config import Parallelism
+from third_party.tt_forge_models.clip.image_classification.jax import (
+    ModelVariant,
+    ModelLoader,
+)
 
 from ..tester import FlaxCLIPTester
 
 VARIANT_NAME = ModelVariant.LARGE_PATCH14
-MODEL_NAME = build_model_name(
-    Framework.JAX,
-    "clip_vit_patch14",
-    "large",
-    ModelTask.CV_IMAGE_CLS,
-    ModelSource.HUGGING_FACE,
-)
 
+MODEL_INFO = ModelLoader.get_model_info(VARIANT_NAME)
 
 # ----- Fixtures -----
 
@@ -46,9 +40,9 @@ def training_tester() -> FlaxCLIPTester:
 @pytest.mark.model_test
 @pytest.mark.record_test_properties(
     category=Category.MODEL_TEST,
-    model_name=MODEL_NAME,
-    model_group=ModelGroup.GENERALITY,
+    model_info=MODEL_INFO,
     run_mode=RunMode.INFERENCE,
+    parallelism=Parallelism.SINGLE_DEVICE,
     bringup_status=BringupStatus.INCORRECT_RESULT,
 )
 @pytest.mark.xfail(
@@ -64,9 +58,9 @@ def test_clip_large_patch14_inference(inference_tester: FlaxCLIPTester):
 @pytest.mark.nightly
 @pytest.mark.record_test_properties(
     category=Category.MODEL_TEST,
-    model_name=MODEL_NAME,
-    model_group=ModelGroup.GENERALITY,
+    model_info=MODEL_INFO,
     run_mode=RunMode.TRAINING,
+    parallelism=Parallelism.SINGLE_DEVICE,
 )
 @pytest.mark.skip(reason="Support for training not implemented")
 def test_clip_large_patch14_training(training_tester: FlaxCLIPTester):
