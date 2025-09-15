5660a01228e3bce91ed958384394cab4a483307f
Lewis Panos <lpanos@tenstorrent.com>
2025-09-15 19:25:42 +0000

    Update licensing info

--- integrations/vllm_plugin/vllm_tt/model_runner.py
+++ integrations/vllm_plugin/vllm_tt/model_runner.py
@@ -1,5 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
+# SPDX-FileCopyrightText: Portions (c) 2025 Tenstorrent AI ULC
+
 import bisect
 import gc
 import time
