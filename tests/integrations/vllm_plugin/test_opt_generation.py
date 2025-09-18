dd3d29d0a2dd3b42cf9f556fcbea261d4d18b86a
Lewis Panos <lpanos@tenstorrent.com>
2025-09-18 18:38:53 +0000

    Dont assert generated tokens for test

--- tests/integrations/vllm_plugin/test_opt_generation.py
+++ tests/integrations/vllm_plugin/test_opt_generation.py
@@ -11,22 +11,15 @@ def test_opt_generation():
     prompts = [
         "Hello, my name is",
     ]
-    sampling_params = vllm.SamplingParams(temperature=0.8, top_p=0.95, max_tokens=16)
+    sampling_params = vllm.SamplingParams(temperature=0.8, top_p=0.95, max_tokens=32)
     llm_args = {
         "model": "facebook/opt-125m",
-        "max_num_batched_tokens": 16,
+        "max_num_batched_tokens": 32,
         "max_num_seqs": 1,
-        "max_model_len": 16,
+        "max_model_len": 32,
     }
     llm = vllm.LLM(**llm_args)
 
     output_text = llm.generate(prompts, sampling_params)[0].outputs[0].text
 
-    expected_output_text = " Kaitlyn. I'm a student at the"
-    assert output_text == expected_output_text, (
-        'Expected output text to be "'
-        + expected_output_text
-        + '", but got "'
-        + output_text
-        + '"'
-    )
+    print(f"prompt: {prompts[0]}, output: {output_text}")
