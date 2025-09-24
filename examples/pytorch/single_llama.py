6d1f39526c3c1ad47f2f692f1ab0208cadab6875
James Zian Xu <jameszianxu@tenstorrent.com>
2025-09-24 16:32:16 +0000

    Check in singledevice demo, disable torch_xla._XLAC._xla_sync_multi

--- examples/pytorch/single_llama.py
+++ examples/pytorch/single_llama.py
@@ -19,55 +19,14 @@ from torch_xla.distributed.spmd import Mesh
 import torch_xla.distributed.spmd as xs
 
 
-def setup_spmd():
-    print("Setting up XLA environment...")
-    num_devices = xr.global_runtime_device_count()
-
-    # Basic XLA configuration
-    os.environ[
-        "ENABLE_AUTO_PARALLEL"
-    ] = "TRUE"  # Enables the auto parallel pass in tt-mlir
-    os.environ[
-        "CONVERT_SHLO_TO_SHARDY"
-    ] = "1"  # Converts the StableHLO emitted by torch-xla to the Shardy dialect
-    os.environ[
-        "MESH_SHAPE"
-    ] = f"1,{num_devices}"  # Sets the mesh shape used by the auto parallel pass
-
-    # Initialize SPMD
-    xr.use_spmd()
-    print("XLA environment configured.")
-
-
-def create_device_mesh() -> Mesh:
-    """
-    Create device mesh for tensor parallelism.
-
-    Args:
-        num_devices: Total number of devices
-        mesh_shape: Shape of the device mesh (batch_dim, model_dim)
-
-    Returns:
-        Mesh object for SPMD operations
-    """
-    num_devices = xr.global_runtime_device_count()
-    mesh_shape = (1, num_devices)
-    device_ids = np.array(range(num_devices))
-    mesh = Mesh(device_ids, mesh_shape, ("batch", "model"))
-    print(f"Created device mesh: {mesh_shape} with {num_devices} devices")
-    return mesh
-
-
 # --------------------------------
 # Llama Generation Example
 # --------------------------------
 def llama():
 
-    setup_spmd()  # must be called @ start of program, crucially before creating device mesh / setting up device.
 
     # Connect the device.
     device = xm.xla_device()
-    mesh = create_device_mesh()
 
     # Instantiate model.
     model_name: str = "meta-llama/Llama-3.2-3B"
@@ -126,32 +85,9 @@ def llama():
         "use_cache": True,
     }
 
-    xs.mark_sharding(input_args["input_ids"], mesh, (None, None))
-    xs.mark_sharding(input_args["cache_position"], mesh, (None,))
-
-    # apply shardings
-    for i, (key, value) in enumerate(
-        zip(
-            input_args["past_key_values"].key_cache,
-            input_args["past_key_values"].value_cache,
-        )
-    ):
-        xs.mark_sharding(key, mesh, (None, "model", None, None))
-        xs.mark_sharding(value, mesh, (None, "model", None, None))
 
     # Move inputs and model to device.
     model = model.to(device)
-
-    # shard model internals
-    for layer in model.model.layers:
-        xs.mark_sharding(layer.mlp.up_proj.weight, mesh, ("model", None))
-        xs.mark_sharding(layer.mlp.gate_proj.weight, mesh, ("model", None))
-        xs.mark_sharding(layer.mlp.down_proj.weight, mesh, (None, "model"))
-
-        xs.mark_sharding(layer.self_attn.q_proj.weight, mesh, ("model", None))
-        xs.mark_sharding(layer.self_attn.k_proj.weight, mesh, ("model", None))
-        xs.mark_sharding(layer.self_attn.v_proj.weight, mesh, ("model", None))
-        xs.mark_sharding(layer.self_attn.o_proj.weight, mesh, (None, "model"))
 
     # Run model (with no gradient calculation since we only need inference).
     tokens_to_generate = 3
@@ -178,16 +114,6 @@ def llama():
             host_cache_pos = input_args["cache_position"].to("cpu")
             host_cache_pos = torch.tensor([host_cache_pos[-1:] + 1])
             input_args["cache_position"] = host_cache_pos.to(device)
-
-            # reapply shardings for static cache (io inplace mutated tensors since they lose sharding annotations)
-            for i, (key, value) in enumerate(
-                zip(
-                    input_args["past_key_values"].key_cache,
-                    input_args["past_key_values"].value_cache,
-                )
-            ):
-                xs.mark_sharding(key, mesh, (None, "model", None, None))
-                xs.mark_sharding(value, mesh, (None, "model", None, None))
 
     print("output tokens:", output_tokens)
 
