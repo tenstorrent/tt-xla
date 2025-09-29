faf7fa66627499339f186137f896fc534515eadc
umalesTT <umales@tenstorrent.com>
2025-09-29 13:57:36 +0000

    Add monkeypatching for jax.random.uniform.

--- tests/jax/single_chip/models/mnist/cnn/dropout/test_mnist_cnn_dropout.py
+++ tests/jax/single_chip/models/mnist/cnn/dropout/test_mnist_cnn_dropout.py
@@ -71,8 +71,8 @@ def test_mnist_cnn_dropout_inference(inference_tester: MNISTCNNTester):
 )
 @pytest.mark.xfail(
     reason=failed_ttmlir_compilation(
-        "error: failed to legalize operation 'stablehlo.bitcast_convert' "
-        "https://github.com/tenstorrent/tt-mlir/issues/979"
+        "error: failed to legalize operation 'stablehlo.select_and_scatter' "
+        "https://github.com/tenstorrent/tt-mlir/issues/4794"
     )
 )
 def test_mnist_cnn_dropout_training(training_tester: MNISTCNNTester):
