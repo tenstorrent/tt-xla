64a55d4f7bad15af4a4d619f941270ee475a5560
ssalice <ssalice@tenstorrent.com>
2025-09-29 23:00:21 +0000

    Unit tests for sdpa, taken from Lewis's branch.

--- tests/torch/single_chip/ops/test_custom_ops.py
+++ tests/torch/single_chip/ops/test_custom_ops.py
@@ -45,3 +45,87 @@ def test_update_cache(num_heads, max_seq_len, head_size):
         [cache, fill_value, cache_position, 0],
         framework=Framework.TORCH,
     )
+
+
+@pytest.mark.parametrize(
+    "batch_size, num_heads, seq_len, head_size, num_kv_heads, max_seq_len, is_causal, scale",
+    [
+        (1, 12, 32, 128, 12, 32, True, 1.0),
+        (1, 12, 32, 128, 12, 128, False, 1.0),
+        (8, 12, 32, 128, 12, 32, True, 1.0),
+        (8, 12, 32, 128, 12, 128, False, 1.0),
+        (1, 12, 32, 128, 4, 32, True, 1.0),
+        (1, 12, 32, 128, 4, 128, False, 1.0),
+        (8, 12, 32, 128, 4, 32, True, 1.0),
+        (8, 12, 32, 128, 4, 128, False, 1.0),
+    ],
+)
+def test_scaled_dot_product_attention(
+    batch_size,
+    num_heads,
+    seq_len,
+    head_size,
+    num_kv_heads,
+    max_seq_len,
+    is_causal,
+    scale,
+):
+
+    query = torch.randn(batch_size, num_heads, seq_len, head_size, dtype=torch.bfloat16)
+    key = torch.randn(
+        batch_size, num_kv_heads, max_seq_len, head_size, dtype=torch.bfloat16
+    )
+    value = torch.randn(
+        batch_size, num_kv_heads, max_seq_len, head_size, dtype=torch.bfloat16
+    )
+    attn_mask = (
+        torch.randn(batch_size, 1, seq_len, max_seq_len, dtype=torch.bfloat16)
+        if not is_causal
+        else None
+    )
+
+    run_op_test(
+        torch.ops.tt.scaled_dot_product_attention,
+        [query, key, value, attn_mask, is_causal, scale],
+        framework=Framework.TORCH,
+    )
+
+
+@pytest.mark.parametrize(
+    "batch_size, num_heads, head_size, num_kv_heads, max_seq_len, is_causal, scale",
+    [
+        (1, 12, 128, 12, 32, True, 1.0),
+        (1, 12, 128, 12, 32, False, 1.0),
+        (1, 12, 128, 12, 128, False, 1.0),
+        (8, 12, 128, 12, 32, True, 1.0),
+        (8, 12, 128, 12, 128, False, 1.0),
+        (1, 12, 128, 4, 32, True, 1.0),
+        (1, 12, 128, 4, 32, False, 1.0),
+        (1, 12, 128, 4, 128, False, 1.0),
+        (8, 12, 128, 4, 32, True, 1.0),
+        (8, 12, 128, 4, 128, False, 1.0),
+    ],
+)
+def test_scaled_dot_product_attention_decode(
+    batch_size, num_heads, head_size, num_kv_heads, max_seq_len, is_causal, scale
+):
+
+    query = torch.randn(1, batch_size, num_heads, head_size, dtype=torch.bfloat16)
+    key = torch.randn(
+        batch_size, num_kv_heads, max_seq_len, head_size, dtype=torch.bfloat16
+    )
+    value = torch.randn(
+        batch_size, num_kv_heads, max_seq_len, head_size, dtype=torch.bfloat16
+    )
+    cur_pos_tensor = torch.arange(batch_size, dtype=torch.int32)
+    attn_mask = (
+        torch.randn(batch_size, 1, num_heads, max_seq_len, dtype=torch.bfloat16)
+        if not is_causal
+        else None
+    )
+
+    run_op_test(
+        torch.ops.tt.scaled_dot_product_attention_decode,
+        [query, key, value, cur_pos_tensor, attn_mask, None, is_causal, scale],
+        framework=Framework.TORCH,
+    )
