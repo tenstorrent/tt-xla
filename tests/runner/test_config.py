55d5146bf76ae805fb8bef19fb7d68ba3d5ac0f2
Kyle Mabee <kmabee@tenstorrent.com>
2025-10-01 21:43:56 +0000

    hrnet/pytorch-hrnetv2_w48_osmr-full-inference fix:

--- tests/runner/test_config.py
+++ tests/runner/test_config.py
@@ -2434,10 +2434,6 @@ test_config = {
     },
     "hrnet/pytorch-hrnetv2_w48_osmr-full-inference": {
         "status": ModelTestStatus.EXPECTED_PASSING,
-        "arch_overrides": {
-            "n150": {
-                "required_pcc": 0.985,  # Decreased Sept 26th - https://github.com/tenstorrent/tt-xla/issues/1491
-            },
-        },
+        "required_pcc": 0.985,  # Decreased Sept 26th - https://github.com/tenstorrent/tt-xla/issues/1491
     },
 }
