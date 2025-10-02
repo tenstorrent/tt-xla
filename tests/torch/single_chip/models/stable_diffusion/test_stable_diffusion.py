44b2ce34bfe6c4bcaf0d057f3076d0723e91376c
ssalice <ssalice@tenstorrent.com>
2025-10-02 14:37:27 +0000

    added some stuff to the stable diffusion submodule test but not tested.

--- tests/torch/single_chip/models/stable_diffusion/test_stable_diffusion.py
+++ tests/torch/single_chip/models/stable_diffusion/test_stable_diffusion.py
@@ -15,34 +15,53 @@ from utils import (
 )
 
 import torch
+import torch_xla.core.xla_model as xm
+import torch_xla.runtime as xr
 from diffusers import StableDiffusion3Pipeline
 from diffusers.pipelines.stable_diffusion.pipeline_stable_diffusion import (
     retrieve_timesteps,
 )
+from third_party.tt_forge_models.stable_diffusion.pytorch.src.model_utils import stable_diffusion_preprocessing_v35
 
-def load_pipe(variant):
+
+def test_stable_diffusion():
     pipe = StableDiffusion3Pipeline.from_pretrained(
         f"stabilityai/stable-diffusion-3.5-large", torch_dtype=torch.float32
     )
-    modules = pipe.transformer
+    transformer_module = pipe.transformer
+    transformer_module.eval()
+
+    # Figure out inputs for the transformer module
+    prompt = "An astronaut riding a green horse"
+    latent_model_input, timestep, prompt_embeds, pooled_prompt_embeds = stable_diffusion_preprocessing_v35(
+        pipe, prompt
+    )
     
-    module.eval()
-    for param in module.parameters():
-        if param.requires_grad:
-            param.requires_grad = False
+    # Prepare inputs for transformer
+    inputs = {
+        "hidden_states": latent_model_input,
+        "encoder_hidden_states": prompt_embeds, 
+        "pooled_projections": pooled_prompt_embeds,
+        "timestep": timestep,
+    }
 
-    return pipe
-
-
-@pytest.mark.model_test
-@pytest.mark.record_test_properties(
-    category=Category.MODEL_TEST,
-    model_info=MODEL_INFO,
-    run_mode=RunMode.INFERENCE,
-    bringup_status=BringupStatus.INCORRECT_RESULT,
-)
-def test_stable_diffusion():
-    pipe = load_pipe()
+    # Run on CPU for golden
+    cpu_output = transformer_module(**inputs)
     
-    # Move the pipeline to CPU
-    pipe.to("cpu")
+    # Run on TT device
+    xr.set_device_type("TT")
+    device = xm.xla_device()
+    compiled_transformer = torch.compile(transformer_module.to("TT"), backend="tt")
+
+    tt_output = compiled_transformer(**inputs)
+
+    # compare outputs
+    comparator = TorchComparator(
+        ComparisonConfig(
+            #atol=AtolConfig(required_atol=0.02),
+            pcc=PccConfig(required_pcc=0.99),
+        )
+    )
+    
+    comparator.compare(tt_output, cpu_output)
+    
