6604eddb12d8a4b52f70d2c39e56e4b71ceb6e5b
James Zian Xu <jameszianxu@tenstorrent.com>
2025-10-06 17:35:08 +0000

    Add simple testcase for y=x+1

---
+++ examples/pytorch/test_tensor_persistence.py
@@ -0,0 +1,23 @@
+import os
+import torch
+import torch_xla
+import torch_xla.runtime as xr
+import torch_xla.core.xla_model as xm
+
+def test_add():
+    class AddModel(torch.nn.Module):
+        def __init__(self):
+            super().__init__()
+        def forward(self, x):
+            return x+1
+
+    xr.set_device_type("TT")
+    device = torch_xla.device()
+
+    x:torch.Tensor = torch.ones((3,3)).to(device)
+    model = AddModel()
+    model = model.to(device)
+    model.compile(backend='tt')
+    y = model(x)
+    
+    print(y)
