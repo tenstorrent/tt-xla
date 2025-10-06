452bcffe873964b6741b5773c75929aba30b846e
Stefan Gligorijevic <189116645+sgligorijevicTT@users.noreply.github.com>
2025-10-06 21:09:20 +0200

    Split up ExecutableImage into FB and SO versions (#1329)
    ### Ticket
    This PR is one of the prerequisites for
    https://github.com/tenstorrent/tt-xla/issues/946
    
    This PR depends on https://github.com/tenstorrent/tt-xla/pull/1328 and
    will be targetting it until it is merged in main, at which point this PR
    will be rebased and retargeted.
    
    ### Problem description
    In preparation for Codegen(internally also called EmitC/EmitPy
    sometimes) support, including using codegen for model execution, we
    should split up classes to allow these separate code paths to exist.
    
    ### What's changed
    ExecutableImage and LoadedExecutableInstance have been split up.
    
    ### Checklist
    - [X] New/Existing tests provide coverage for changes

--- inc/common/pjrt_implementation/compile_options.h
+++ inc/common/pjrt_implementation/compile_options.h
@@ -2,14 +2,27 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-#ifndef TT_XLA_INC_COMMON_PJRT_IMPLEMENTATION_MODULE_BUILDER_COMPILE_OPTIONS_H_
-#define TT_XLA_INC_COMMON_PJRT_IMPLEMENTATION_MODULE_BUILDER_COMPILE_OPTIONS_H_
+#ifndef TT_XLA_INC_COMMON_PJRT_IMPLEMENTATION_COMPILE_OPTIONS_H_
+#define TT_XLA_INC_COMMON_PJRT_IMPLEMENTATION_COMPILE_OPTIONS_H_
 
 // c++ standard library includes
+#include <optional>
 #include <string>
 #include <unordered_map>
 
-namespace tt::pjrt::module_builder {
+namespace tt::pjrt {
+
+// Enumeration for backend types
+enum class BackendRuntime {
+  // Targets tt-mlir TTNN runtime to execute the compiled flatbuffer.
+  TTNNFlatbuffer,
+
+  // Generates TTNN C++ code to be compiled and executed.
+  TTNNCodegenCpp,
+
+  // Generates TTNN Python code.
+  TTNNCodegenPy
+};
 
 // POD struct containing various options used to customize module compilation.
 struct CompileOptions {
@@ -41,6 +54,9 @@ struct CompileOptions {
   // fixed.
   bool enable_fusing_conv2d_with_multiply_pattern = false;
 
+  // Backend runtime which should be targeted for compilation and execution.
+  BackendRuntime backend = BackendRuntime::TTNNFlatbuffer;
+
   static CompileOptions
   parse(const std::unordered_map<std::string, std::string> &compile_options);
 };
@@ -48,12 +64,17 @@ struct CompileOptions {
 namespace internal {
 
 // Parse out the value of one specific boolean flag from the options map.
-bool parseBoolOption(
+std::optional<bool> parseBoolOption(
+    const std::unordered_map<std::string, std::string> &compile_options,
+    std::string option_name);
+
+// Parse backend option from string to enum
+std::optional<BackendRuntime> parseBackendOption(
     const std::unordered_map<std::string, std::string> &compile_options,
     std::string option_name);
 
 } // namespace internal
 
-} // namespace tt::pjrt::module_builder
+} // namespace tt::pjrt
 
-#endif // TT_XLA_INC_COMMON_PJRT_IMPLEMENTATION_MODULE_BUILDER_COMPILE_OPTIONS_H_
+#endif // TT_XLA_INC_COMMON_PJRT_IMPLEMENTATION_COMPILE_OPTIONS_H_
