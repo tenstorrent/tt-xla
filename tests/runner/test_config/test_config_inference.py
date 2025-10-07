09f7cfb01bc4685af5b6dbc0932aa49bf64a8b9d
kkannan <kkannan@tenstorrent.com>
2025-10-07 05:16:40 +0000

    Add test config for mplug-owl2

--- tests/runner/test_config/test_config_inference.py
+++ tests/runner/test_config/test_config_inference.py
@@ -2503,4 +2503,8 @@ test_config = {
         "reason": "Out of Memory: Not enough space to allocate 69599232 B L1 buffer across 72 banks, where each bank needs to store 966656 B, but bank size is only 1366016 B - https://github.com/tenstorrent/tt-xla/issues/1497",
         "bringup_status": BringupStatus.FAILED_RUNTIME,
     },
+    "mplug_owl2/pytorch-llama2_7b-full-inference": {
+        "status": ModelTestStatus.EXPECTED_PASSING,
+        "bringup_status": BringupStatus.FAILED_FE_COMPILATION,
+    },
 }
