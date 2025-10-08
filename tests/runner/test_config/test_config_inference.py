ba8a59771ac3fe4bf8a02c5a92ec929d2bfba48c
Het Shah <hshah@tenstorrent.com>
2025-10-08 15:53:43 -0400

    Revert composite ops to allow nightlies to succeed (#1624)
    ### Ticket
    https://github.com/tenstorrent/tt-xla/issues/1616
    
    ### Problem description
    PR https://github.com/tenstorrent/tt-xla/pull/1482 added support for
    composite ops for torch graphs. However, this caused regressions in
    current multi-chip TP models.
    
    ### What's changed
    Reverting the composite ops support to allow TP models to pass again.
    Also reverting
    https://github.com/tenstorrent/tt-xla/commit/f8ca9edc82d6c27f6dbf127db9eb244465560700
    since the models now pass again.
    
    ### Checklist
    - [x] New/Existing tests provide coverage for changes

--- tests/runner/test_config/test_config_inference.py
+++ tests/runner/test_config/test_config_inference.py
@@ -1830,9 +1830,17 @@ test_config = {
     },
     "gemma/pytorch-google/gemma-1.1-7b-it-full-inference": {
         "supported_archs": ["p150", "n300-llmbox"],
-        "status": ModelTestStatus.KNOWN_FAILURE_XFAIL,
-        "reason": "Operand at index 0 has type x, but decomposition has type y - https://github.com/tenstorrent/tt-xla/issues/1616",
-        "bringup_status": BringupStatus.FAILED_TTMLIR_COMPILATION,
+        "assert_pcc": False,
+        "status": ModelTestStatus.EXPECTED_PASSING,
+        "bringup_status": BringupStatus.INCORRECT_RESULT,
+        "arch_overrides": {
+            "p150": {
+                "reason": "AssertionError: PCC comparison failed. Calculated: pcc=0.976563572883606. Required: pcc=0.99",
+            },
+            "n300-llmbox": {
+                "reason": "AssertionError: PCC comparison failed. Calculated: pcc=0.9626210927963257. Required: pcc=0.97",
+            },
+        },
     },
     "stable_diffusion_xl/pytorch-stable-diffusion-xl-base-1.0-full-inference": {
         "status": ModelTestStatus.KNOWN_FAILURE_XFAIL,
@@ -2388,9 +2396,7 @@ test_config = {
     },
     "gemma/pytorch-google/gemma-2-9b-it-full-inference": {
         "supported_archs": ["p150", "n300-llmbox"],
-        "status": ModelTestStatus.KNOWN_FAILURE_XFAIL,
-        "reason": "Operand at index 0 has type x, but decomposition has type y - https://github.com/tenstorrent/tt-xla/issues/1616",
-        "bringup_status": BringupStatus.FAILED_TTMLIR_COMPILATION,
+        "status": ModelTestStatus.EXPECTED_PASSING,
     },
     "gemma/pytorch-google/gemma-2-27b-it-full-inference": {
         "supported_archs": ["p150", "n300-llmbox"],
@@ -2400,9 +2406,17 @@ test_config = {
     },
     "falcon/pytorch-tiiuae/falcon-7b-instruct-full-inference": {
         "supported_archs": ["p150", "n300-llmbox"],
-        "status": ModelTestStatus.KNOWN_FAILURE_XFAIL,
-        "reason": "Operand at index 0 has type x, but decomposition has type y - https://github.com/tenstorrent/tt-xla/issues/1616",
-        "bringup_status": BringupStatus.FAILED_TTMLIR_COMPILATION,
+        "arch_overrides": {
+            "p150": {
+                "assert_pcc": False,
+                "status": ModelTestStatus.EXPECTED_PASSING,
+                "bringup_status": BringupStatus.INCORRECT_RESULT,
+                "reason": "AssertionError: PCC comparison failed. Calculated: pcc=0.9418849945068359. Required: pcc=0.99 - https://github.com/tenstorrent/tt-xla/issues/1475",
+            },
+            "n300-llmbox": {
+                "status": ModelTestStatus.EXPECTED_PASSING,
+            },
+        },
     },
     "d_fine/pytorch-nano-full-inference": {
         "status": ModelTestStatus.NOT_SUPPORTED_SKIP,
