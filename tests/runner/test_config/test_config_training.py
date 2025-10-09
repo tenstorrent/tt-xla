fc17df03f260bc0c15ec00f9f973c66086b51b59
Kyle Mabee <kmabee@tenstorrent.com>
2025-10-09 16:54:00 +0000

    Mass update to account for new named test now contain single_device in name

--- tests/runner/test_config/test_config_training.py
+++ tests/runner/test_config/test_config_training.py
@@ -7,14 +7,14 @@ from tests.utils import BringupStatus
 
 
 test_config = {
-    "mnist/pytorch-full-training": {
+    "mnist/pytorch-single_device-full-training": {
         "status": ModelTestStatus.KNOWN_FAILURE_XFAIL,
         "reason": "error: failed to legalize operation 'stablehlo.rng_bit_generator' "
         "https://github.com/tenstorrent/tt-mlir/issues/4793",
         "bringup_status": BringupStatus.FAILED_TTMLIR_COMPILATION,
         "markers": ["push"],
     },
-    "autoencoder/pytorch-linear-full-training": {
+    "autoencoder/pytorch-linear-single_device-full-training": {
         "status": ModelTestStatus.EXPECTED_PASSING,
         "markers": ["push"],
     },
