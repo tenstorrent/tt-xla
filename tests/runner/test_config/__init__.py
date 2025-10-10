8ac5a13463d402ea6a84aa79456faa9c194ff76e
Kyle Mabee <kmabee@tenstorrent.com>
2025-10-10 02:51:32 +0000

    New Parallelism.DATA_PARALLEL tests added, tagged and job in CI
     - Enable Parallelism.DATA_PARALLEL tests in pytest param in test_models.py
     - Add initial test_config_inference_data_parallel.py with 75 passing tests
     - Add data_parallel job on n300 to nightly CI

--- tests/runner/test_config/__init__.py
+++ tests/runner/test_config/__init__.py
@@ -2,6 +2,9 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+from .test_config_inference_data_parallel import (
+    test_config as test_config_inference_data_parallel,
+)
 from .test_config_inference_single_device import (
     PLACEHOLDER_MODELS as PLACEHOLDER_MODELS_INFERENCE,
 )
@@ -20,5 +23,6 @@ PLACEHOLDER_MODELS = PLACEHOLDER_MODELS_INFERENCE
 test_config = (
     test_config_inference_single_device
     | test_config_inference_tensor_parallel
+    | test_config_inference_data_parallel
     | test_config_training_single_device
 )
