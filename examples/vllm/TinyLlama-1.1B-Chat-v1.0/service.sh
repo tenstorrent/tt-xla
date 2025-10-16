5f96bf1bd63057eaae12c20c804f7120a4c8dc43
Lewis Panos <lpanos@tenstorrent.com>
2025-10-16 20:23:47 +0000

    Always compile seq len 1 for generative model runner. Do not put prompt lens on device. Perform post-processing on the hidden states in pooling_runner on host. Add Qwen3-Embedding-4B embedding server demo. Add TinyLlama-1.1B chat server demo

---
+++ examples/vllm/TinyLlama-1.1B-Chat-v1.0/service.sh
@@ -0,0 +1,6 @@
+TT_RUNTIME_ENABLE_PROGRAM_CACHE=1 LOGGER_LEVEL=DEBUG vllm serve TinyLlama/TinyLlama-1.1B-Chat-v1.0 \
+    --max-model-len 128 \
+    --max-num-batched-tokens 128 \
+    --max-num-seqs 1 \
+    --no-enable-prefix-caching \
+    --additional-config "{\"enable_const_eval\": \"False\", \"min_context_len\": 32}"
