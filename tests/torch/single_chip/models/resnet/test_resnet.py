0c7647a8644b9d8fe1dadf03d2c3b85f352fb6e4
Kyle Mabee <kmabee@tenstorrent.com>
2025-10-16 18:56:58 +0000

    Flip polarity make program cache enabled by default, disable with optional TT_RUNTIME_ENABLE_PROGRAM_CACHE=0

--- tests/torch/single_chip/models/resnet/test_resnet.py
+++ tests/torch/single_chip/models/resnet/test_resnet.py
@@ -49,7 +49,6 @@ def create_inference_tester(format: str, enable_optimizer: bool) -> ResnetTester
 
 @pytest.fixture
 def trace_tester(monkeypatch: MonkeyPatch) -> ResnetTester:
-    monkeypatch.setenv("TT_RUNTIME_ENABLE_PROGRAM_CACHE", "1")
     monkeypatch.setenv("TT_RUNTIME_TRACE_REGION_SIZE", "10000000")
 
     compiler_config = CompilerConfig(enable_optimizer=True, enable_trace=True)
