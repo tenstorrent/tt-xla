17484b55a7e4e5065bccfcc9ee2857e0dbfc98db
Lewis Panos <lpanos@tenstorrent.com>
2025-10-17 18:20:41 +0000

    Add SPDX header to sevice scripts

--- examples/vllm/TinyLlama-1.1B-Chat-v1.0/service.sh
+++ examples/vllm/TinyLlama-1.1B-Chat-v1.0/service.sh
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+
 TT_RUNTIME_ENABLE_PROGRAM_CACHE=1 LOGGER_LEVEL=DEBUG vllm serve TinyLlama/TinyLlama-1.1B-Chat-v1.0 \
     --max-model-len 128 \
     --max-num-batched-tokens 128 \
