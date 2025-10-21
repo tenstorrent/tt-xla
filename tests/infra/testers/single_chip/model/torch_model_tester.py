ff294666cfdd1ddfbb81fb7c511d3ca4955131ec
Nikola Drakulic <ndrakulic@tenstorrent.com>
2025-10-21 15:11:30 +0000

    Use unpack_forward_output

--- tests/infra/testers/single_chip/model/torch_model_tester.py
+++ tests/infra/testers/single_chip/model/torch_model_tester.py
@@ -143,11 +143,18 @@ class TorchModelTester(ModelTester):
 
         workload.compiled_executable = torch.compile(workload.model, backend=backend)
 
+    def _unwrap_model_output(self, output: Any) -> torch.Tensor:
+        """
+        Unwraps model output to a single tensor.
+        """
+        raise NotImplementedError("Subclasses must implement this method")
+
     def _test_training(self) -> Tuple[ComparisonResult, ...]:
         # Run forward on CPU
         # TODO: Needs further investigation https://github.com/tenstorrent/tt-xla/issues/1391
         # self._compile_for_cpu(self._workload)
         cpu_res = self._run_on_cpu(self._workload)
+        cpu_res = self._unpack_forward_output(cpu_res)
 
         # Generate random gradient
         random_grad = torch.randn(cpu_res.shape, dtype=cpu_res.dtype)
@@ -168,6 +175,8 @@ class TorchModelTester(ModelTester):
         # TODO: Needs further investigation https://github.com/tenstorrent/tt-xla/issues/1391
         # self._compile_for_tt_device(self._workload)
         tt_res = self._run_on_tt_device(self._workload)
+        tt_res = self._unpack_forward_output(tt_res)
+
         # Force graph break so we can differentiate between forward and backward
         torch_xla.sync(wait=True)
 
