e092d418069eb753c277e117bdd3bd2a64f511ce
Lewis Panos <lpanos@tenstorrent.com>
2025-10-22 18:08:47 +0000

    set vllm worker multiprocessing method to spawn in plugin register

--- integrations/vllm_plugin/vllm_tt/__init__.py
+++ integrations/vllm_plugin/vllm_tt/__init__.py
@@ -1,5 +1,10 @@
 # SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
 #
 # SPDX-License-Identifier: Apache-2.0
+
+import os
+
+
 def register():
+    os.environ["VLLM_WORKER_MULTIPROC_METHOD"] = "spawn"
     return "vllm_tt.platform.TTPlatform"
