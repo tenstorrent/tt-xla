bbc913e86f691d4ba3100c01a1ec76f1940074fe
Lewis Panos <lpanos@tenstorrent.com>
2025-10-22 18:11:56 +0000

    Add explanatory comment

--- integrations/vllm_plugin/vllm_tt/__init__.py
+++ integrations/vllm_plugin/vllm_tt/__init__.py
@@ -6,5 +6,6 @@ import os
 
 
 def register():
+    # Setting worker multiprocessing method to spawn to avoid hangs in consecutive vllm pytest runs
     os.environ["VLLM_WORKER_MULTIPROC_METHOD"] = "spawn"
     return "vllm_tt.platform.TTPlatform"
