3bb33259b5c03b137b94d8b7e026ec7744b693f0
Kyle Mabee <kmabee@tenstorrent.com>
2025-10-29 16:52:48 +0000

    debug code. revisit.

--- tests/infra/workloads/torch_workload.py
+++ tests/infra/workloads/torch_workload.py
@@ -49,10 +49,21 @@ class TorchWorkload(Workload):
     # If model has shard specs and running on multichip mesh, then convert StableHLO
     # to Shardy dialect and initialize XLA SPMD runtime.
     def _enable_xla_spmd_if_needed(self) -> None:
-        is_multichip = bool(self.mesh and len(self.mesh.device_ids) > 1)
-        has_shard_specs = False
-        if callable(self.shard_spec_fn) and self.model is not None:
-            has_shard_specs = bool(self.shard_spec_fn(self.model))
+        # is_multichip = bool(self.mesh and len(self.mesh.device_ids) > 1)
+        # has_shard_specs = False
+        # if callable(self.shard_spec_fn) and self.model is not None:
+        #     has_shard_specs = bool(self.shard_spec_fn(self.model))
+
+        # Case I was trying to fix...
+        # What if we ran single chip 4B test here... would it enable SPMD because shard_spec_fn is not None?
+
+        # This is old logic which worked okay.
+        has_shard_specs = self.shard_spec_fn is not None
+        is_multichip = self.mesh and len(self.mesh.device_ids) > 1
+        print(
+            f"KCM inside _enable_xla_spmd_if_needed now... has_shard_specs: {has_shard_specs} is_multichip: {is_multichip}",
+            flush=True,
+        )
 
         if has_shard_specs and is_multichip:
             enable_spmd()
