32665355983b66fac84e77eecfdd6f38d0d0acf4
Vladimir Brkic <vbrkic@tenstorrent.com>
2025-10-31 16:07:40 +0000

    Remove not needed files

--- tests/sweeps/utils/failing_reasons/__init__.py
+++ tests/sweeps/utils/failing_reasons/__init__.py
@@ -6,12 +6,10 @@
 
 from .failing_reasons import FailingReason, FailingReasons, FailingReasonsFinder
 from .failing_reasons_common import ExceptionData
-from .failing_reasons_validation import FailingReasonsValidation
 
 __all__ = [
     "FailingReason",
     "FailingReasons",
     "FailingReasonsFinder",
-    "FailingReasonsValidation",
     "ExceptionData",
 ]
