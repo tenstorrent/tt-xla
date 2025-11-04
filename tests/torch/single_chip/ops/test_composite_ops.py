71422d5a42c950af6fc3159249d8402cf3b3ca28
Sungjoon Shon <sshon@tenstorrent.com>
2025-11-04 17:35:04 +0000

    Test for composite op
    composite op testing

--- tests/torch/single_chip/ops/test_composite_ops.py
+++ tests/torch/single_chip/ops/test_composite_ops.py
@@ -14,15 +14,33 @@ from tests.infra.comparators.comparison_config import ComparisonConfig
 from tests.infra.testers.single_chip.op.op_tester import run_op_test_with_random_inputs
 
 
+import torch_xla
+import torch_xla.core.xla_model as xm
+import torch_xla.distributed.spmd as xs
+import torch_xla.runtime as xr
+from torch_xla.distributed.spmd import Mesh
+import numpy as np
+
+def setup_mesh(mesh_shape, axis_names):
+    xr.use_spmd()
+    import os
+    os.environ["CONVERT_SHLO_TO_SHARDY"] = "1"
+    device_ids = np.arange(np.prod(mesh_shape))
+    mesh = Mesh(device_ids=device_ids, mesh_shape=mesh_shape, axis_names=axis_names)
+
+    return mesh
+
 @pytest.mark.parametrize("approx", ["none", "tanh"])
 def test_composite_gelu(approx):
     """
     Tests example model that has a composite gelu operation.
     """
+    import os
+    os.system('tt-smi -r')
 
     class MM(torch.nn.Module):
         def forward(self, x):
-            return composite_gelu(x, approx)
+            return composite_gelu(composite_gelu(x, approx), approx)
 
     input = torch.randn(32, 32)
 
@@ -32,7 +50,10 @@ def test_composite_gelu(approx):
     device = xm.xla_device()
     model = torch.compile(model.to(device), backend="tt")
 
-    output = model(input.to(device))
+    input = input.to(device)
+    mesh = setup_mesh(mesh_shape=(1, 2), axis_names=("x", "y"))
+    xs.mark_sharding(input, mesh, (None, "y"))
+    output = model(input)
 
     comparator = TorchComparator(ComparisonConfig())
     comparator.compare(output, golden)
@@ -46,7 +67,7 @@ def test_composite_gelu_eager(approx):
 
     class MM(torch.nn.Module):
         def forward(self, x):
-            return composite_gelu(x, approx)
+            return composite_gelu(composite_gelu(x, approx), approx)
 
     input = torch.randn(32, 32)
 
