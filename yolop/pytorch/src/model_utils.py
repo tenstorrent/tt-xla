02bf560ba13956c6395bc5dbcb1dadee0f4e7793
Kamalraj Kannan <157608228+kamalrajkannan78@users.noreply.github.com>
2025-11-05 18:39:55 +0530

    Bringup YOLOP pytorch model (#248)
    ### Ticket
    
    - https://github.com/tenstorrent/tt-xla/issues/1902
    
    ### Problem description
    
    - Bringup YOLOP pytorch model
    
    ### What's changed
    
    - Added support for YOLOP pytorch model
    
    ### Checklist
    - [x] Verified the changes through local testing
    
    ### Logs
    
    -
    [nov5_yolop_cpu_run.log](https://github.com/user-attachments/files/23359209/nov5_cpu_run_from_loader_final.log)
    -
    [nov5_yolop_xla_run.log](https://github.com/user-attachments/files/23359223/nov5_yolop_xla_final.log)

---
+++ yolop/pytorch/src/model_utils.py
@@ -0,0 +1,46 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import cv2
+import numpy as np
+
+
+def letterbox_for_img(
+    img,
+    new_shape=(640, 640),
+    color=(114, 114, 114),
+    auto=True,
+    scaleFill=False,
+    scaleup=True,
+):
+    shape = img.shape[:2]
+    if isinstance(new_shape, int):
+        new_shape = (new_shape, new_shape)
+
+    r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
+    if not scaleup:
+        r = min(r, 1.0)
+
+    new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
+
+    dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]
+
+    if auto:
+        dw, dh = np.mod(dw, 32), np.mod(dh, 32)
+
+    elif scaleFill:
+        dw, dh = 0.0, 0.0
+        new_unpad = (new_shape[1], new_shape[0])
+
+    dw /= 2
+    dh /= 2
+    if shape[::-1] != new_unpad:
+        img = cv2.resize(img, new_unpad, interpolation=cv2.INTER_AREA)
+
+    top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
+    left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
+    img = cv2.copyMakeBorder(
+        img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color
+    )
+    return img, (dw, dh)
