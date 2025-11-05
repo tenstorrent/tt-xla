38db8d64f3a2843f2474808681d6c8985e458b92
Predrag Ilkic <pilkic@tenstorrent.com>
2025-11-05 20:45:55 +0100

    address comments & close submesh properly
    (cherry picked from commit 19cd3a71f6fadd9be6e881814a4c497593113b49)

--- pjrt_implementation/src/api/module_builder/module_builder.cc
+++ pjrt_implementation/src/api/module_builder/module_builder.cc
@@ -860,11 +860,13 @@ tt_pjrt_status ModuleBuilder::convertFromTTIRToTTNN(
 
   enableVerboseIRPrinting(ttir_to_ttnn_pm);
 
+  // Run the pass manager.
+  mlir::LogicalResult mlir_result = ttir_to_ttnn_pm.run(mlir_module.get());
+
   // Close the optimizer submesh now that the compilation is complete.
   client_instance->closeOptimizerSubmesh();
 
-  // Run the pass manager.
-  if (mlir::failed(ttir_to_ttnn_pm.run(mlir_module.get()))) {
+  if (mlir::failed(mlir_result)) {
     DLOG_F(ERROR, "Failed to convert from TTIR to TTNN module");
     return tt_pjrt_status::kInternal;
   }
