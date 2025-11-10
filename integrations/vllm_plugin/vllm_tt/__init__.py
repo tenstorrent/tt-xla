7007186dc953b1e4d0855ca292aa81d628b67ab4
gengelageTT <gengelage@tenstorrent.com>
2025-11-10 21:40:06 +0000

    set VLLM_TARGET_DEVICE = "empty"

--- integrations/vllm_plugin/vllm_tt/__init__.py
+++ integrations/vllm_plugin/vllm_tt/__init__.py
@@ -8,4 +8,5 @@ import os
 def register():
     # Setting worker multiprocessing method to spawn to avoid hangs in consecutive vllm pytest runs
     os.environ["VLLM_WORKER_MULTIPROC_METHOD"] = "spawn"
+    os.environ["VLLM_TARGET_DEVICE"] = "empty"
     return "vllm_tt.platform.TTPlatform"
