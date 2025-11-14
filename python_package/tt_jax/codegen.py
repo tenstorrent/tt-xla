3e35f9c194bdca65ae66aff70c6e430c0cbe6dd1
Stefan Gligorijevic <189116645+sgligorijevicTT@users.noreply.github.com>
2025-11-14 13:37:40 +0000

    Add convenience wrapper functions for codgen

---
+++ python_package/tt_jax/codegen.py
@@ -0,0 +1,47 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+Helper functions for Codegen(EmitC/EmitPy) specific to Jax.
+"""
+
+from typing import Callable
+
+import jax
+
+
+def codegen_py(
+    func: Callable,
+    *args,
+    compiler_options: dict = {},
+    export_path: str = "codegen_result",
+    export_tensors: bool = True,
+    **kwargs
+):
+    real_compile_options = {
+        **compiler_options,
+        "backend": "codegen_py",
+        "export_path": export_path,
+        "export_tensors": export_tensors,
+    }
+    jax.jit(func, compiler_options=real_compile_options)(*args, **kwargs)
+    return None
+
+
+def codegen_cpp(
+    func: Callable,
+    *args,
+    compiler_options: dict = {},
+    export_path: str = "codegen_result",
+    export_tensors: bool = True,
+    **kwargs
+):
+    real_compile_options = {
+        **compiler_options,
+        "backend": "codegen_cpp",
+        "export_path": export_path,
+        "export_tensors": export_tensors,
+    }
+    jax.jit(func, compiler_options=real_compile_options)(*args, **kwargs)
+    return None
---
+++ python_package/tt_torch/codegen.py
@@ -0,0 +1,68 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+Helper functions for Codegen(EmitC/EmitPy) specific to Torch.
+"""
+
+from typing import Callable
+
+import torch
+import torch.nn as nn
+import torch_xla
+import torch_xla.core.xla_model as xm
+import torch_xla.runtime as xr
+
+# Set up XLA runtime for TT backend.
+xr.set_device_type("TT")
+
+
+def codegen_py(
+    model: nn.Module,
+    *args,
+    compiler_options: dict = {},
+    export_path: str = "codegen_result",
+    export_tensors: bool = True,
+    **kwargs
+):
+    real_compile_options = {
+        **compiler_options,
+        "backend": "codegen_py",
+        "export_path": export_path,
+        "export_tensors": export_tensors,
+    }
+    torch_xla.set_custom_compile_options(real_compile_options)
+    device = xm.xla_device()
+    model.compile(backend="tt")
+    model = model.to(device)
+    args = [arg.to(device) for arg in args if isinstance(arg, torch.Tensor)]
+    kwargs = {k: v.to(device) for k, v in kwargs.items() if isinstance(v, torch.Tensor)}
+    output = model(*args, **kwargs)
+    torch_xla.sync(wait=True)
+    return None
+
+
+def codegen_cpp(
+    model: nn.Module,
+    *args,
+    compiler_options: dict = {},
+    export_path: str = "codegen_result",
+    export_tensors: bool = True,
+    **kwargs
+):
+    real_compile_options = {
+        **compiler_options,
+        "backend": "codegen_cpp",
+        "export_path": export_path,
+        "export_tensors": export_tensors,
+    }
+    torch_xla.set_custom_compile_options(real_compile_options)
+    device = xm.xla_device()
+    model.compile(backend="tt")
+    model = model.to(device)
+    args = [arg.to(device) for arg in args if isinstance(arg, torch.Tensor)]
+    kwargs = {k: v.to(device) for k, v in kwargs.items() if isinstance(v, torch.Tensor)}
+    output = model(*args, **kwargs)
+    torch_xla.sync(wait=True)
+    return None
