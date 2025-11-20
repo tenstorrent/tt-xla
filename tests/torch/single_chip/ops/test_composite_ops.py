94e14845bd6497a2651b0bd64ceb06071e2e39e6
mstojkovicTT <mstojkovic@tenstorrent.com>
2025-11-20 10:32:16 +0000

    add composite rms

--- tests/torch/single_chip/ops/test_composite_ops.py
+++ tests/torch/single_chip/ops/test_composite_ops.py
@@ -8,7 +8,7 @@ import torch_xla.core.xla_model as xm
 from infra.comparators.torch_comparator import TorchComparator
 from infra.utilities.types import Framework
 from torch.nn import functional as F
-from tt_torch.composite_ops import composite_gelu
+from tt_torch.composite_ops import composite_gelu, composite_rms_norm
 
 from tests.infra.comparators.comparison_config import ComparisonConfig
 from tests.infra.testers.single_chip.op.op_tester import run_op_test_with_random_inputs
@@ -127,3 +127,62 @@ def test_patched_gelu_op_test(approx):
     run_op_test_with_random_inputs(
         gelu_with_approx, [(32, 32)], framework=Framework.TORCH
     )
+
+
+@pytest.mark.parametrize("use_weight", [True, False])
+def test_rmsnorm(use_weight):
+
+    class RMSNormModel(torch.nn.Module):
+        def __init__(self, normalized_shape):
+            super().__init__()
+            self.normalized_shape = normalized_shape
+
+        def forward(self, x, weight):
+            return torch.nn.functional.rms_norm(x, self.normalized_shape, weight)
+
+    normalized_shape = (32,)
+    input_shape = (4, 32)
+    input_tensor = torch.randn(input_shape)
+
+    weight = torch.randn(normalized_shape) if use_weight else None
+
+    model = RMSNormModel(normalized_shape)
+    golden = model(input_tensor, weight if use_weight else None)
+
+    device = xm.xla_device()
+    model = torch.compile(model.to(device), backend="tt")
+
+    output = model(input_tensor.to(device), weight.to(device) if use_weight else None)
+
+    comparator = TorchComparator(ComparisonConfig())
+    comparator.compare(output, golden)
+
+
+@pytest.mark.parametrize("use_weight", [True, False])
+def test_composite_rms_norm(use_weight):
+    """
+    Tests example model that has a composite RMS norm operation.
+    """
+
+    class RMSNormModel(torch.nn.Module):
+        def __init__(self, normalized_shape):
+            super().__init__()
+            self.normalized_shape = normalized_shape
+
+        def forward(self, x, weight):
+            return composite_rms_norm(x, self.normalized_shape, weight)
+
+    normalized_shape = (32,)
+    input_shape = (4, 32)
+    input_tensor = torch.randn(input_shape)
+    weight = torch.randn(normalized_shape) if use_weight else None
+
+    model = RMSNormModel(normalized_shape)
+    golden = model(input_tensor, weight if use_weight else None)
+
+    device = xm.xla_device()
+    model = torch.compile(model.to(device), backend="tt")
+    output = model(input_tensor.to(device), weight.to(device) if use_weight else None)
+
+    comparator = TorchComparator(ComparisonConfig())
+    comparator.compare(output, golden)
