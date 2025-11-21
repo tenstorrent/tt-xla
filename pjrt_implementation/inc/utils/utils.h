d06d831ab6caa90fcde38f3f899297f22c15e302
Aleksandar Colic <acolic@tenstorrent.com>
2025-11-21 17:24:14 +0100

    [bugfix] Closing device on tt::runtime::submit failure (#2108)
    It might happen that tt::runtime::submit fails (sometimes with
    exceptions from metal). Since those exceptions are never caught, we are
    leaving mesh device open and in potentially bad state. With this change,
    we will always close mesh device on tt::runtime::submit failure.
    
    One concrete problem occurs when buffer allocations fails in metal,
    which leaves allocator in a bad state. Next time allocator is called (in
    the next test run within the same process, for example), it will fail
    with the same allocation error even if the memory is available.
    
    In the future, we might want to wrap all calls to runtime with
    introduced function wrapper (std::invoke) and close mesh device on
    failure.
    
    Issue: https://github.com/tenstorrent/tt-xla/issues/1940

--- pjrt_implementation/inc/utils/utils.h
+++ pjrt_implementation/inc/utils/utils.h
@@ -5,7 +5,12 @@
 #ifndef TT_XLA_PJRT_IMPLEMENTATION_INC_UTILS_UTILS_H_
 #define TT_XLA_PJRT_IMPLEMENTATION_INC_UTILS_UTILS_H_
 
+#include "logging.h"
+#include <functional>
+#include <optional>
 #include <sstream>
+#include <type_traits>
+#include <variant>
 #include <vector>
 
 namespace tt::pjrt::utils {
@@ -19,6 +24,47 @@ template <typename T> std::string to_string(const std::vector<T> vec) {
   }
   res << "]";
   return res.str();
+}
+
+// Invokes function with arguments inside try/catch block.
+// Return type is std::optional with derived type from calling function, so user
+// can check return value for nullopt (or with implicit bool operator).
+// If function return type is void, std::optional will hold std::monostate.
+//
+// Example usage:
+// std::vector<int> my_fn(int first, int second);
+//
+// auto r = utils::invoke(my_fn, arg1, arg2);
+// if (!r)
+//  ... failed - handle error ....
+//
+// or keep going if successful:
+// std::vector<int>& output = *r;
+// ...
+//
+// Also, you can invoke it with lambda:
+// auto r = utils::invoke([&] { return my_fn(arg1, arg2); });
+//
+template <class Fn, class... Args,
+          class ReturnType = std::invoke_result_t<Fn, Args...>>
+std::optional<std::conditional_t<std::is_same_v<ReturnType, void>,
+                                 std::monostate, ReturnType>>
+invoke(Fn &&fn, Args &&...args) {
+  try {
+    if constexpr (std::is_same_v<ReturnType, void>) {
+      std::invoke(std::forward<Fn>(fn), std::forward<Args>(args)...);
+      return std::optional{std::monostate{}};
+    } else {
+      return std::optional{
+          std::invoke(std::forward<Fn>(fn), std::forward<Args>(args)...)};
+    }
+  } catch (const std::exception &ex) {
+    DLOG_F(ERROR, "Exception:\n{%s}\n", ex.what());
+  } catch (...) {
+    DLOG_F(ERROR, "Unknown exception.");
+  }
+
+  return std::nullopt;
 }
 
 } // namespace tt::pjrt::utils
