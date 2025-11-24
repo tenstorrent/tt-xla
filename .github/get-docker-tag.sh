a01d8820d40c8b2d742a6e0d901076a5466373f3
Nebojsa Sumrak <nsumrak@tenstorrent.com>
2025-11-24 12:31:14 +0100

    check if mlir docker tag is supplied

--- .github/get-docker-tag.sh
+++ .github/get-docker-tag.sh
@@ -8,6 +8,13 @@
 
 # Exit immediately if a command exits with a non-zero status
 set -e
+
+# Check if parameter $1 is provided
+if [ -z "$1" ]; then
+	echo "Error: MLIR_DOCKER_TAG parameter is required" >&2
+	exit 1
+fi
+
 MLIR_DOCKER_TAG=$1
 DOCKERFILE_HASH=$( (cat .github/Dockerfile.base .github/Dockerfile.ci venv/requirements-dev.txt python_package/requirements.txt | sha256sum) | cut -d ' ' -f 1)
 COMBINED_HASH=$( (echo $DOCKERFILE_HASH $MLIR_DOCKER_TAG | sha256sum) | cut -d ' ' -f 1)
