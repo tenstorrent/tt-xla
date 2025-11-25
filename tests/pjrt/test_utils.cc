180e744d6998aca10a4b87e1af7eb62b4dc67b06
acicovicTT <acicovic@tenstorrent.com>
2025-11-25 14:01:02 +0000

    Fix linter errors

--- tests/pjrt/test_utils.cc
+++ tests/pjrt/test_utils.cc
@@ -39,9 +39,7 @@ protected:
     std::filesystem::remove(TMP_LOG_FILE_NAME);
   }
 
-  void TearDown() override {
-    std::filesystem::remove(TMP_LOG_FILE_NAME);
-  }
+  void TearDown() override { std::filesystem::remove(TMP_LOG_FILE_NAME); }
 };
 
 const char *PJRTUtilLoggingTests::TMP_LOG_FILE_NAME = "tmp_test.log";
@@ -51,63 +49,55 @@ const char *PJRTUtilLoggingTests::ENV_VAR_LOG_FILE_PATH = "TT_XLA_LOGGER_FILE";
 const char *PJRTUtilLoggingTests::ENV_VAR_LOGGER_LEVEL = "LOGGER_LEVEL";
 
 const char *PJRTUtilLoggingTests::ENV_VAR_BRINGUP_LOGGING =
-  "ENABLE_BRINGUP_STAGE_LOGGING";
-
+    "ENABLE_BRINGUP_STAGE_LOGGING";
 
 // ---------- Tests ------------------------------------------------------------
 
 TEST(PJRTUtilUnitTests, Test_getPJRTBufferTypeString_defaultCase) {
   PJRT_Buffer_Type invalid_type = static_cast<PJRT_Buffer_Type>(~0U);
-  EXPECT_EQ(
-    tt::pjrt::data_type_utils::getPJRTBufferTypeString(invalid_type),
-    "UNKNOWN_TYPE");
+  EXPECT_EQ(tt::pjrt::data_type_utils::getPJRTBufferTypeString(invalid_type),
+            "UNKNOWN_TYPE");
 }
 
 TEST(PJRTUtilUnitTests, Test_convertRuntimeToPJRTDataType_failCase) {
   const std::vector<tt::target::DataType> unsupported_types = {
-    tt::target::DataType::Bool,
-    tt::target::DataType::Int8,
-    tt::target::DataType::Int16,
-    tt::target::DataType::Int64,
-    tt::target::DataType::UInt64,
-    tt::target::DataType::Float64,
+      tt::target::DataType::Bool,   tt::target::DataType::Int8,
+      tt::target::DataType::Int16,  tt::target::DataType::Int64,
+      tt::target::DataType::UInt64, tt::target::DataType::Float64,
   };
   for (tt::target::DataType unsupported : unsupported_types) {
     EXPECT_THROW(
-      tt::pjrt::data_type_utils::convertRuntimeToPJRTDataType(unsupported),
-      std::runtime_error);
+        tt::pjrt::data_type_utils::convertRuntimeToPJRTDataType(unsupported),
+        std::runtime_error);
   }
 }
 
 TEST(PJRTUtilUnitTests, Test_convertPJRTToRuntimeDataType_failCase) {
   const std::vector<PJRT_Buffer_Type> unsupported_types = {
-    PJRT_Buffer_Type_INVALID,
-    PJRT_Buffer_Type_F8E5M2,
-    PJRT_Buffer_Type_F8E4M3FN,
-    PJRT_Buffer_Type_F8E4M3B11FNUZ,
+      PJRT_Buffer_Type_INVALID,
+      PJRT_Buffer_Type_F8E5M2,
+      PJRT_Buffer_Type_F8E4M3FN,
+      PJRT_Buffer_Type_F8E4M3B11FNUZ,
   };
   for (PJRT_Buffer_Type unsupported : unsupported_types) {
     EXPECT_THROW(
-      tt::pjrt::data_type_utils::convertPJRTToRuntimeDataType(unsupported),
-      std::runtime_error);
+        tt::pjrt::data_type_utils::convertPJRTToRuntimeDataType(unsupported),
+        std::runtime_error);
   }
 }
 
 TEST(PJRTUtilUnitTests, Test_convertDataTypeBetweenPJRTAndRuntime_successCase) {
   const std::vector<tt::target::DataType> supported_types_in_both = {
-    tt::target::DataType::UInt8,
-    tt::target::DataType::UInt16,
-    tt::target::DataType::UInt32,
-    tt::target::DataType::Int32,
-    tt::target::DataType::Float16,
-    tt::target::DataType::Float32,
-    tt::target::DataType::BFloat16,
+      tt::target::DataType::UInt8,    tt::target::DataType::UInt16,
+      tt::target::DataType::UInt32,   tt::target::DataType::Int32,
+      tt::target::DataType::Float16,  tt::target::DataType::Float32,
+      tt::target::DataType::BFloat16,
   };
   for (tt::target::DataType runtime_type : supported_types_in_both) {
-    EXPECT_EQ(
-      runtime_type,
-      tt::pjrt::data_type_utils::convertPJRTToRuntimeDataType(
-        tt::pjrt::data_type_utils::convertRuntimeToPJRTDataType(runtime_type)));
+    EXPECT_EQ(runtime_type,
+              tt::pjrt::data_type_utils::convertPJRTToRuntimeDataType(
+                  tt::pjrt::data_type_utils::convertRuntimeToPJRTDataType(
+                      runtime_type)));
   }
 }
 
