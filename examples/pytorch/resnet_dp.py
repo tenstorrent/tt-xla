12c9508b20f59b93cdb29103530a27675a2a35e8
gengelageTT <gengelage@tenstorrent.com>
2025-11-25 19:17:34 +0000

    resnet dp example working

--- examples/pytorch/resnet_dp.py
+++ examples/pytorch/resnet_dp.py
@@ -7,61 +7,81 @@ import os
 import numpy as np
 import torch
 import torch_xla
-import torch_xla.core.xla_model as xm
 import torch_xla.distributed.spmd as xs
 import torch_xla.runtime as xr
+from PIL import Image
 from torch_xla.distributed.spmd import Mesh
 from transformers import AutoImageProcessor, ResNetForImageClassification
+
+from third_party.tt_forge_models.tools.utils import get_file
 
 
 # --------------------------------
 # Test run
 # --------------------------------
 def resnet_dp():
+    # Setup SPMD and get number of devices.
+    os.environ["CONVERT_SHLO_TO_SHARDY"] = "1"
+    xr.use_spmd()
+
+    num_devices = xr.global_runtime_device_count()
+
     # Instantiate model.
     model_name = "microsoft/resnet-50"
     model: torch.nn.Module = ResNetForImageClassification.from_pretrained(model_name)
     model = model.to(torch.bfloat16)
-    model = model.eval()
 
-    # Instantiate image processor.
+    # Put it in inference mode and compile it.
+    model = model.eval()
+    compiled_model = torch.compile(model, backend="tt")
+
+    # Prepare inputs.
     image_processor = AutoImageProcessor.from_pretrained(
         model_name, dtype=torch.bfloat16
     )
 
-    # Setup SPMD.
-    os.environ["CONVERT_SHLO_TO_SHARDY"] = "1"
-    xr.use_spmd()
+    image_file = get_file("http://images.cocodataset.org/val2017/000000039769.jpg")
+    images = [
+        Image.open(image_file).convert("RGB")
+    ] * num_devices  # batch_size = num_devices
+    inputs = image_processor(images=images, return_tensors="pt").pixel_values
+    inputs = inputs.to(torch.bfloat16)
 
-    # Create mesh.
-    num_devices = xr.global_runtime_device_count()
+    # Create a mesh.
     mesh_shape = (1, num_devices)
     device_ids = np.array(range(num_devices))
     mesh = Mesh(device_ids, mesh_shape, ("model", "batch"))
 
-    # Connect the device.
-    device = xm.xla_device()
-
-    # Generate dummy inputs with batch size of num devices.
-    batch_size = num_devices
-    images = torch.randn(batch_size, 3, 224, 224)
-    inputs = image_processor(images=images, return_tensors="pt").pixel_values
-
     # Move inputs and model to device.
+    device = torch_xla.device()
+
     inputs = inputs.to(device)
-    model = model.to(device)
+    compiled_model = compiled_model.to(device)
 
     # Mark sharding for inputs along batch dimension.
     xs.mark_sharding(inputs, mesh, ("batch", None, None, None))
 
-    # Compile model
-    compiled_model = torch.compile(model, backend="tt")
-
-    # Run model (with no gradient calculation since we only need inference).
+    # Run model.
     with torch.no_grad():
         output = compiled_model(inputs)
 
-    print(output)
+    # Post-process outputs.
+    post_process_output(output, model.config)
+
+
+def post_process_output(output, config):
+    logits = output.logits.cpu()
+    probabilities = torch.softmax(logits, dim=-1)
+    top_5_probs, top_5_indices = torch.topk(probabilities, k=5)
+
+    print(f"Processing {logits.shape[0]} batch items:")
+    for batch_idx in range(logits.shape[0]):
+        print(f"\nInput {batch_idx + 1} - Top 5 predictions:")
+        for i in range(5):
+            idx = top_5_indices[batch_idx, i].item()
+            prob = top_5_probs[batch_idx, i].item() * 100
+            label = config.id2label[idx]
+            print(f"{i+1}. {label}: {prob:.2f}%")
 
 
 # --------------------------------
