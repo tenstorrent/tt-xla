5c1fc32e8faffe43d612f3ec18b37d7ef8a9cacb
gengelageTT <gengelage@tenstorrent.com>
2025-12-02 16:49:59 +0000

    update test imports

--- tests/torch/single_host/ops/test_composite_ops.py
+++ tests/torch/single_host/ops/test_composite_ops.py
@@ -1,10 +1,15 @@
 # SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
 #
 # SPDX-License-Identifier: Apache-2.0
+import os
 
+import numpy as np
 import pytest
 import torch
+import torch_xla
 import torch_xla.core.xla_model as xm
+import torch_xla.distributed.spmd as xs
+import torch_xla.runtime as xr
 from infra.comparators.torch_comparator import TorchComparator
 from infra.utilities.types import Framework
 from torch.nn import functional as F
