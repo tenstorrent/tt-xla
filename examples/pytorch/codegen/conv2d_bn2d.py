a08177bb430bcca34eca532d26b6494b2f278c06
kkannan <kkannan@tenstorrent.com>
2025-12-04 08:23:03 +0000

    batchnorm absent in python code generated by emitpy

---
+++ examples/pytorch/codegen/conv2d_bn2d.py
@@ -0,0 +1,36 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+Demonstrates how to hook into compile options to use Codegen, from Torch
+"""
+
+import os
+
+import torch
+import torch.nn as nn
+import torch_xla
+import torch_xla.core.xla_model as xm
+import torch_xla.runtime as xr
+from tt_torch import codegen_py
+
+from third_party.tt_forge_models.openpose.v2.pytorch import ModelLoader
+
+# Set up XLA runtime for TT backend.
+xr.set_device_type("TT")
+
+
+# Any compile options you could specify when executing the model normally can also be used with codegen.
+extra_options = {
+    # "enable_optimizer": True,
+    # "enable_memory_layout_analysis": True,
+    # "enable_l1_interleaved": False,
+}
+
+loader = ModelLoader()
+model = loader.load_model(dtype_override=torch.bfloat16)
+model.eval()
+x =  torch.load('s1_p_ip.pt',map_location="cpu")
+
+codegen_py(model, x, export_path="conv2d_batchnorm2d_issue", compiler_options=extra_options)
