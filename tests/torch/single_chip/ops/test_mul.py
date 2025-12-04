a343d0192b5acbfb697b23d27233a7cdf10320e8
gengelageTT <gengelage@tenstorrent.com>
2025-12-04 22:16:45 +0000

    add unit tests for np errors

--- tests/torch/single_chip/ops/test_mul.py
+++ tests/torch/single_chip/ops/test_mul.py
@@ -2,10 +2,14 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+import numpy as np
 import pytest
 import torch
-from infra import Framework, run_op_test_with_random_inputs
+from infra import Framework, run_op_test, run_op_test_with_random_inputs
+from infra.comparators.torch_comparator import TorchComparator
 from utils import Category
+
+from tests.infra.comparators.comparison_config import ComparisonConfig
 
 
 @pytest.mark.push
@@ -21,3 +25,35 @@ def test_mul():
     run_op_test_with_random_inputs(
         mul, [(32, 32), (32, 32)], dtype=torch.bfloat16, framework=Framework.TORCH
     )
+
+
+@pytest.mark.nightly
+@pytest.mark.single_device
+@pytest.mark.record_test_properties(category=Category.OP_TEST)
+def test_numpy_inplace_multiply_torch_override():
+    """
+    Test that TorchFunctionOverride works correctly for numpy array inplace multiplication operations.
+    """
+    from tt_torch.torch_overrides import torch_function_override
+
+    def inplace_multiply(T):
+        T[:2, :] *= 8
+        return T
+
+    input_T = np.array([[0, -1, 16], [-1, 0, 32], [0, 0, 1]], dtype=np.float32)
+
+    # Temporarily disable the override to get golden output
+    torch_function_override.__exit__(None, None, None)
+    try:
+        golden_compiled_op = torch.compile(inplace_multiply)
+        golden = golden_compiled_op(input_T)
+        golden = torch.from_numpy(golden)
+    finally:
+        torch_function_override.__enter__()  # Always re-enable it
+
+    compiled_op = torch.compile(inplace_multiply)
+    output = compiled_op(input_T)
+    output = torch.from_numpy(output)
+
+    comparator = TorchComparator(ComparisonConfig())
+    comparator.compare(output, golden)
