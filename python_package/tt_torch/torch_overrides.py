a27d613541c0306f62fd6ec9b0d45ba1fdab4918
gengelageTT <gengelage@tenstorrent.com>
2025-12-04 20:58:44 +0000

    skip override for numpy arrays and only call is_compiling() for matmul/linear

--- python_package/tt_torch/torch_overrides.py
+++ python_package/tt_torch/torch_overrides.py
@@ -1,15 +1,26 @@
 # SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
 #
 # SPDX-License-Identifier: Apache-2.0
+import numpy as np
 import torch
 from torch.overrides import TorchFunctionMode
 
 
 class TorchFunctionOverride(TorchFunctionMode):
     def __torch_function__(self, func, types, args, kwargs=None):
-        if not torch.compiler.is_compiling() and (
+        # When torch.compile/dynamo traces operations on numpy arrays, it wraps them
+        # with torch's fake tensor machinery. The override being active causes dynamo
+        # to treat numpy operations as torch operations, occasionally causing errors.
+        #
+        # Return NotImplemented to use the default numpy handlers instead
+        if args:
+            for arg in args:
+                if isinstance(arg, np.ndarray):
+                    return NotImplemented
+
+        if (
             func.__name__ == "matmul" or func.__name__ == "linear"
-        ):
+        ) and not torch.compiler.is_compiling():
             if len(args[0].shape) >= 4 or len(args[1].shape) >= 4:
                 if func.__name__ == "linear":
                     # Linear function transposes args[1]
