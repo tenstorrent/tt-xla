b2e2a1be9b49bbbbbdf973c7afcbd9282babb539
Aleks Knezevic <aknezevic@tenstorrent.com>
2025-12-05 16:42:20 +0000

    Another repro

--- tests/torch/single_chip/graphs/test_attention.py
+++ tests/torch/single_chip/graphs/test_attention.py
@@ -21,6 +21,7 @@ from transformers.models.gemma.modeling_gemma import GemmaAttention
 from transformers.models.llama.modeling_llama import (
     ALL_ATTENTION_FUNCTIONS,
     LlamaAttention,
+    LlamaDecoderLayer,
     eager_attention_forward,
 )
 from transformers.models.mistral.modeling_mistral import MistralAttention
@@ -142,6 +143,82 @@ def parametrize_is_llmbox():
 
 """Llama attention tests"""
 
+@pytest.mark.nightly
+@parametrize_is_llmbox()  # True for llmbox, False for single device
+@pytest.mark.parametrize("seq_len", [512])
+@pytest.mark.parametrize(
+    "variant,variant_config",
+    get_available_variants("llama").items(),
+    ids=[str(k) for k in get_available_variants("llama").keys()],
+)
+def test_llama_embedding_decode_layer(seq_len, variant, variant_config, is_llmbox):
+    if "70b" in str(variant) and not is_llmbox:
+        pytest.skip("70B models don't fit on a single device")
+
+    xr.set_device_type("TT")
+
+    loader = LlamaModelLoader(variant=variant)
+    config = loader.load_config()
+    class Wrapper(torch.nn.Module):
+        def __init__(self):
+            super().__init__()
+            self.embed_tokens = torch.nn.Embedding(config.vocab_size, config.hidden_size, 0)
+
+            self.decoder_layer = LlamaDecoderLayer(config, layer_idx=0).to(torch.bfloat16)
+
+        def forward(self, input_ids, position_embeddings, attention_mask, past_key_state):
+            embedding = self.embed_tokens(input_ids)
+            return self.decoder_layer(embedding, attention_mask=attention_mask, position_embeddings=position_embeddings, past_key_state=past_key_state)
+
+    wrapper = Wrapper().to(torch.bfloat16)
+    batch_size = 1
+    num_heads = config.num_attention_heads
+
+    if is_llmbox:
+        num_devices = xr.global_runtime_device_count()
+        device_ids = np.array(range(num_devices))
+
+        batch_size = 2
+        mesh_shape = (2, num_devices // 2)
+        mesh = Mesh(device_ids, mesh_shape, ("batch", "model"))
+
+        def get_shard_spec(attention, args, kwargs):
+            shard_specs = {}
+            shard_specs[args[0]] = ("batch", None)  # input_ids
+            shard_specs[args[1][0]] = ("batch", None, None)  # cos
+            shard_specs[args[1][1]] = ("batch", None, None)  # sin
+            shard_specs[args[2]] = ("batch", None, None, None)  # mask
+            shard_specs[wrapper.embed_tokens.weight] = (None, "model")
+            shard_specs[wrapper.decoder_layer.self_attn.q_proj.weight] = ("model", None)
+            shard_specs[wrapper.decoder_layer.self_attn.k_proj.weight] = ("model", None)
+            shard_specs[wrapper.decoder_layer.self_attn.v_proj.weight] = ("model", None)
+            shard_specs[wrapper.decoder_layer.self_attn.o_proj.weight] = (None, "model")
+            shard_specs[wrapper.decoder_layer.mlp.gate_proj.weight] = ("model", None)
+            shard_specs[wrapper.decoder_layer.mlp.up_proj.weight] = ("model", None)
+            shard_specs[wrapper.decoder_layer.mlp.down_proj.weight] = ("model", None)
+            return shard_specs
+
+    else:
+        mesh = None
+        get_shard_spec = None
+
+    input_ids = torch.randint(0, config.vocab_size, (batch_size, seq_len))
+    cos_sin = torch.rand(batch_size, seq_len, config.head_dim, dtype=torch.bfloat16)
+    position_embeddings = (cos_sin, cos_sin)
+    attention_mask = torch.rand(batch_size, 1, seq_len, seq_len, dtype=torch.bfloat16)
+
+    past_key_states = None
+
+    comparison_config = ComparisonConfig(pcc=PccConfig(required_pcc=0.98))
+
+    run_graph_test(
+        wrapper,
+        [input_ids, position_embeddings, attention_mask, past_key_states],
+        framework=Framework.TORCH,
+        comparison_config=comparison_config,
+        mesh=mesh,
+        shard_spec_fn=get_shard_spec,
+    )
 
 @pytest.mark.nightly
 @parametrize_is_llmbox()  # True for llmbox, False for single device
