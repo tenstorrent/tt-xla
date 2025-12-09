faa152c22df126adecc31ff8027f96c45d845ca0
ssalice <ssalice@tenstorrent.com>
2025-12-09 16:10:31 +0000

    disabled perf measurement in test_multihost for now.

--- tests/torch/multi_host/llmbox/test_multihost.py
+++ tests/torch/multi_host/llmbox/test_multihost.py
@@ -52,6 +52,7 @@ def test_multihost_models(model_variant):
             "pytest",
             "-ssv",
             f"tests/runner/test_models.py::test_all_models_torch[{model_variant}]",
+            "--disable-perf-measurement",
         ],
         env=distributed_env,
         cwd=os.getcwd(),
