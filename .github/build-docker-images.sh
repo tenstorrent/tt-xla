c153043a8458804dfdb2d655a98272e1c7e911a9
Nebojsa Sumrak <nsumrak@tenstorrent.com>
2025-12-09 14:04:41 +0100

    fix

--- .github/build-docker-images.sh
+++ .github/build-docker-images.sh
@@ -32,9 +32,7 @@ fi
 
 if [ "$CHECK_ONLY" = false ]; then
     echo "Ensure tt-mlir docker images with tag: $MLIR_DOCKER_TAG exist"
-    ./.github/build-docker-images.sh ci  --check-only
-    EXIT_CODE=$?
-    if [ $EXIT_CODE -ne 0 ]; then
+    if ! ./.github/build-docker-images.sh ci --check-only; then
         echo -e "\033[31mDocker image does not exist.\033[0m"
         echo -e "\033[31mYou should build tt-mlir docker image for sha $tt_mlir_sha first, and then rerun the tt-xla workflow.\033[0m"
         exit $EXIT_CODE
