0168e1d852504530de7f318b2f53787a0945e93f
mstojkovicTT <mstojkovic@tenstorrent.com>
2025-12-15 14:58:36 +0000

    add support for torch_options

--- tests/infra/testers/single_chip/graph/graph_tester.py
+++ tests/infra/testers/single_chip/graph/graph_tester.py
@@ -33,12 +33,13 @@ def run_graph_test(
     framework: Framework = Framework.JAX,
     mesh: Optional[Mesh] = None,
     shard_spec_fn: Optional[Callable] = None,
+    torch_options: dict = None,
 ) -> None:
     """
     Tests `graph` with `inputs` by running it on TT device and CPU and comparing the
     results based on `comparison_config`.
     """
-    tester = GraphTester(comparison_config, framework)
+    tester = GraphTester(comparison_config, framework, torch_options=torch_options)
     if framework == Framework.TORCH:
         workload = TorchWorkload(
             model=graph, args=inputs, mesh=mesh, shard_spec_fn=shard_spec_fn
@@ -54,10 +55,11 @@ def run_graph_test_with_random_inputs(
     comparison_config: ComparisonConfig = ComparisonConfig(),
     framework: Framework = Framework.JAX,
     dtype: str | DTypeLike | torch.dtype = "float32",
+    torch_options: dict = None,
 ) -> None:
     """
     Tests `graph` with random inputs by running it on TT device and CPU and comparing
     the results based on `comparison_config`.
     """
-    tester = GraphTester(comparison_config, framework)
+    tester = GraphTester(comparison_config, framework, torch_options=torch_options)
     tester.test_with_random_inputs(graph, input_shapes, dtype=dtype)
