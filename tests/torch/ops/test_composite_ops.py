2438b906f0eaa51a617661b04561b76776b4d3fa
mstojkovicTT <mstojkovic@tenstorrent.com>
2025-12-17 16:10:26 +0000

    init

--- tests/torch/ops/test_composite_ops.py
+++ tests/torch/ops/test_composite_ops.py
@@ -29,11 +29,7 @@ from tests.utils import parametrize_arch
 @pytest.mark.single_device
 @pytest.mark.parametrize("approx", ["none", "tanh"])
 def test_composite_gelu(approx):
-    """
-    Tests example model that has a composite gelu operation.
-    """
-
-    class MM(torch.nn.Module):
+    class GeluModel(torch.nn.Module):
         def forward(self, x):
             return composite_gelu(x, approx)
 
@@ -41,118 +37,44 @@ def test_composite_gelu(approx):
 
     input = torch.randn(32, 32)
 
-    model = MM()
-    golden = model(input)
+    model = GeluModel()
 
-    device = xm.xla_device()
-    model = torch.compile(model.to(device), backend="tt", options=options)
-
-    output = model(input.to(device))
-
-    comparator = TorchComparator(ComparisonConfig())
-    comparator.compare(output, golden)
-
-
-@pytest.mark.single_device
-@pytest.mark.parametrize("approx", ["none", "tanh"])
-def test_composite_gelu_eager(approx):
-    """
-    Tests example model in eager mode that has a composite gelu operation.
-    """
-
-    class MM(torch.nn.Module):
-        def forward(self, x):
-            return composite_gelu(x, approx)
-
-    input = torch.randn(32, 32)
-
-    model = MM()
-    golden = model(input)
-
-    device = xm.xla_device()
-    model = model.to(device)
-    input = input.to(device)
-
-    output = model(input).to("cpu")
-
-    comparator = TorchComparator(ComparisonConfig())
-    comparator.compare(output, golden)
+    # Disable inplace buffers for inductor compilation
+    # so that we can compare the results with the golden model.
+    with torch._inductor.config.patch({"inplace_buffers": False}):
+        run_graph_test(
+            model,
+            [input],
+            comparison_config=ComparisonConfig(),
+            framework=Framework.TORCH,
+            torch_options=options,
+        )
 
 
 @pytest.mark.single_device
 @pytest.mark.parametrize("approx", ["none", "tanh"])
-def test_patched_gelu(approx):
-    """
-    Tests example model that has a gelu operation (replaced with our composite gelu
-    operation in (tt-xla/python_package/torch_plugin_tt/__init__.py).
-    """
-
-    class MM(torch.nn.Module):
+def test_patched_gelu_functional(approx):
+    class GeluModel(torch.nn.Module):
         def forward(self, x):
             return F.gelu(input=x, approximate=approx)
 
     options = {"tt_enable_composite_ops": True}
 
     input = torch.randn(32, 32)
+    model = GeluModel()
 
-    model = MM()
-    golden = model(input)
-
-    device = xm.xla_device()
-    model = torch.compile(model.to(device), backend="tt", options=options)
-
-    output = model(input.to(device))
-
-    comparator = TorchComparator(ComparisonConfig())
-    comparator.compare(output, golden)
-
-
-@pytest.mark.single_device
-@pytest.mark.parametrize("approx", ["none", "tanh"])
-def test_patched_gelu_eager(approx):
-    """
-    Tests example model in eager mode that has a gelu operation (replaced with our composite gelu
-    operation in (tt-xla/python_package/torch_plugin_tt/__init__.py).
-    """
-
-    class MM(torch.nn.Module):
-        def forward(self, x):
-            return F.gelu(input=x, approximate=approx)
-
-    input = torch.randn(32, 32)
-
-    model = MM()
-    golden = model(input)
-
-    device = xm.xla_device()
-    model = model.to(device)
-    input = input.to(device)
-
-    output = model(input).to("cpu")
-
-    comparator = TorchComparator(ComparisonConfig())
-    comparator.compare(output, golden)
-
-
-@pytest.mark.single_device
-@pytest.mark.parametrize("approx", ["none", "tanh"])
-def test_patched_gelu_op_test(approx):
-    """
-    Tests torch gelu operation (replaced with our composite gelu operation in
-    (tt-xla/python_package/torch_plugin_tt/__init__.py) using run_op_test_with_random_inputs utility.
-    """
-
-    def gelu_with_approx(x):
-        return F.gelu(x, approximate=approx)
-
-    run_op_test_with_random_inputs(
-        gelu_with_approx, [(32, 32)], framework=Framework.TORCH
+    run_graph_test(
+        model,
+        [input],
+        comparison_config=ComparisonConfig(),
+        framework=Framework.TORCH,
+        torch_options=options,
     )
 
 
-@parametrize_arch(["single_device", "dual_chip"])
+@pytest.mark.single_device
 @pytest.mark.parametrize("use_weight", [True, False])
-def test_rmsnorm(use_weight, arch):
+def test_patched_rms_norm_functional_single_device(use_weight):
 
     class RMSNormModel(torch.nn.Module):
         def __init__(self, normalized_shape):
@@ -167,47 +89,21 @@ def test_rmsnorm(use_weight, arch):
     normalized_shape = (32,)
     input_shape = (4, 32)
     input_tensor = torch.randn(input_shape)
-
     weight = torch.randn(normalized_shape) if use_weight else None
-
     model = RMSNormModel(normalized_shape)
-    golden = model(input_tensor, weight if use_weight else None)
 
-    device = xm.xla_device()
-    model = torch.compile(model.to(device), backend="tt", options=options)
-
-    input_tensor = input_tensor.to(device)
-    weight = weight.to(device) if use_weight else None
-
-    if arch == "dual_chip":
-        # Set SPMD mode and get number of devices.
-        os.environ["CONVERT_SHLO_TO_SHARDY"] = "1"
-        xr.use_spmd()
-
-        num_devices = xr.global_runtime_device_count()
-
-        # Create a mesh.
-        mesh_shape = (1, num_devices)
-        device_ids = np.array(range(num_devices))
-        mesh = xs.Mesh(device_ids, mesh_shape, ("model", "batch"))
-
-        # Mark sharding for inputs along batch dimension.
-        xs.mark_sharding(input_tensor, mesh, ("batch", None))
-        if use_weight:
-            xs.mark_sharding(weight, mesh, (None,))
-
-    output = model(input_tensor, weight)
-
-    comparator = TorchComparator(ComparisonConfig())
-    comparator.compare(output, golden)
+    run_graph_test(
+        model,
+        [input_tensor, weight],
+        comparison_config=ComparisonConfig(),
+        framework=Framework.TORCH,
+        torch_options=options,
+    )
 
 
-@pytest.mark.single_device
+@pytest.mark.dual_chip
 @pytest.mark.parametrize("use_weight", [True, False])
-def test_composite_rms_norm(use_weight):
-    """
-    Tests example model that has a composite RMS norm operation.
-    """
+def test_patched_rms_norm_functional_batch_parallel(use_weight):
 
     class RMSNormModel(torch.nn.Module):
         def __init__(self, normalized_shape):
@@ -215,24 +111,70 @@ def test_composite_rms_norm(use_weight):
             self.normalized_shape = normalized_shape
 
         def forward(self, x, weight):
-            return composite_rms_norm(x, self.normalized_shape, weight)
+            return torch.nn.functional.rms_norm(x, self.normalized_shape, weight)
 
-    options = {"tt_enable_composite_ops": False}
+    options = {"tt_enable_composite_ops": True}
 
     normalized_shape = (32,)
     input_shape = (4, 32)
     input_tensor = torch.randn(input_shape)
     weight = torch.randn(normalized_shape) if use_weight else None
-
     model = RMSNormModel(normalized_shape)
-    golden = model(input_tensor, weight if use_weight else None)
 
-    device = xm.xla_device()
-    model = torch.compile(model.to(device), backend="tt", options=options)
-    output = model(input_tensor.to(device), weight.to(device) if use_weight else None)
+    # Create a mesh.
+    num_devices = xr.global_runtime_device_count()
+    mesh_shape = (1, num_devices)
+    device_ids = np.array(range(num_devices))
+    mesh = xs.Mesh(device_ids, mesh_shape, ("model", "batch"))
 
-    comparator = TorchComparator(ComparisonConfig())
-    comparator.compare(output, golden)
+    # Mark sharding for inputs along batch dimension.
+    shard_specs = {}
+    shard_specs[input_tensor] = ("batch", None)
+    if use_weight:
+        shard_specs[weight] = (None,)
+
+    run_graph_test(
+        model,
+        [input_tensor, weight],
+        comparison_config=ComparisonConfig(),
+        framework=Framework.TORCH,
+        torch_options=options,
+        mesh=mesh,
+        shard_spec_fn=shard_specs,
+    )
+
+
+@pytest.mark.single_device
+@pytest.mark.parametrize("use_weight", [True, False])
+@pytest.mark.parametrize(
+    "batch_size, seq_len, hidden_size", [(1, 32, 32), (1, 197, 768), (1, 1024, 768)]
+)
+def test_composite_rms_norm(use_weight, batch_size, seq_len, hidden_size):
+
+    class RMSNormModel(torch.nn.Module):
+        def __init__(self, normalized_shape):
+            super().__init__()
+            self.normalized_shape = normalized_shape
+
+        def forward(self, x, weight):
+            return composite_rms_norm(x, (self.normalized_shape,), weight)
+
+    options = {"tt_enable_composite_ops": False}
+
+    input_tensor = torch.randn(batch_size, seq_len, hidden_size)
+    weight = torch.randn(hidden_size) if use_weight else None
+    model = RMSNormModel(hidden_size)
+
+    # Disable inplace buffers for inductor compilation
+    # so that we can compare the results with the golden model.
+    with torch._inductor.config.patch({"inplace_buffers": False}):
+        run_graph_test(
+            model,
+            [input_tensor, weight],
+            comparison_config=ComparisonConfig(),
+            framework=Framework.TORCH,
+            torch_options=options,
+        )
 
 
 @pytest.mark.single_device
