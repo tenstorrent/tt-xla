de0c0962c78cc3e6b4810a63b8f6df213fc31048
vvukomanTT <vvukoman@tenstorrent.com>
2025-12-23 10:53:11 +0000

    enable custom wheel name functionality

--- python_package/setup.py
+++ python_package/setup.py
@@ -46,6 +46,10 @@ class SetupConfig:
     @property
     def version(self) -> str:
         """Wheel version."""
+        custom_version = os.environ.get("TTXLA_WHEEL_VERSION_OVERRIDE")
+        if custom_version:
+            return custom_version
+
         short_hash = (
             subprocess.check_output(["git", "rev-parse", "--short", "HEAD"])
             .decode("ascii")
