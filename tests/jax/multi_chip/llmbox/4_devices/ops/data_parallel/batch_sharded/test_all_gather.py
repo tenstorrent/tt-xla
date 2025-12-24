3d2ed0411f79c1da818b9d66b62e50f49987df1e
acicovicTT <acicovic@tenstorrent.com>
2025-12-24 11:10:20 +0000

    Update

--- tests/jax/multi_chip/llmbox/4_devices/ops/data_parallel/batch_sharded/test_all_gather.py
+++ tests/jax/multi_chip/llmbox/4_devices/ops/data_parallel/batch_sharded/test_all_gather.py
@@ -24,11 +24,6 @@ from utils import failed_fe_compilation, incorrect_result
 @pytest.mark.parametrize(
     ("x_shape", "mesh_shape", "axis_names"), [((8192, 784), (1, 4), ("batch", "model"))]
 )
-@pytest.mark.xfail(
-    reason=incorrect_result(
-        "PCC comparison assertion. https://github.com/tenstorrent/tt-mlir/issues/6217"
-    )
-)
 # Cannot use ShardingMode.INPUTS because it does not define axis names and we are using jax.lax.all_gather
 @pytest.mark.parametrize(
     "sharding_mode",
