6339c6d5fe6f12d93e0ab050d90c96c51449a9f5
vvukomanTT <vvukoman@tenstorrent.com>
2025-12-25 10:03:02 +0000

    add only commit sha tag to the docker image

--- .github/build-release-docker-image.sh
+++ .github/build-release-docker-image.sh
@@ -11,7 +11,7 @@ if [ $# -ne 2 ]; then
     exit 1
 fi
 
-VERSION_TAG=$1
+COMMIT_TAG=$1
 IS_NIGHTLY_RELEASE=$2
 
 REPO=tenstorrent/tt-xla
@@ -19,32 +19,23 @@ IMAGE_NAME=ghcr.io/$REPO/tt-xla-slim
 ON_MAIN=$(git branch --show-current | grep -q main && echo "true" || echo "false")
 LATEST_TAG="latest"
 
-if [[ "$IS_NIGHTLY_RELEASE" == "true" ]]; then
-  LATEST_TAG="nightly-latest"
-fi
-
 build_and_push() {
   local image_name=$1
   local dockerfile=$2
   local on_main=$3
 
-  echo "Building image $image_name:$VERSION_TAG"
+  echo "Building image $image_name:$COMMIT_TAG"
   docker build \
     --progress=plain \
-    -t $image_name:$VERSION_TAG \
+    -t $image_name:$COMMIT_TAG \
     -f $dockerfile .
 
-  echo "Pushing image $image_name:$VERSION_TAG"
-  docker push $image_name:$VERSION_TAG
-
-  if [[ "$on_main" = "true" ]]; then
-    printf "\nPushing latest tag for $image_name"
-    docker buildx imagetools create $image_name:$DOCKER_TAG --tag $image_name:$LATEST_TAG --tag $image_name:$VERSION_TAG
-  fi
+  echo "Pushing image $image_name:$COMMIT_TAG"
+  docker push $image_name:$COMMIT_TAG
 }
 
 build_and_push $IMAGE_NAME .github/Dockerfile.release $ON_MAIN
 
 echo "Image built and pushed successfully"
 echo "IMAGE_NAME:"
-echo $IMAGE_NAME:$VERSION_TAG
+echo $IMAGE_NAME:$COMMIT_TAG
