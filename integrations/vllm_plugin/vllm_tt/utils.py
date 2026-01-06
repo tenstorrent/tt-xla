29d84169caaaf2bc4597209fd7f2e5e1bccd5948
Muhammad Asif Manzoor <mmanzoor@tenstorrent.com>
2026-01-06 19:08:35 +0000

    Add explicit sharding of weights for each projection

--- integrations/vllm_plugin/vllm_tt/utils.py
+++ integrations/vllm_plugin/vllm_tt/utils.py
@@ -1,13 +1,15 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
-# SPDX-FileCopyrightText: Portions (c) 2025 Tenstorrent AI ULC
+# SPDX-FileCopyrightText: Portions (c) 2026 Tenstorrent AI ULC
 
 from collections import OrderedDict
+from typing import List
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch_xla.distributed.spmd as xs
+from torch.nn.parameter import Parameter
 from vllm.model_executor.layers.linear import MergedColumnParallelLinear
 
 from .logger import tt_init_logger
@@ -15,12 +17,103 @@ from .logger import tt_init_logger
 logger = tt_init_logger(__name__)
 
 
+class XlaMergedColumnParallelLinear(nn.Module):
+
+    def __init__(self, merged_column_parallel_linear: nn.Module, mesh: "xs.Mesh"):
+        super().__init__()
+        assert isinstance(merged_column_parallel_linear, MergedColumnParallelLinear)
+        self.skip_bias_add = merged_column_parallel_linear.skip_bias_add
+        self.return_bias = merged_column_parallel_linear.return_bias
+        self.num_outputs = len(merged_column_parallel_linear.output_sizes)
+        self.output_sizes = merged_column_parallel_linear.output_sizes
+        self.layer_prefix = merged_column_parallel_linear.prefix.replace(".", "_")
+
+        self.weights: List[Parameter] = []
+        self.biases: List[Parameter] = []
+        self._load_weights_from_merged_column_parallel_linear(
+            merged_column_parallel_linear
+        )
+        if mesh is not None:
+            self._shard_weight(mesh)
+
+    def _shard_weight(self, mesh: "xs.Mesh"):
+        for i in range(self.num_outputs):
+            self.weights[i] = Parameter(self.weights[i].to("xla"), requires_grad=False)
+            xs.mark_sharding(self.weights[i], mesh, ("x", None))
+
+            if self.biases[i] is not None:
+                self.biases[i] = Parameter(
+                    self.biases[i].to("xla"), requires_grad=False
+                )
+                xs.mark_sharding(self.biases[i], mesh, ("x",))
+
+    def _load_weights_from_merged_column_parallel_linear(
+        self, merged_column_parallel_linear: nn.Module
+    ):
+        # The weight is a concatenation of all output weights along the output dimension
+        merged_column_parallel_weight = merged_column_parallel_linear.weight.data.cpu()
+
+        start_idx = 0
+        for i, output_size in enumerate(self.output_sizes):
+            end_idx = start_idx + output_size
+            weight = Parameter(
+                merged_column_parallel_weight[start_idx:end_idx], requires_grad=False
+            )
+            self.weights.append(weight)
+            logger.info(f"weight{i}: {self.layer_prefix}_weight_{i}")
+            self.register_parameter(f"{self.layer_prefix}_weight_{i}", weight)
+            logger.info(f"weight_{i}: {weight}")
+            start_idx = end_idx
+
+        if merged_column_parallel_linear.bias is not None:
+            start_idx = 0
+            for i, output_size in enumerate(self.output_sizes):
+                end_idx = start_idx + output_size
+                bias = Parameter(
+                    merged_column_parallel_linear.bias[start_idx:end_idx],
+                    requires_grad=False,
+                )
+                self.biases.append(bias)
+                self.register_parameter(f"{self.layer_prefix}_bias_{i}", bias)
+                start_idx = end_idx
+        else:
+            for i in range(self.num_outputs):
+                self.biases.append(None)
+                self.register_parameter(f"{self.layer_prefix}_bias_{i}", None)
+
+    def forward(self, input):
+        projections = []
+        output_biases = []
+
+        for i in range(self.num_outputs):
+            bias = self.biases[i] if not self.skip_bias_add else None
+            proj = F.linear(input, self.weights[i], bias)
+            projections.append(proj)
+
+            if self.skip_bias_add and self.biases[i] is not None:
+                output_biases.append(self.biases[i])
+
+        # Concatenate all projections to match the original output shape.
+        merged_proj = torch.cat(projections, dim=-1)
+
+        output_bias = (
+            torch.cat(output_biases, dim=-1)
+            if (self.skip_bias_add and output_biases)
+            else None
+        )
+
+        if not self.return_bias:
+            return merged_proj
+        return merged_proj, output_bias
+
+
 def partition_merged_column_parallel_linear(
     layer: torch.nn.Module, mesh: xs.Mesh
 ) -> torch.nn.Module:
     assert isinstance(layer, MergedColumnParallelLinear)
-    xs.mark_sharding(layer.weight, mesh, ("x", None))
-    return layer
+    xla_layer = XlaMergedColumnParallelLinear(layer, mesh)
+    logger.info("Applied parallel sharding to %s", layer)
+    return xla_layer
 
 
 MODULE_TYPE_TO_WRAPPING_FUNC = OrderedDict(
