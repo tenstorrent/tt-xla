e680c360c5bee1a203906125f7450655149807af
kkannan <kkannan@tenstorrent.com>
2026-01-19 06:06:30 +0000

    L1 error in qwen 2.5 vl 3b due to conv3d

---
+++ tests/torch/ops/test_conv3d.py
@@ -0,0 +1,39 @@
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import pytest
+import torch
+from infra import Framework, run_op_test_with_random_inputs
+from utils import Category
+from loguru import logger
+
+
+@pytest.mark.nightly
+@pytest.mark.record_test_properties(category=Category.OP_TEST)
+def test_conv3d():
+
+    class Conv3D(torch.nn.Module):
+        def __init__(self):
+            super().__init__()
+            self.conv = torch.nn.Conv3d(
+                3,
+                1280,
+                kernel_size=[2, 14, 14],
+                stride=[2, 14, 14],
+                bias=False,
+            )
+
+        def forward(self, x):
+            return self.conv(x)
+        
+    model = Conv3D()
+    model.to(torch.bfloat16)
+    logger.info("model={}",model)
+
+    run_op_test_with_random_inputs(
+        model,
+        [(2204, 3, 2, 14, 14)],
+        dtype=torch.bfloat16,
+        framework=Framework.TORCH,
+    )
