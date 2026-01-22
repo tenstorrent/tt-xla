820118c421515b25a4b1bcd8548f66ef60a7ac2b
Muhammad Asif Manzoor <mmanzoor@tenstorrent.com>
2026-01-22 16:40:34 -0500

    [vLLM plugin] Add sharding for VocabParallelEmbedding layer (#2904)
    ### Ticket
    https://github.com/tenstorrent/tt-xla/issues/2903
    
    ### What's changed
    - Add sharding for VocabParallelEmbedding layer
    - Weights are sharded on hidden_size (dim=1)
    
    ### Checklist
    - [X] New/Existing tests provide coverage for changes

--- integrations/vllm_plugin/vllm_tt/model_runner.py
+++ integrations/vllm_plugin/vllm_tt/model_runner.py
@@ -88,7 +88,7 @@ from .attention import (
 )
 from .logger import tt_init_logger
 from .platform import TTConfig
-from .utils import shard_model
+from .vllm_utils import shard_model
 
 
 def add_kv_sharing_layers_to_kv_cache_groups(
@@ -1364,6 +1364,7 @@ class TTModelRunner(LoRAModelRunnerMixin, KVConnectorModelRunnerMixin):
             self.model = model
         self.model.compile(backend="tt", dynamic=False)
         self.sampler = TPUSampler()
+        logger.info(f"Compiled model: \n{self.model}")
 
     def reload_weights(self) -> None:
         assert (
