c61e2c119fff7cab9a2820f73abc4acf3d070ac0
vkovinicTT <vkovinic@tenstorrent.com>
2026-01-22 13:57:41 +0000

    memory per op output

--- pjrt_implementation/src/api/flatbuffer_loaded_executable_instance.cc
+++ pjrt_implementation/src/api/flatbuffer_loaded_executable_instance.cc
@@ -188,7 +188,7 @@ tt_pjrt_status FlatbufferLoadedExecutableInstance::execute(
   DLOG_F(LOG_DEBUG, "FlatbufferLoadedExecutableInstance::Execute");
   LOG_BRINGUP_STAGE("RUNTIME_EXECUTION_START");
 
-  tt::runtime::setMemoryLogLevel(tt::runtime::MemoryLogLevel::Program);
+  tt::runtime::setMemoryLogLevel(tt::runtime::MemoryLogLevel::Operation);
 
   if (args->num_devices != m_executable_image->getNumDevicesToUtilize()) {
     LOG_F(ERROR, "Device count mismatch: %zu vs %zu", args->num_devices,
