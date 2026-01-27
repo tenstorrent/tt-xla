11bbe124115fe5b4547ea67c98fd26b796e7c8a4
Stefan Gligorijevic <189116645+sgligorijevicTT@users.noreply.github.com>
2026-01-27 12:52:35 +0000

    Test that we indeed do not use AOTAutograd

---
+++ tests/torch/graphs/test_negative_backward_over_compile.py
@@ -0,0 +1,37 @@
+# SPDX-FileCopyrightText: (c) 2026 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import pytest
+import torch
+import torch_xla.core.xla_model as xm
+from utils import Category
+
+
+class SimpleLinear(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear = torch.nn.Linear(8, 8, bias=False)
+
+    def forward(self, x):
+        return self.linear(x).relu()
+
+
+# This test becomes inapplicable once we add AOT autograd
+
+
+@pytest.mark.push
+@pytest.mark.nightly
+@pytest.mark.single_device
+@pytest.mark.record_test_properties(category=Category.GRAPH_TEST)
+def test_compile_backward_faults_without_aot_autograd():
+    torch.manual_seed(0)
+    device = xm.xla_device()
+    model = SimpleLinear().to(device)
+    model.compile(backend="tt")
+
+    x = torch.randn(4, 8, device=device, requires_grad=True)
+    output = model(x).sum()
+
+    with pytest.raises(Exception):
+        output.backward()
