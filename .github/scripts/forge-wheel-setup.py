370eca2c0e7e2f14278f60332d29079b93761745
vvukomanTT <vvukoman@tenstorrent.com>
2026-01-29 15:08:36 +0000

    publish with test dep versions

--- .github/scripts/forge-wheel-setup.py
+++ .github/scripts/forge-wheel-setup.py
@@ -8,14 +8,16 @@ from setuptools import setup
 version_tag = os.environ.get("VERSION_TAG")
 if not version_tag:
     raise RuntimeError("VERSION_TAG environment variable must be set")
+testing_tag = "0.9.0.dev20260129"
 
 setup(
     name="tt-forge",
     version=version_tag,
     homepage="https://github.com/tenstorrent/tt-forge",
+    # wheel versions hardcoded for testing since publishing to pypi doesn't work rn
     install_requires=[
-        f"pjrt-plugin-tt @https://pypi.eng.aws.tenstorrent.com/pjrt-plugin-tt/pjrt_plugin_tt-{version_tag}-cp311-cp311-linux_x86_64.whl",
-        f"vllm_tt @https://pypi.eng.aws.tenstorrent.com/vllm-tt/vllm_tt-{version_tag}-cp311-cp311-linux_x86_64.whl",
+        f"pjrt-plugin-tt @https://pypi.eng.aws.tenstorrent.com/pjrt-plugin-tt/pjrt_plugin_tt-{testing_tag}-cp311-cp311-linux_x86_64.whl",
+        f"vllm_tt @https://pypi.eng.aws.tenstorrent.com/vllm-tt/vllm_tt-{testing_tag}-cp311-cp311-linux_x86_64.whl",
     ],
     python_requires=">=3.11",
     py_modules=[],
