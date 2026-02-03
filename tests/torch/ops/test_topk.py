693597cf2662d604a2f120cfa2011461eceb2754
Sanja Djukic <sdjukic@tenstorrent.com>
2026-02-04 00:34:23 +0100

    XFAIL failing tests (#3090)
    ### Ticket
    [Link to Github
    Issue](https://github.com/tenstorrent/tt-xla/issues/3089)
    
    ### Problem description
    Tests are unsupported
    
    https://github.com/tenstorrent/tt-xla/actions/runs/21611201420/job/62320131998
    
    ### What's changed
    XFAIL tests
    
    ### Checklist
    - [ ] New/Existing tests provide coverage for changes

--- tests/torch/ops/test_topk.py
+++ tests/torch/ops/test_topk.py
@@ -8,27 +8,6 @@ from infra import Framework, run_op_test_with_random_inputs
 from utils import Category, TTArch, get_torch_device_arch
 
 
-def check_device_compatibility(input_shape: tuple, k: int):
-    """
-    Check device architecture and conditionally xfail or skip tests.
-    - All tests are xfailed on Blackhole
-    - Last only some are skipped on Wormhole
-    """
-    device_arch = get_torch_device_arch()
-
-    if device_arch == TTArch.BLACKHOLE:
-        pytest.xfail(
-            "All topk tests are xfailed on Blackhole architecture with bad PCC - https://github.com/tenstorrent/tt-xla/issues/1797"
-        )
-    elif device_arch == TTArch.WORMHOLE_B0:
-        if (input_shape == (1, 40) and k == 5) or (
-            input_shape == (1, 8400) and k == 300
-        ):
-            pytest.xfail(
-                "Test skipped on Wormhole architecture with bad PCC - https://github.com/tenstorrent/tt-xla/issues/1797"
-            )
-
-
 @pytest.mark.nightly
 @pytest.mark.single_device
 @pytest.mark.record_test_properties(
@@ -45,10 +24,11 @@ def check_device_compatibility(input_shape: tuple, k: int):
         ((1, 8400), 300),
     ],
 )
+@pytest.mark.xfail(
+    reason="Input tensor data type for ttnn.sort must be BFLOAT16 or UINT16, got DataType::FLOAT32 (https://github.com/tenstorrent/tt-xla/issues/3089)"
+)
 def test_topk_indices(input_shape: tuple, k: int):
     """Test topk operation returning indices."""
-
-    check_device_compatibility(input_shape, k)
 
     class TopKIndices(torch.nn.Module):
         def __init__(self, k):
