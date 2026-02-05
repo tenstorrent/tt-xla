2ac53f97780c9076746fda865829498da6edebc5
Stefan Gligorijevic <189116645+sgligorijevicTT@users.noreply.github.com>
2026-02-05 13:46:12 +0100

    Add cache positions to attention tests (#3161)
    ### Ticket
    Addresses 1/2 of #3165 
    
    ### Problem description
    Attention tests failed on nightly after experimental compile was added.
    I rootcaused this to a constant getting embedded into the graph instead
    of an input argument coming in, causing a conversion pattern to fail in
    MLIR. This is because cache positions weren't being passed into the test
    and instead getting defaulted.
    
    ### What's changed
    This change both papers over the lack of generality of a conversion
    pattern in MLIR **and makes the test more representative of how models
    would be used in the wild**
    
    Conversion will be fixed too, at a later date.
    
    ### Checklist
    - [X] New/Existing tests provide coverage for changes

--- tests/torch/graphs/test_attention.py
+++ tests/torch/graphs/test_attention.py
@@ -304,9 +304,17 @@ def test_llama_attention_decode(variant, variant_config, arch):
     )
     past_key_states = static_cache
 
+    cache_positions = torch.randint(0, max_cache_len, (seq_len,), dtype=torch.long)
+
     run_graph_test(
         attention,
-        [hidden_states, position_embeddings, attention_mask, past_key_states],
+        [
+            hidden_states,
+            position_embeddings,
+            attention_mask,
+            past_key_states,
+            cache_positions,
+        ],
         framework=Framework.TORCH,
         mesh=mesh,
         shard_spec_fn=get_shard_spec,
@@ -729,9 +737,17 @@ def test_qwen3_attention_decode(variant, variant_config, arch):
     )
     past_key_states = static_cache
 
+    cache_positions = torch.randint(0, max_cache_len, (seq_len,), dtype=torch.long)
+
     run_graph_test(
         attention,
-        [hidden_states, position_embeddings, attention_mask, past_key_states],
+        [
+            hidden_states,
+            position_embeddings,
+            attention_mask,
+            past_key_states,
+            cache_positions,
+        ],
         framework=Framework.TORCH,
         mesh=mesh,
         shard_spec_fn=get_shard_spec,
@@ -1384,9 +1400,17 @@ def test_qwen2_5_attention_decode(variant, variant_config, arch):
     )
     past_key_states = static_cache
 
+    cache_positions = torch.randint(0, max_cache_len, (seq_len,), dtype=torch.long)
+
     run_graph_test(
         attention,
-        [hidden_states, position_embeddings, attention_mask, past_key_states],
+        [
+            hidden_states,
+            position_embeddings,
+            attention_mask,
+            past_key_states,
+            cache_positions,
+        ],
         framework=Framework.TORCH,
         mesh=mesh,
         shard_spec_fn=get_shard_spec,
@@ -1770,9 +1794,17 @@ def test_gemma_attention_decode(variant, variant_config, arch):
     )
     past_key_states = static_cache
 
+    cache_positions = torch.randint(0, max_cache_len, (seq_len,), dtype=torch.long)
+
     run_graph_test(
         attention,
-        [hidden_states, position_embeddings, attention_mask, past_key_states],
+        [
+            hidden_states,
+            position_embeddings,
+            attention_mask,
+            past_key_states,
+            cache_positions,
+        ],
         framework=Framework.TORCH,
         mesh=mesh,
         shard_spec_fn=get_shard_spec,
@@ -2043,6 +2075,8 @@ def test_mistral_attention_decode(variant, variant_config, arch):
     )
     past_key_states = static_cache
 
+    cache_positions = torch.randint(0, max_cache_len, (seq_len,), dtype=torch.long)
+
     if arch == "llmbox":
         num_devices = xr.global_runtime_device_count()
         mesh_shape = (1, num_devices)
@@ -2063,7 +2097,13 @@ def test_mistral_attention_decode(variant, variant_config, arch):
 
     run_graph_test(
         attention,
-        [hidden_states, position_embeddings, attention_mask, past_key_states],
+        [
+            hidden_states,
+            position_embeddings,
+            attention_mask,
+            past_key_states,
+            cache_positions,
+        ],
         framework=Framework.TORCH,
         mesh=mesh,
         shard_spec_fn=get_shard_spec,
