2dcdd0e5e6d68f918d9083583a37bfa4d308acd8
Muhammad Asif Manzoor <mmanzoor@tenstorrent.com>
2026-02-05 01:40:03 +0000

    Create single function with duplicate code and call it from all the tests

--- tests/integrations/vllm_plugin/pooling/test_tensor_parallel_inference.py
+++ tests/integrations/vllm_plugin/pooling/test_tensor_parallel_inference.py
@@ -7,6 +7,8 @@ import pytest
 import torch
 import vllm
 
+from tests.integrations.vllm_plugin.pooling.utils import run_pooling_test
+
 
 @pytest.mark.push
 @pytest.mark.tensor_parallel
@@ -28,47 +30,13 @@ def test_tensor_parallel_n300(
     Test tensor parallel inference with vLLM for embedding models on N300.
     """
 
-    path = os.path.join(os.path.dirname(__file__), baseline_path)
-    loaded_data = torch.load(path)
-
-    prompts = [
-        "The quick-thinking engineer designed a compact neural processor that could adapt to changing data patterns in real time, optimizing energy use while maintaining exceptional computational accuracy as well.",
-        "Hello, my name is chatbot. How can I help you?",
-        "We build computers for AI. We design Graph Processors, high-performance RISC CPUs, and configurable chips that run our robust software stack.",
-        "The capital of France is Paris",
-    ]
-    llm_args = {
-        "model": model_name,
-        "task": "embed",
-        "dtype": "bfloat16",
-        "max_model_len": 64,
-        "disable_sliding_window": True,
-        "max_num_batched_tokens": 128,
-        "max_num_seqs": 2,
-        "additional_config": {
-            "enable_tensor_parallel": True,
-        },
-    }
-    model = vllm.LLM(**llm_args)
-
-    output_embedding = model.embed(prompts)
-
-    pcc_values = []
-    for idx, (prompt, output) in enumerate(zip(prompts, output_embedding)):
-        embeds = output.outputs.embedding
-        embeds_trimmed = (
-            (str(embeds[:16])[:-1] + ", ...]") if len(embeds) > 16 else embeds
-        )
-        print(f"Prompt: {prompt!r} \nEmbeddings: {embeds_trimmed} (size={len(embeds)})")
-
-        output_tensor = torch.tensor(embeds, dtype=torch.float32)
-        golden_tensor = loaded_data[f"prompt{idx}"]
-        pcc = torch.corrcoef(torch.stack([output_tensor, golden_tensor]))[0, 1]
-        print("PCC:", pcc.item())
-        pcc_values.append(pcc.item())
-        print("-" * 60)
-
-    assert all(p >= 0.99 for p in pcc_values), f"Low PCC values: {pcc_values}"
+    run_pooling_test(
+        model_name,
+        baseline_path,
+        max_model_len=64,
+        enable_tensor_parallel=True,
+        max_num_batched_tokens=128,
+    )
 
 
 @pytest.mark.nightly
@@ -95,44 +63,10 @@ def test_tensor_parallel_llmbox(
     Test tensor parallel inference with vLLM for embedding models on LLMBox.
     """
 
-    path = os.path.join(os.path.dirname(__file__), baseline_path)
-    loaded_data = torch.load(path)
-
-    prompts = [
-        "The quick-thinking engineer designed a compact neural processor that could adapt to changing data patterns in real time, optimizing energy use while maintaining exceptional computational accuracy as well.",
-        "Hello, my name is chatbot. How can I help you?",
-        "We build computers for AI. We design Graph Processors, high-performance RISC CPUs, and configurable chips that run our robust software stack.",
-        "The capital of France is Paris",
-    ]
-    llm_args = {
-        "model": model_name,
-        "task": "embed",
-        "dtype": "bfloat16",
-        "max_model_len": 64,
-        "disable_sliding_window": True,
-        "max_num_batched_tokens": 128,
-        "max_num_seqs": 2,
-        "additional_config": {
-            "enable_tensor_parallel": True,
-        },
-    }
-    model = vllm.LLM(**llm_args)
-
-    output_embedding = model.embed(prompts)
-
-    pcc_values = []
-    for idx, (prompt, output) in enumerate(zip(prompts, output_embedding)):
-        embeds = output.outputs.embedding
-        embeds_trimmed = (
-            (str(embeds[:16])[:-1] + ", ...]") if len(embeds) > 16 else embeds
-        )
-        print(f"Prompt: {prompt!r} \nEmbeddings: {embeds_trimmed} (size={len(embeds)})")
-
-        output_tensor = torch.tensor(embeds, dtype=torch.float32)
-        golden_tensor = loaded_data[f"prompt{idx}"]
-        pcc = torch.corrcoef(torch.stack([output_tensor, golden_tensor]))[0, 1]
-        print("PCC:", pcc.item())
-        pcc_values.append(pcc.item())
-        print("-" * 60)
-
-    assert all(p >= 0.99 for p in pcc_values), f"Low PCC values: {pcc_values}"
+    run_pooling_test(
+        model_name,
+        baseline_path,
+        max_model_len=64,
+        enable_tensor_parallel=True,
+        max_num_batched_tokens=128,
+    )
