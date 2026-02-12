6966e12f0e008f0c958f3c4c82c88601c1f591f3
Nebojsa Sumrak <nsumrak@tenstorrent.com>
2026-02-12 13:59:50 +0100

    enable tools for normal build, disable in wheel build

--- python_package/setup.py
+++ python_package/setup.py
@@ -303,6 +303,7 @@ class CMakeBuildPy(build_py):
             "-B",
             "build",
             "-DTTXLA_ENABLE_EWHEEL_INSTALL=OFF",
+            "-DTTXLA_ENABLE_TOOLS=OFF",
             "-DCODE_COVERAGE=" + code_coverage,
             "-DTTXLA_ENABLE_EXPLORER=" + enable_explorer,
             "-DCMAKE_INSTALL_PREFIX=" + str(install_dir),
