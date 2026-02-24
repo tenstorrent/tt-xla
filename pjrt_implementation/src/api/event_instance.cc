888ea957a76ea3f05a0ba6181910c7c254db8658
jzx <j>
2026-02-24 00:36:07 +0000

    cherry pick pedja change for evt instance to not run out of threads

--- pjrt_implementation/src/api/event_instance.cc
+++ pjrt_implementation/src/api/event_instance.cc
@@ -11,6 +11,7 @@
 #include "api/event_instance.h"
 
 // c++ standard library includes
+#include <exception>
 #include <stdexcept>
 
 // tracy includes
@@ -30,57 +31,20 @@ std::unique_ptr<EventInstance> EventInstance::createInstance() {
 
 EventInstance::EventInstance()
     : m_ready(false), m_status(tt_pjrt_status::kUnknown),
-      m_indestructible(false) {
-
-  m_callbacks_thread = std::make_unique<std::thread>(
-      [](EventInstance *event_instance) {
-        std::unique_lock<std::mutex> ready_lock(event_instance->m_ready_mutex);
-        event_instance->m_ready_condition.wait(
-            ready_lock, [event_instance] { return event_instance->m_ready; });
-        ready_lock.unlock();
-
-        // Copying the status and callbacks vector instead of accessing them
-        // directly from the event, because the event can be destroyed inside
-        // one of the callbacks.
-        tt_pjrt_status status = event_instance->m_status;
-        std::vector<OnReadyCallback> on_ready_callbacks(
-            event_instance->m_on_ready_callbacks);
-
-        // After this point we shouldn't access the event anymore!
-        for (OnReadyCallback &callback : on_ready_callbacks) {
-          callback.callback_function(
-              *ErrorInstance::makeError(status).release(), callback.user_arg);
-        }
-      },
-      this);
-}
+      m_indestructible(false), m_awaiters_count(0) {}
 
 EventInstance::~EventInstance() {
-  if (std::this_thread::get_id() == m_callbacks_thread->get_id()) {
-    // An `EventInstance` is allowed to delete itself in one of its callbacks,
-    // resulting in callbacks thread being the thread calling the destructor.
-    // In such cases, we must let the thread continue running independent of the
-    // destructor to avoid a deadlock. This can happen only when event is ready
-    // so it is safe to not notify the callbacks thread.
-    m_callbacks_thread->detach();
-    m_callbacks_thread.release();
-  } else {
-    killTheCallbacksThread();
-  }
-}
+  std::lock_guard<std::mutex> ready_lock(m_ready_mutex);
 
-void EventInstance::killTheCallbacksThread() {
-  // Caller should never destroy the event before it is ready, but in case that
-  // happens we don't want to leave the callbacks thread hanging so marking the
-  // event as ready with Aborted error status.
-  // Purposefully using `isReady` to wait in case `markAsReady` is in progress.
-  if (!isReady()) {
-    DLOG_F(WARNING, "Destroying the event before it is ready!");
-    markAsReady(tt_pjrt_status::kAborted);
+  if (m_awaiters_count || !m_on_ready_callbacks.empty()) {
+    // There are consumers of this event that are still waiting for it to be
+    // ready. This case is not handled properly, so crash the process here.
+    LOG_F(ERROR,
+          "Destroying the event while there are still consumers waiting on it! "
+          "m_awaiters_count: %zu, m_on_ready_callbacks.size(): %zu",
+          m_awaiters_count.load(), m_on_ready_callbacks.size());
+    std::terminate();
   }
-
-  // Let the callbacks thread finish.
-  m_callbacks_thread->join();
 }
 
 void EventInstance::bindApi(PJRT_Api *api) {
@@ -104,34 +68,21 @@ static void inline logWarningOnMultipleReadyMarks() {
   DLOG_F(WARNING, "Event marked as ready multiple times!");
 }
 
-void EventInstance::markAsReady(tt_pjrt_status status) {
-  // Skip if the event was already marked as ready. This could happen if the
-  // caller destroyed the event before it was ready so we marked it as ready
-  // with the Aborted error status, and then the event work finished and marked
-  // event as ready again. Another case could be if we have a bug where the same
-  // event is passed to two workloads or if we somehow report twice from within
-  // the same workload. Logging warning in that case so we are aware.
+void EventInstance::markAsReadyNoLock(tt_pjrt_status status) {
   if (m_ready) {
     logWarningOnMultipleReadyMarks();
     return;
   }
-  {
-    std::lock_guard<std::mutex> ready_lock(m_ready_mutex);
-    if (m_ready) {
-      logWarningOnMultipleReadyMarks();
-      return;
-    }
-
-    m_ready = true;
-    m_status = status;
-  }
-
+  m_ready = true;
+  m_status = status;
   m_ready_condition.notify_all();
 }
 
 void EventInstance::await() {
+  m_awaiters_count++;
   std::unique_lock<std::mutex> ready_lock(m_ready_mutex);
   m_ready_condition.wait(ready_lock, [this] { return m_ready; });
+  m_awaiters_count--;
 }
 
 void EventInstance::onReady(PJRT_Event_OnReadyCallback callback_function,
@@ -148,24 +99,40 @@ void EventInstance::onReady(PJRT_Event_OnReadyCallback callback_function,
   //   available (emplacing a value or setting an error), which can accidentally
   //   lead to executing a very expensive computations on a low-latency thread."
   //
-  // Based on this we decide to invoke the callback immediately on the calling
-  // thread if the event is ready, otherwise to add it to the list so it can be
-  // executed on a separate thread once the event is ready. We don't execute the
-  // pending callbacks on a thread which marks event as ready and instead do it
-  // on a separate thread to avoid doing expensive computations on a low-latency
-  // thread (as the XLA comment warns).
+  // For now, to keep things simple, we will execute the callbacks on the same
+  // thread which is marking the event as ready. Or in this thread, if the
+  // event is already ready.
 
-  if (m_ready) {
-    callback_function(getErrorFromStatus(), user_arg);
-  } else {
-    std::unique_lock<std::mutex> ready_lock(m_ready_mutex);
-    if (m_ready) {
-      // No need to hold the lock while the callback executes.
-      ready_lock.unlock();
-      callback_function(getErrorFromStatus(), user_arg);
-    } else {
+  {
+    std::lock_guard<std::mutex> ready_lock(m_ready_mutex);
+    if (!m_ready) {
       m_on_ready_callbacks.push_back({callback_function, user_arg});
+      return;
     }
+  }
+
+  // The event is already ready, so execute the callback immediately on the
+  // calling thread.
+  callback_function(getErrorFromStatus(), user_arg);
+}
+
+void EventInstance::markAsReadyAndCallback(EventInstance *event_instance,
+                                           tt_pjrt_status status) {
+  std::unique_lock<std::mutex> ready_lock(event_instance->m_ready_mutex);
+  event_instance->markAsReadyNoLock(status);
+
+  // Move the callbacks from the event instance - so that the event instance can
+  // be safely destroyed in the callback if needed.
+  std::vector<OnReadyCallback> callbacks_to_execute =
+      std::move(event_instance->m_on_ready_callbacks);
+
+  // Release the lock before executing callbacks.
+  ready_lock.unlock();
+
+  // Execute callbacks without holding lock (event may be destroyed in callback)
+  for (OnReadyCallback &callback : callbacks_to_execute) {
+    callback.callback_function(*ErrorInstance::makeError(status).release(),
+                               callback.user_arg);
   }
 }
 
