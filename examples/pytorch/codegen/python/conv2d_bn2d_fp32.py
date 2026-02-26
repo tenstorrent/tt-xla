fa0811b12a33b6155edbbb8dce5d034f8233812d
kkannan <kkannan@tenstorrent.com>
2026-02-26 07:49:49 +0000

    pcc drop in centernet hrgls due to conv2d + bn2d

---
+++ examples/pytorch/codegen/python/conv2d_bn2d_fp32.py
@@ -0,0 +1,18 @@
+
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import torch
+import torch_xla.runtime as xr
+from tt_torch import codegen_py
+from third_party.tt_forge_models.centernet.pytorch import ModelLoader,ModelVariant
+
+# Set up XLA runtime for TT backend
+xr.set_device_type("TT")
+
+loader = ModelLoader(ModelVariant.HOURGLASS_COCO)
+model = loader.load_model()
+inputs = loader.load_inputs()
+
+codegen_py(model, inputs, export_path="conv2d_bn2d_fp32")
