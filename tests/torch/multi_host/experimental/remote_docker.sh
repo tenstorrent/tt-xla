e800eb804f2100ece29f3e9229f841af2f9ced12
James Xu <jameszianxu@tenstorrent.com>
2026-02-26 21:05:45 +0000

    container to container smokecheck

--- tests/torch/multi_host/experimental/remote_docker.sh
+++ tests/torch/multi_host/experimental/remote_docker.sh
@@ -1,8 +1,8 @@
+#!/bin/bash
 # SPDX-FileCopyrightText: (c) 2026 Tenstorrent AI ULC
 #
 # SPDX-License-Identifier: Apache-2.0
 
-#!/bin/bash
 # Reference from metal: https://github.com/tenstorrent/tt-metal/commit/8c8b385e58a4a262dd98
 # To be used for mca option plm_rsh_agent script path
 
@@ -19,8 +19,10 @@ REMOTE_COMMAND="$*"
 # LogLevel=ERROR: Suppress the "Warning: Permanently added..." message for a cleaner MPI output
 SSH_OPTS="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR"
 
+IMAGE_NAME="multihost-poc"
+USER="ubuntu"
+
 # Use bash -c inside docker exec to handle the complex MPI environment string
-ssh -A $SSH_OPTS -l ubuntu "$HOST" sudo docker exec \
+ssh -A $SSH_OPTS -l $USER "$HOST" sudo docker exec \
   -u root \
-  -e LD_LIBRARY_PATH=/opt/ttmlir-toolchain/lib:/lib/x86_64-linux-gnu \
-  ubuntu-host-mapped bash -c "'$REMOTE_COMMAND'"
+  $IMAGE_NAME bash -c "'$REMOTE_COMMAND'"
