bad5517a10e53f29b042b96d5544ac0876d97562
Nebojsa Sumrak <nsumrak@tenstorrent.com>
2026-03-03 12:56:10 +0100

    [CI] Manylinux build support (#2632)
    ### Ticket
    closes https://github.com/tenstorrent/tt-xla/issues/922
    https://github.com/tenstorrent/tt-xla/issues/798
    https://github.com/tenstorrent/tt-xla/issues/3399
    related https://github.com/tenstorrent/tt-xla/issues/3140
    
    ### Problem description
    Implement manylinux build support 
    
    ### What's changed
    In order to have multilinux support for tt-xla wheel, ci build docker
    should be based on multilinux: base all docker builds on existing mlir
    images plus tt-xla requirements.
    A lot of changes, with removal of hardcoded stuff.
    New workflow for manylinux build.
    Finally, added manylinux option to test workflow.
    
    ### Checklist
    - [x] New/Existing tests provide coverage for changes

---
+++ .github/docker_install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: (c) 2024 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+
+set -e
+
+# Install XLA specific dependencies
+# Model dependencies:
+# - Whisper and Wav2Vec2 : ffmpeg
+pkg_install \
+    git-lfs \
+    patchelf \
+    protobuf-compiler \
+    libprotobuf-dev \
+    ffmpeg \
+    g++-12
+
+# Install uv tool for managing Python packages
+curl -LsSf https://astral.sh/uv/install.sh | env UV_INSTALL_DIR=/usr/local/bin sh
+
+ln -sf /usr/bin/FileCheck-17 /usr/bin/FileCheck
