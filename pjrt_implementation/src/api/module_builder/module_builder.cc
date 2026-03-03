7a236ecf58ad87fcb0d7611277186e84687e9d66
Radenko Pavlovic <rpavlovic@tenstorrent.com>
2026-03-03 19:28:57 +0000

    Set tt-mlir sha, enable greedy opt
    Set opt-level=2 for TP models.

--- pjrt_implementation/src/api/module_builder/module_builder.cc
+++ pjrt_implementation/src/api/module_builder/module_builder.cc
@@ -990,6 +990,8 @@ tt_pjrt_status ModuleBuilder::convertFromTTIRToTTNN(
   }
 
   options.optimizationLevel = compile_options.optimization_level;
+  // options.enableBfp8Conversion = compile_options.enable_bfp8_conversion;
+  options.enableGreedyOptimizer = true;
   // Map user-facing dtype names to WeightDtype enum values.
   if (compile_options.experimental_weight_dtype == "bfp8") {
     options.experimentalWeightDtype = mlir::tt::ttnn::WeightDtype::BFP_BFloat8;
