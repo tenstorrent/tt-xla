338040f48cee4753318a063481416a53585de80c
Nebojsa Sumrak <nsumrak@tenstorrent.com>
2026-03-05 11:32:58 +0100

    fix cmds for pkgm

--- python_package/ttxla_tools/install_sfpi.py
+++ python_package/ttxla_tools/install_sfpi.py
@@ -17,11 +17,11 @@ from pathlib import Path
 
 def get_fedora_package_manager():
     if shutil.which("dnf"):
-        return ["dnf", "downgrade"]
+        return ["dnf", "install", "--allowerasing"]
     elif shutil.which("yum"):
-        return ["yum", "downgrade"]
+        return ["yum", "install"]
     elif shutil.which("zypper"):
-        return ["zypper", "install", "--oldpackage"]
+        return ["zypper", "install", "--oldpackage", "--allow-downgrade"]
     else:
         return None
 
