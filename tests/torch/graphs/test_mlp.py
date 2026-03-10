fa2cb0c3ca7cb4f3deb2e3f0e9840f3faa865640
Defne Dilbaz <ddilbaz@tenstorrent.com>
2026-03-10 17:02:14 -0400

    GPT-OSS 2D Shard Spec Tests (#3666)
    ### Ticket
    #3586 
    
    ### Problem description / What's changed
    Adding 2d shard specs to gpt-oss attention and mlp unit tests since
    that's how the model is sharded currently. This is aimed to help debug
    120B pcc error (0.90) on galaxy.

--- tests/torch/graphs/test_mlp.py
+++ tests/torch/graphs/test_mlp.py
@@ -498,9 +498,8 @@ def test_gpt_oss_mlp(variant, variant_config, arch):
     )
 
     if arch == "llmbox":
-        comparison_config = ComparisonConfig(pcc=PccConfig(required_pcc=0.97))
         num_devices = xr.global_runtime_device_count()
-        mesh_shape = (1, num_devices)
+        mesh_shape = (2, num_devices // 2)
         device_ids = np.array(range(num_devices))
         mesh = Mesh(device_ids, mesh_shape, ("batch", "model"))
 
@@ -508,19 +507,18 @@ def test_gpt_oss_mlp(variant, variant_config, arch):
             shard_specs = {}
 
             # Router weights (not sharded).
-            shard_specs[mlp.router.weight] = (None, None)
+            shard_specs[mlp.router.weight] = (None, "batch")
             shard_specs[mlp.router.bias] = (None,)
 
             # Shard experts across devices.
-            shard_specs[mlp.experts.gate_up_proj] = ("model", None, None)
+            shard_specs[mlp.experts.gate_up_proj] = ("model", "batch", None)
             shard_specs[mlp.experts.gate_up_proj_bias] = ("model", None)
-            shard_specs[mlp.experts.down_proj] = ("model", None, None)
-            shard_specs[mlp.experts.down_proj_bias] = ("model", None)
+            shard_specs[mlp.experts.down_proj] = ("model", None, "batch")
+            shard_specs[mlp.experts.down_proj_bias] = ("model", "batch")
 
             return shard_specs
 
     else:
-        comparison_config = ComparisonConfig(pcc=PccConfig(required_pcc=0.99))
         mesh = None
         get_shard_spec = None
 
@@ -528,7 +526,6 @@ def test_gpt_oss_mlp(variant, variant_config, arch):
         mlp,
         [hidden_states],
         framework=Framework.TORCH,
-        comparison_config=comparison_config,
         mesh=mesh,
         shard_spec_fn=get_shard_spec,
     )
