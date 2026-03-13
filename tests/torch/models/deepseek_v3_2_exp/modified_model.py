bd866a2e6c32b446f855d102f175aea738daf9d7
Het Shah <hshah@tenstorrent.com>
2026-03-13 23:09:32 +0000

    fix test for prefill

--- tests/torch/models/deepseek_v3_2_exp/modified_model.py
+++ tests/torch/models/deepseek_v3_2_exp/modified_model.py
@@ -893,10 +893,21 @@ class MLA(nn.Module):
             Indexer(args, haddamard_matrix) if args.index_n_heads > 0 else None
         )
         self.register_buffer("hadamard_matrix", haddamard_matrix, persistent=False)
+
+        self.register_buffer(
+            "kv_cache",
+            torch.zeros(args.max_batch_size, args.max_seq_len, self.kv_lora_rank),
+            persistent=False,
+        )
+        self.register_buffer(
+            "pe_cache",
+            torch.zeros(args.max_batch_size, args.max_seq_len, self.qk_rope_head_dim),
+            persistent=False,
+        )
+        # Preset topk indices tensor. Only used for testing.
+        self.register_buffer("prepopulated_topk_indices", None, persistent=False)
         self.dequant_wkv_b = None
 
-        # Preset topk indices tensor. Only used for testing.
-        self.prepopulated_topk_indices = None
 
     def forward(
         self,
@@ -955,7 +966,10 @@ class MLA(nn.Module):
 
             # indexer
             if self.indexer is not None:
-                topk_indices = self.indexer(x, qr, start_pos, freqs_cis, mask)
+                if self.prepopulated_topk_indices is not None:
+                    topk_indices = self.prepopulated_topk_indices
+                else:
+                    topk_indices = self.indexer(x, qr, start_pos, freqs_cis, mask)
                 index_mask = torch.full(
                     (bsz, seqlen, seqlen), float("-inf"), device=x.device
                 ).scatter_(-1, topk_indices, 0)
