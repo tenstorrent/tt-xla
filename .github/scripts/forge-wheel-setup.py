75e4b07eda0dcb8682c72e2bcce1b53d8cd643b1
vvukomanTT <vvukoman@tenstorrent.com>
2026-03-13 10:46:18 +0000

    setup forge release

--- .github/scripts/forge-wheel-setup.py
+++ .github/scripts/forge-wheel-setup.py
@@ -8,6 +8,7 @@ from setuptools import setup
 version_tag = os.environ.get("VERSION_TAG")
 if not version_tag:
     raise RuntimeError("VERSION_TAG environment variable must be set")
+version_tag = "1.0.0.dev20260311000821"
 
 setup(
     name="tt-forge",
