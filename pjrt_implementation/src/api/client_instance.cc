d59150b72c30be7d23676eba557b84b0dfe2d601
James Zian Xu <jameszianxu@tenstorrent.com>
2026-03-18 22:39:38 +0000

    try opening in fabric 2d torus xy??

--- pjrt_implementation/src/api/client_instance.cc
+++ pjrt_implementation/src/api/client_instance.cc
@@ -284,11 +284,6 @@ PJRT_Error *ClientInstance::initialize() {
 
 
   if (distributed_runtime) {
-    
-//     if (std::getenv("TT_RUNTIME_USING_DUALT3K") != nullptr &&
-//     std::string(std::getenv("TT_RUNTIME_USING_DUALT3K")) != "0") {
-//   tt::runtime::setFabricConfig(tt::runtime::FabricConfig::FABRIC_2D);
-// }
     tt_pjrt_status launch_result = launchDistributedRuntime();
     if (!tt_pjrt_status_is_ok(launch_result)) {
       return *ErrorInstance::makeError(launch_result).release();
@@ -338,7 +333,7 @@ tt_pjrt_status ClientInstance::populateDevices() {
   // bugs
   if (std::getenv("TT_RUNTIME_USING_DUALT3K") != nullptr &&
       std::string(std::getenv("TT_RUNTIME_USING_DUALT3K")) != "0") {
-    tt::runtime::setFabricConfig(tt::runtime::FabricConfig::FABRIC_2D);
+    tt::runtime::setFabricConfig(tt::runtime::FabricConfig::FABRIC_2D_TORUS_XY);
   }
   m_system_descriptor = tt::runtime::getCurrentSystemDesc();
 
@@ -477,7 +472,7 @@ ClientInstance::computeFabricConfig(const std::vector<uint32_t> &mesh_shape) {
     // [Workaround] Override fabric config to FABRIC_2D for dual t3k cluster
     if (std::getenv("TT_RUNTIME_USING_DUALT3K") != nullptr &&
         std::string(std::getenv("TT_RUNTIME_USING_DUALT3K")) != "0") {
-      return tt::runtime::MeshFabricConfig{tt::runtime::FabricConfig::FABRIC_2D,
+      return tt::runtime::MeshFabricConfig{tt::runtime::FabricConfig::FABRIC_2D_TORUS_XY,
                                            {}};
     }
 
@@ -492,6 +487,7 @@ ClientInstance::computeFabricConfig(const std::vector<uint32_t> &mesh_shape) {
   }
   return tt::runtime::computeMeshFabricConfig(m_system_descriptor, mesh_shape);
 }
+
 
 tt::runtime::Device ClientInstance::getOrCreateMeshDevice(
     const std::vector<uint32_t> &target_mesh_shape) {
