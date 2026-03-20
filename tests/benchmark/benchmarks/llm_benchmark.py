618726e89510fae79c0e939e432ac5daab16cfa4
Sungjoon Shon <sshon@tenstorrent.com>
2026-03-20 00:50:22 +0000

    Modify GPT-OSS TP benchmark test to sparse MoE
    Inject custom MoE logic for GPT-OSS sparse MoE variant into benchmark test

--- tests/benchmark/benchmarks/llm_benchmark.py
+++ tests/benchmark/benchmarks/llm_benchmark.py
@@ -22,6 +22,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, PreTrainedTokenize
 from transformers.cache_utils import StaticCache
 from transformers.modeling_outputs import CausalLMOutputWithPast
 from tt_torch.sharding import sharding_constraint_hook
+from tt_torch.sparse_mlp import enable_sparse_mlp
 from utils import (
     build_xla_export_name,
     compute_pcc,
@@ -210,6 +211,7 @@ def benchmark_llm_torch_xla(
     model_name_for_accuracy: str = None,
     hf_model_name_for_accuracy: str = None,
     max_output_tokens=None,
+    inject_custom_moe: bool = False,
 ):
     """
     Benchmark an LLM (Large Language Model) using PyTorch and torch-xla.
@@ -353,6 +355,8 @@ def benchmark_llm_torch_xla(
     if is_multichip:
         shard_specs = shard_spec_fn(model_loader, model)
         mesh = get_mesh(model_loader, mesh_config_fn)
+        if inject_custom_moe:
+            enable_sparse_mlp(model, mesh.mesh_shape, cluster_axis=0)
         if shard_specs is not None:
             for tensor, shard_spec in shard_specs.items():
                 xs.mark_sharding(tensor, mesh, shard_spec)
