8e2c1195bd2f2ebbd674ab7a0c5b9018ac388972
Stefan Gligorijevic <189116645+sgligorijevicTT@users.noreply.github.com>
2026-03-25 10:19:48 +0000

    Respect dry run

--- pjrt_implementation/inc/api/so_loaded_executable_instance.h
+++ pjrt_implementation/inc/api/so_loaded_executable_instance.h
@@ -58,10 +58,6 @@ private:
                      tt::runtime::Device device, size_t num_devices,
                      std::uint32_t program_index, size_t arg_index) override;
 
-  // Create default-initialized output buffers for SO execution
-  void createDefaultOutputBuffers(PJRT_Buffer **const *output_lists,
-                                  size_t num_devices);
-
   // Fills the output lists of the PJRT API with actual output tensors
   // produced by PythonModelRunner execution.
   void fillPJRTOutputLists(
