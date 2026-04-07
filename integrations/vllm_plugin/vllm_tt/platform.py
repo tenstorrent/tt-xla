83eed75c51910067c3d2e27c74c6471e3c1cf0c8
Muhammad Asif Manzoor <mmanzoor@tenstorrent.com>
2026-04-07 17:20:02 +0000

    [vLLM] uplift v0.19

--- integrations/vllm_plugin/vllm_tt/platform.py
+++ integrations/vllm_plugin/vllm_tt/platform.py
@@ -273,6 +273,11 @@ class TTPlatform(Platform):
             )
 
     @classmethod
+    def update_block_size_for_backend(cls, vllm_config: "VllmConfig") -> None:
+        # TT backend requires a block size divisible by 32 for optimal performance.
+        return 32
+
+    @classmethod
     def is_pin_memory_available(cls):
         logger.warning("Pin memory is not supported on TT.")
         return False
@@ -286,7 +291,7 @@ class TTPlatform(Platform):
         cls,
         prompt: "PromptType",
         params: "ParamsType",
-        processed_inputs: "ProcessorInputs",
+        # processed_inputs: "ProcessorInputs",
     ) -> None:
         pass
 
