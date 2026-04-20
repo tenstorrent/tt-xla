f4bf39564fb3aed91c5afa9a55cc4cfd449346b6
Kyle Mabee <kmabee@tenstorrent.com>
2026-04-20 14:54:46 +0000

    Garbage output debug: Update test_llama3_3b_generation.py to add opt_1 test, change prompt, increase tokens, remove topp for simplicity
    (cherry picked from commit 7db5c8bba88bd6d0e3f28dfa129cf10f2f33cf42)

--- tests/integrations/vllm_plugin/generative/test_llama3_3b_generation.py
+++ tests/integrations/vllm_plugin/generative/test_llama3_3b_generation.py
@@ -9,9 +9,9 @@ import vllm
 @pytest.mark.single_device
 def test_llama3_3b_generation():
     prompts = [
-        "I like taking walks in the",
+        "Tell me a story.",
     ]
-    sampling_params = vllm.SamplingParams(temperature=0.8, top_p=0.95, max_tokens=32)
+    sampling_params = vllm.SamplingParams(temperature=0.8, max_tokens=64)
     llm_args = {
         "model": "meta-llama/Llama-3.2-3B",
         "max_num_batched_tokens": 128,
@@ -21,6 +21,31 @@ def test_llama3_3b_generation():
         "additional_config": {
             "enable_const_eval": False,
             "min_context_len": 32,
+        },
+    }
+    llm = vllm.LLM(**llm_args)
+
+    output_text = llm.generate(prompts, sampling_params)[0].outputs[0].text
+    print(f"prompt: {prompts[0]}, output: {output_text}")
+
+
+@pytest.mark.nightly
+@pytest.mark.single_device
+def test_llama3_3b_generation_opt_level_1():
+    prompts = [
+        "Tell me a story.",
+    ]
+    sampling_params = vllm.SamplingParams(temperature=0.8, max_tokens=64)
+    llm_args = {
+        "model": "meta-llama/Llama-3.2-3B",
+        "max_num_batched_tokens": 128,
+        "max_num_seqs": 1,
+        "max_model_len": 128,
+        "gpu_memory_utilization": 0.002,
+        "additional_config": {
+            "enable_const_eval": False,
+            "min_context_len": 32,
+            "optimization_level": 1,
         },
     }
     llm = vllm.LLM(**llm_args)
