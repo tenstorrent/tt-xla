f48870657e051938256163ea9b464c9f03faffe3
James Zian Xu <jameszianxu@tenstorrent.com>
2026-04-20 15:11:26 +0000

    Clean up some of the container launch

---
+++ .github/scripts/filter_multihost_worker_hostfile.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+readonly SOURCE_HOSTFILE="${1:-/etc/mpirun/hostfile}"
+readonly OUTPUT_HOSTFILE="${2:-/tmp/mpirun-workers-hostfile}"
+readonly CONTROLLER_HOSTNAME="${CONTROLLER_HOSTNAME:-$(hostname -s)}"
+
+awk -v controller="${CONTROLLER_HOSTNAME}" '
+  /^[[:space:]]*#/ {next}
+  NF == 0 {next}
+  {
+    host=$1
+    short=host
+    sub(/\..*$/, "", short)
+    if (host != controller && short != controller) {
+      print
+    }
+  }
+' "${SOURCE_HOSTFILE}" > "${OUTPUT_HOSTFILE}"
