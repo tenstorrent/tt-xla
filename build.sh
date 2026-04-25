0a4cae42bc8347fd55d566e7ff5ab1414fd2e621
Nicholas Smith <nsmith@tenstorrent.com>
2026-04-25 15:18:03 -0500

    init

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git submodule update --init --recursive
+
+cd tt-xla
+source venv/activate
+cmake -G Ninja -B build -DCMAKE_BUILD_TYPE=Debug -DTTMLIR_SOURCE_DIR_OVERRIDE=../tt-mlir -DTTMLIR_TTMETAL_SOURCE_DIR=../tt-metal
+cmake --build build
