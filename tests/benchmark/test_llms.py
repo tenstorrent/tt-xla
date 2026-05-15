962b87e1fef766bd9a9ddcd161fa3f39b1988618
James Zian Xu <jameszianxu@tenstorrent.com>
2026-05-15 21:59:05 +0000

    add back cli controls for max_output_tokens (default 1), num_layers (default 2), batch_size (defaut 64)

--- tests/benchmark/test_llms.py
+++ tests/benchmark/test_llms.py
@@ -2082,11 +2082,12 @@ def test_kimi_k2_tp_galaxy_2_layers(
         ModelLoader,
         variant,
         output_file,
-        num_layers=2,
+        num_layers=num_layers or 2,
         request=request,
         accuracy_testing=accuracy_testing,
-        batch_size=64,  # Test hangs for a batch size of 128 - Issue: https://github.com/tenstorrent/tt-xla/issues/4565
-        max_output_tokens=1,
+        batch_size=batch_size
+        or 64,  # Test hangs for a batch size of 128 - Issue: https://github.com/tenstorrent/tt-xla/issues/4565
+        max_output_tokens=max_output_tokens or 1,
         decode_only=decode_only,  # This test fails prefill + decode with low decode pcc = Issue: https://github.com/tenstorrent/tt-xla/issues/4614
         input_output_sharding_spec=("batch", None),
         use_mla_cache=True,
