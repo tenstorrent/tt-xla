d7cbd1a0fd6512618c5a083959e908bc8a75aa76
ctr-kkannan <ctr-kkannan@ext.tenstorrent.com>
2026-05-21 13:36:41 +0000

    pcc drop in text_encoder_2 of playground_v2_5

--- tests/torch/models/playground_v2_5/test_text_encoder_2.py
+++ tests/torch/models/playground_v2_5/test_text_encoder_2.py
@@ -14,11 +14,11 @@ from third_party.tt_forge_models.playground_v2_5.pytorch import (
     ModelLoader,
     ModelVariant,
 )
+from loguru import logger
 
-
-@pytest.mark.xfail(
-    reason="AssertionError: Evaluation result 0 failed: PCC comparison failed. Calculated: pcc=0.9711299232660258. Required: pcc=0.99 — https://github.com/tenstorrent/tt-xla/issues/4709"
-)
+# @pytest.mark.xfail(
+#     reason="AssertionError: Evaluation result 0 failed: PCC comparison failed. Calculated: pcc=0.9711299232660258. Required: pcc=0.99 — https://github.com/tenstorrent/tt-xla/issues/4709"
+# )
 @pytest.mark.nightly
 @pytest.mark.model_test
 def test_text_encoder_2():
@@ -28,6 +28,8 @@ def test_text_encoder_2():
     loader = ModelLoader(ModelVariant.TEXT_ENCODER_2)
     model = loader.load_model(dtype_override=torch.float32)
     inputs = loader.load_inputs(dtype_override=torch.float32)
+    
+    logger.info("model={}",model)
 
     run_graph_test(
         model,
