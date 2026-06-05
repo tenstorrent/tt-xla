0b241ffcfdf7f33e06c17631215c5f30b5fd6acb
AI Model Bringup <ai-bringup@tenstorrent.com>
2026-06-05 13:24:22 +0000

    perf(mobilenet_v2_0.35_96): opt=2 trace=True

--- tests/benchmark/test_vision.py
+++ tests/benchmark/test_vision.py
@@ -492,8 +492,8 @@ def test_mobilenetv2_035_96(output_file, request):
         batch_size=batch_size,
         input_size=input_size,
         data_format=data_format,
-        optimization_level=0,
-        trace_enabled=False,
+        optimization_level=2,
+        trace_enabled=True,
     )
 
 
