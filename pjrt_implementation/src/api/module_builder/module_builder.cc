21726fffcf68512c34c5e79a5c251a8f10680396
acicovicTT <acicovic@tenstorrent.com>
2026-06-09 14:24:20 +0000

    Remove optimizer-related device pointer option from module builder

--- pjrt_implementation/src/api/module_builder/module_builder.cc
+++ pjrt_implementation/src/api/module_builder/module_builder.cc
@@ -1002,17 +1002,6 @@ tt_pjrt_status ModuleBuilder::convertFromTTIRToTTNN(
 
   options.meshShape = {devices_mesh_shape[0], devices_mesh_shape[1]};
 
-  // Use the `options.devicePtr` to pass the device pointer to the optimizer in
-  // order to avoid closing and reopening the device afterwards.
-  // Optimizer is enabled for optimization_level >= 1
-  if (compile_options.optimization_level >= 1) {
-    tt::runtime::Device submesh_for_optim =
-        client_instance->getOrCreateOptimizerSubmesh(devices_mesh_shape);
-    options.devicePtr =
-        std::static_pointer_cast<tt::tt_metal::distributed::MeshDevice>(
-            submesh_for_optim.handle);
-  }
-
   // TODO(dmilinkovic): Disable const-eval on CPU for EmitC backend until
   // CPU hoisting support is added.
   // https://github.com/tenstorrent/tt-mlir/issues/6100
