590a0c329de7474c893022ff50a94d4b87aa0560
Muhammad Asif Manzoor <mmanzoor@tenstorrent.com>
2026-07-09 01:03:15 +0000

    Attention layer chagnes

--- integrations/vllm_plugin/vllm_tt/__init__.py
+++ integrations/vllm_plugin/vllm_tt/__init__.py
@@ -24,6 +24,19 @@ def register():
 
 
 def register_oot_layers():
+    # Patch vLLM Attention symbol from the general-plugins path.
+    import vllm.model_executor.layers.attention as _attn_pkg
+    import vllm.model_executor.layers.attention.attention as _attn_mod
+    import vllm.model_executor.layers.attention.encoder_only_attention as _enc_attn_mod
+    from vllm_tt.attention_impls.attention import TTAttention, TTEncoderOnlyAttention
+
+    _attn_mod.Attention = TTAttention
+    _attn_pkg.Attention = TTAttention
+    _enc_attn_mod.Attention = TTAttention
+
+    _enc_attn_mod.EncoderOnlyAttention = TTEncoderOnlyAttention
+    _attn_pkg.EncoderOnlyAttention = TTEncoderOnlyAttention
+
     # Registers all OOT backends
     from .attention_impls import attention_mla  # noqa: F401
     from .layers.fused_moe import TTFusedMoE  # noqa: F401
